f10044b73951276a5027b027ba688ff8df883a2f
Jeff Siarto <jeff@element84.com>
2015-06-22 00:46:44 -0400

    MMT-4: Dashboard view stub and temporary pages controller; renamed index.html and set the dashboard as the root page; CSS and image cleanup; Added body class conditional to the application layout.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def dashboard
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'search#search1'
+   root 'pages#dashboard'
 
   get "/fetch_collection_search_results" => 'search#fetch_collection_search_results', as: 'fetch_collection_search_results'
 
