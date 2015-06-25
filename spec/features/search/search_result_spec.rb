f5ba99cb21a22a1db9db0e04994b013d1bb418c4
Kenneth Shae Murphy <Kenneth.S.Murphy@NASA.gov>
2015-06-25 16:37:15 -0400

    MMT-268: Incorporated PR Feedback

--- spec/features/search/search_result_spec.rb
+++ spec/features/search/search_result_spec.rb
@@ -1,30 +1,19 @@
-# MMT-9, MMT-22
+# MMT-22
 
 require 'rails_helper'
 
-results_per_page = 25
-
-describe 'Quick Find Search results' do
+describe 'Search results' do
 
   before :each do
     visit "/search"
   end
-  it 'the user is on the search page' do
-    expect(page).to have_content('Search Result')
-  end
-  context "and performing a quick find search that should return 2 hits" do
+  context 'when performing a collection search by entry id' do
     before do
       fill_in 'entry_id', with: 'DEM_100M_1'
       click_on 'Find'
     end
     it 'displays collection results' do
       expect(page).to have_content('2 Results for: Entry Id: "DEM_100M_1"')
-    end
-    it 'finds collections' do
-      expect(page).not_to have_content('No collections found')
-    end
-    it 'has insufficient rows to display pagination' do
-      expect(page).not_to have_content("Showing records 1 - #{results_per_page} of")
     end
     it 'displays expected Entry ID, Entry Title and Last Modified values' do
       expect(page).to have_content('C1000000719-EDF_OPS')
@@ -35,19 +24,13 @@ describe 'Quick Find Search results' do
 
   end
 
-  context "and performing a quick find search from that should no hits" do
+  context 'when performing a search that has no results' do
     before do
       fill_in 'entry_id', with: 'NO HITS'
       click_on 'Find'
     end
     it 'displays collection results' do
       expect(page).to have_content(' Results')
-    end
-    it 'finds no collections' do
-      expect(page).to have_content('No collections found')
-    end
-    it 'has insufficient rows to display pagination' do
-      expect(page).not_to have_content("Showing records 1 - #{results_per_page} of")
     end
     # We could add a test to actually examine the results table contents more specifically
   end
