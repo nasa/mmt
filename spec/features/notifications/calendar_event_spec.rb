af1ca0eb1f7a88f9f1bda44b8179a32f0d1d5234
Kenneth Shae Murphy <Kenneth.S.Murphy@NASA.gov>
2015-07-07 15:54:43 -0400

    MMT-127: Made suggested VCR related changes.

--- spec/features/notifications/calendar_event_spec.rb
+++ spec/features/notifications/calendar_event_spec.rb
@@ -6,45 +6,78 @@ BASE_STRING = "This site may be unavailable for a brief period due to planned ma
 
 describe 'Calendar Event Query' do
 
-  context 'When on home page and not logged in' do
-    it 'shows no notifications' do
+  context 'when on home page and not logged in' do
+    before do
       visit "/"
+    end
+
+    it 'shows no notifications' do
       expect(page).to_not have_content(BASE_STRING)
     end
   end
 
 
-  context 'On home page and logged in' do
+  context 'when on home page and logged in' do
     before :each do
       login
     end
-    it 'shows no notifications if there are none' do
-      VCR.use_cassette 'notifications/no_notifications' do
-        visit "/"
+
+    context 'when there are no notifications in the db' do
+      before do
+        VCR.use_cassette('notifications/no_notifications', record: :none) do
+          visit "/"
+        end
+      end
+
+      it 'does not display a notification' do
         expect(page).to_not have_content(BASE_STRING)
       end
     end
-    it 'shows the proper upcoming notification if there are several future ones' do
-      VCR.use_cassette 'notifications/several_future_notifications' do
-        visit "/"
+
+    context 'when there are several future notifications in the db' do
+      before do
+        VCR.use_cassette('notifications/several_future_notifications', record: :none) do
+          visit "/"
+        end
+      end
+
+      it 'shows the proper upcoming notification' do
         expect(page).to have_content("#{BASE_STRING} on Sunday, 7/18/2021 between 8 am - 9 am ET")
       end
     end
-    it 'shows a multi-day outage correctly' do
-      VCR.use_cassette 'notifications/multi_day_notification' do
-        visit "/"
+
+    context 'when the notification is for a multi-day outage' do
+      before do
+        VCR.use_cassette('notifications/multi_day_notification', record: :none) do
+          visit "/"
+        end
+      end
+
+      it 'shows the multi-day outage correctly' do
         expect(page).to have_content("#{BASE_STRING} between Monday, 7/12/2021 at 5 am and Tuesday, 7/13/2021 at 4 pm ET")
       end
     end
-    it 'does not show expired notifications' do
-      VCR.use_cassette 'notifications/expired_notifications' do
-        visit "/"
+
+    context 'when the notification are all expired' do
+      before do
+        VCR.use_cassette('notifications/expired_notifications', record: :none) do
+          visit "/"
+        end
+      end
+
+      it 'no notifications are shown' do
         expect(page).to_not have_content(BASE_STRING)
       end
     end
-    it 'shows Noon and Midnight times properly' do
-      VCR.use_cassette 'notifications/noon_midnight_notifications' do
-        visit "/"
+
+    context 'when Noon and Midnight should be shown' do
+      before do
+        VCR.use_cassette('notifications/noon_midnight_notifications', record: :none) do
+          visit "/"
+        end
+      end
+
+      it 'shows Noon and Midnight times properly' do
         expect(page).to have_content("#{BASE_STRING} on Tuesday, 6/17/2025 between 12 Midnight - 12 Noon ET")
       end
     end
