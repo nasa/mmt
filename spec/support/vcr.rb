26af36419d4d95935aafed56a70c03bfb424ef3d
Kenneth Shae Murphy <Kenneth.S.Murphy@NASA.gov>
2015-07-08 08:04:16 -0400

    MMT-127: Changed host exclusion command in vcr.rb

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,5 +2,5 @@ VCR.configure do |c|
   c.cassette_library_dir = Rails.root.join("spec", "vcr")
   c.hook_into :faraday
   c.allow_http_connections_when_no_cassette = true
-  c.ignore_localhost = true
+  c.ignore_hosts '127.0.0.1', 'localhost'
 end
