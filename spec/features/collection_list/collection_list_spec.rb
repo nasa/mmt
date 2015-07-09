212f09582afd70ab338bad0d2d7aeed5be6eaa5a
Kenneth Shae Murphy <Kenneth.S.Murphy@NASA.gov>
2015-07-09 16:34:02 -0400

    MMT-113: Removed collection_list_spec.rb

--- spec/features/collection_list/collection_list_spec.rb
+++
@@ -1,7 +0,0 @@
-# MMT-113
-
-# In the future we need to add tests for 0 collections, 1 collection and multiple pages of collections.
-# When we have more realistic data we also need to add tests for various counts of granules, and the proper formatting of those counts.
-
-# NOTE: Tests for this feature are now located in the public_holdings_spec.rb file
-
