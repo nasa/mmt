4005854451b26f6358f89acf5b6236a256107562
Matthew Crouch <matthew@element84.com>
2015-07-24 19:41:58 -0400

    MMT-11 fixed broken specs and removed un-needed changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714180621) do
+ActiveRecord::Schema.define(version: 20150713153409) do
 
   create_table "drafts", force: :cascade do |t|
     t.integer  "user_id"
