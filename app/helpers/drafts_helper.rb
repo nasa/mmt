a52f940591161c6984c915b01b019aadf6cf214e
Kenneth Shae Murphy <Kenneth.S.Murphy@NASA.gov>
2015-07-27 11:43:43 -0400

    MMT-53: Incorporated feedback from PR

--- app/helpers/drafts_helper.rb
+++ app/helpers/drafts_helper.rb
@@ -1,3 +1,17 @@
 module DraftsHelper
 
+
+  def construct_keyword_string(hash_obj, str)
+    # Assumes hash is passed in as ordered
+    hash_obj.each do |key, value|
+      if value.is_a?(String)
+        str << ' > ' if !str.blank?
+        str = str << value
+      else # Use tail recursion to construct the string found in the sub-hash
+        str = construct_keyword_string(value, str)
+      end
+    end
+    return str
+  end
+
 end
