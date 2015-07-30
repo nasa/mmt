05a043ccffda6deb906cef2eb6f84db8f565648e
Kenneth Shae Murphy <Kenneth.S.Murphy@NASA.gov>
2015-07-30 13:20:52 -0400

    MMT-57: Incorporated PR feedback.

--- spec/models/draft_spec.rb
+++ spec/models/draft_spec.rb
@@ -11,13 +11,13 @@ describe Draft do
     expect(draft.title).to eq('<Untitled Collection Record>')
   end
 
-  # id method
+  # display_entry_id method
   it 'returns a drafts entry_id if available' do
     draft = build(:draft, entry_id: 'ID Example')
     expect(draft.display_entry_id).to eq('ID Example')
   end
   it 'returns <Blank Entry Id> if there is no entry id' do
-    draft = build(:draft, entry_title: nil)
+    draft = build(:draft)
     expect(draft.display_entry_id).to eq('<Blank Entry Id>')
   end
 end
