23fa3e22f6c1d08d42e7e3438ec28f92922349ae
Kenneth Shae Murphy <Kenneth.S.Murphy@NASA.gov>
2015-07-30 13:13:57 -0400

    MMT-57: Added tests for Entry Id populating and retrieval, per PR feedback.

--- spec/models/draft_spec.rb
+++ spec/models/draft_spec.rb
@@ -6,9 +6,18 @@ describe Draft do
     draft = build(:draft, entry_title: 'Title Example')
     expect(draft.title).to eq('Title Example')
   end
-  it 'returns Untitled if there is no entry title' do
+  it 'returns <Untitled Collection Record> if there is no entry title' do
     draft = build(:draft, entry_title: nil)
     expect(draft.title).to eq('<Untitled Collection Record>')
   end
 
+  # id method
+  it 'returns a drafts entry_id if available' do
+    draft = build(:draft, entry_id: 'ID Example')
+    expect(draft.display_entry_id).to eq('ID Example')
+  end
+  it 'returns <Blank Entry Id> if there is no entry id' do
+    draft = build(:draft, entry_title: nil)
+    expect(draft.display_entry_id).to eq('<Blank Entry Id>')
+  end
 end
