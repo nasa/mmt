2bbcc58fd9cd9af46583ed4ca257e989daec46d1
mreese <mreese@element84.com>
2015-08-03 11:16:09 -0500

    MMT-292: Style accordion and rearrange add new buttons

--- app/assets/javascripts/accordion.js
+++ app/assets/javascripts/accordion.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $(document).on('click', '.accordion > .accordion-header', function (e) {
     jQuery(this).parent().find('.accordion-body').slideToggle('fast');  // apply the toggle to the ul
     jQuery(this).parent().toggleClass('is-closed');
-    jQuery(this).find("i.chevron-toggle").toggleClass("closed");
+    jQuery(this).find("i.toggle").toggleClass("closed");
     e.preventDefault();
   });
 
