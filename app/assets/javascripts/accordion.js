736fa63850257de6c8e44ee906a4ef6d6b619d75
Matthew Crouch <matthew@element84.com>
2015-08-04 09:01:38 -0400

    MMT-292 fixed chevron behavior and added US Dollars to Fees field

--- app/assets/javascripts/accordion.js
+++ app/assets/javascripts/accordion.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
   $(document).on('click', '.accordion > .accordion-header', function (e) {
     jQuery(this).parent().find('.accordion-body').slideToggle('fast');  // apply the toggle to the ul
     jQuery(this).parent().toggleClass('is-closed');
-    jQuery(this).find("i.toggle").toggleClass("closed");
     e.preventDefault();
   });
 
