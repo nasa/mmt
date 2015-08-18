faf81dbb9077aee9faf89a4f155999c6818cfd9e
Matthew Crouch <matthew@element84.com>
2015-08-18 12:04:47 -0400

    removed duplicate global requests

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,4 +3,5 @@ VCR.configure do |c|
   c.hook_into :faraday
   c.allow_http_connections_when_no_cassette = false
   c.ignore_hosts '127.0.0.1', 'localhost'
+  c.default_cassette_options = {allow_playback_repeats: true}
 end
