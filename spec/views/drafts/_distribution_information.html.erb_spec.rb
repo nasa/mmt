d70a71af3281cee74737a9c2c7197f2d83e97a45
Kenneth Shae Murphy <Kenneth.S.Murphy@NASA.gov>
2015-09-01 17:26:35 -0400

    MMT-297: Insured view test data passed schema validation.

--- spec/views/drafts/_distribution_information.html.erb_spec.rb
+++ spec/views/drafts/_distribution_information.html.erb_spec.rb
@@ -8,7 +8,7 @@ include TypesHelper
 
 template_path = 'drafts/previews/_distribution_information.html.erb'
 
-test_fee = '1234.56'
+test_fee = 1234.56
 
 describe template_path, type: :view do
   context 'when the distribution information' do
@@ -30,20 +30,20 @@ describe template_path, type: :view do
       before do
         draft_json['RelatedUrls'] = [
             # minimal object populating
-            {},
-            {"URLs"=>[]},
+            {"URLs"=>['test url']},
             # Regular object populating
             {"Description"=> 'test 1 Description',"Protocol"=> 'test 1 Protocol',"URLs"=> ['test 1a URL', 'test 1b URL'],"Title"=>'test 1 Title',"MimeType"=>'test 1 MimeType',
-             "Caption"=>'test 1 Caption',"FileSize"=>{"Size"=>123, "Unit"=>'test 1 FileSize Unit'}, "ContentType"=>{"Type"=>'test 1 ContentType Type', "Subtype"=>'test 1 ContentType Subtype'}},
+             "Caption"=>'test 1 Caption',"FileSize"=>{"Size"=>123, "Unit"=>'Unt1'}, "ContentType"=>{"Type"=>'test 1 ContentType Type', "Subtype"=>'test 1 ContentType Subtype'}},
             {"Description"=> 'test 2 Description',"Protocol"=> 'test 2 Protocol',"URLs"=> ['test 2a URL', 'test 2b URL'],"Title"=>'test 2 Title',"MimeType"=>'test 2 MimeType',
-             "Caption"=>'test 2 Caption',"FileSize"=>{"Size"=>321, "Unit"=>'test 2 FileSize Unit'}, "ContentType"=>{"Type"=>'test 2 ContentType Type', "Subtype"=>'test 2 ContentType Subtype'}}
+             "Caption"=>'test 2 Caption',"FileSize"=>{"Size"=>321, "Unit"=>'Unt2'}, "ContentType"=>{"Type"=>'test 2 ContentType Type', "Subtype"=>'test 2 ContentType Subtype'}}
         ]
         draft_json['Distributions'] = [
             {},
-            {"DistributionMedia"=>'test 2 DistributionMedia',"DistributionSize"=>'test 2 DistributionSize',"DistributionFormat"=>'test 2 DistributionFormat', "Fees"=>'bad fee amount'},
+            {"DistributionMedia"=>'test 2 DistributionMedia',"DistributionSize"=>'test 2 DistributionSize',"DistributionFormat"=>'test 2 DistributionFormat', "Fees"=>test_fee},
             {"DistributionMedia"=>'test 1 DistributionMedia',"DistributionSize"=>'test 1 DistributionSize',"DistributionFormat"=>'test 1 DistributionFormat', "Fees"=>test_fee}
         ]
         assign(:draft, build(:draft, draft: draft_json))
+        #output_schema_validation draft_json
         render :template => template_path, :locals=>{draft: draft_json}
       end
 
@@ -55,13 +55,8 @@ describe template_path, type: :view do
         end
       end
 
-      it 'handles bad currency values' do # TODO: Does NOT properly handle '123.456'
-        expect(rendered).to have_content('bad fee amount')
-      end
-
       it 'handles good currency values' do
-        value = number_to_currency(test_fee.to_f)
-        expect(rendered).to have_content(number_to_currency(test_fee.to_f))
+        expect(rendered).to have_content(number_to_currency(test_fee))
       end
     end
 
