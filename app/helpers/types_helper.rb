dee0c1c48512fbfd4c6ef2c5a0de0157fe8e507d
Kenneth Shae Murphy <Kenneth.S.Murphy@NASA.gov>
2015-09-01 09:56:44 -0400

    MMT-297: Incorporated PR feedback.

--- app/helpers/types_helper.rb
+++ app/helpers/types_helper.rb
@@ -1,7 +1,7 @@
 module TypesHelper
-  PointsType = [
-      { name: 'Longitude' },
-      { name: 'Latitude' }
+  PointsType = [ # Referenced by BoundaryType, GeometryType, LinesType, etc
+    { name: 'Longitude' },
+    { name: 'Latitude' }
   ]
   AccessConstraintsType = [
     { name: 'Description' },
@@ -15,24 +15,22 @@ module TypesHelper
     { name: 'Country' }
   ]
   AltitudeSystemDefinitionType = [
-      { name: 'DatumName' },
-      { name: 'DistanceUnits' },
-      { name: 'EncodingMethod' },
-      { name: 'Resolution', options: [:sub_type] }
+    { name: 'DatumName' },
+    { name: 'DistanceUnits' },
+    { name: 'EncodingMethod' },
+    { name: 'Resolution', options: [:sub_type] }
   ]
   BoundingRectanglesType = [
-      { name: 'CenterPoint', options: [:sub_type] },
-      { name: 'WestBoundingCoordinate' },
-      { name: 'NorthBoundingCoordinate' },
-      { name: 'EastBoundingCoordinate' },
-      { name: 'SouthBoundingCoordinate' }
+    { name: 'CenterPoint', options: [:sub_type] },
+    { name: 'WestBoundingCoordinate' },
+    { name: 'NorthBoundingCoordinate' },
+    { name: 'EastBoundingCoordinate' },
+    { name: 'SouthBoundingCoordinate' }
   ]
   BoundaryType = [
-      { name: 'Points', options: [:sub_type]  }
+    { name: 'Points', options: [:sub_type]  }
   ]
-  BoundariesType = [
-      { name: 'Points', options: [:sub_type]  }
-  ]
+  BoundariesType = BoundaryType
   CenterPointType = PointsType
   CharacteristicsType = [
     { name: 'Name' },
@@ -58,8 +56,8 @@ module TypesHelper
     { name: 'Subtype' }
   ]
   Coordinate1Type = [
-      { name: 'MinimumValue' },
-      { name: 'MaximumValue' }
+    { name: 'MinimumValue' },
+    { name: 'MaximumValue' }
   ]
   Coordinate2Type = Coordinate1Type
   DateType = [
@@ -78,44 +76,44 @@ module TypesHelper
     { name: 'Authority' }
   ]
   ExclusionZoneType = [
-      { name: 'Boundaries', options: [:sub_type] }
+    { name: 'Boundaries', options: [:sub_type] }
   ]
   FileSizeType = [
     { name: 'Size' },
     { name: 'Unit' }
   ]
   GeodeticModelType = [
-      { name: 'HorizontalDatumName' },
-      { name: 'EllipsoidName' },
-      { name: 'SemiMajorAxis' },
-      { name: 'DenominatorOfFlatteningRatio' }
+    { name: 'HorizontalDatumName' },
+    { name: 'EllipsoidName' },
+    { name: 'SemiMajorAxis' },
+    { name: 'DenominatorOfFlatteningRatio' }
   ]
   GeographicCoordinateSystemType = [
-      { name: 'GeographicCoordinateUnits' },
-      { name: 'LatitudeResolution' },
-      { name: 'LongitudeResolution' }
+    { name: 'GeographicCoordinateUnits' },
+    { name: 'LatitudeResolution' },
+    { name: 'LongitudeResolution' }
   ]
   GeometryType = [
-      { name: 'CoordinateSystem', options: [:select_type], select_type: 'CoordinateSystemOptions'  },
-      { name: 'Points', options: [:sub_type] },
-      { name: 'BoundingRectangles', options: [:sub_type] },
-      { name: 'GPolygons', options: [:sub_type] },
-      { name: 'Lines', options: [:sub_type] }
+    { name: 'CoordinateSystem', options: [:select_type], select_type: 'CoordinateSystemOptions'  },
+    { name: 'Points', options: [:sub_type] },
+    { name: 'BoundingRectangles', options: [:sub_type] },
+    { name: 'GPolygons', options: [:sub_type] },
+    { name: 'Lines', options: [:sub_type] }
   ]
   GPolygonsType = [
-      { name: 'CenterPoint', options: [:sub_type] },
-      { name: 'Boundary', options: [:sub_type] },
-      { name: 'ExclusionZone', options: [:sub_type] }
+    { name: 'CenterPoint', options: [:sub_type] },
+    { name: 'Boundary', options: [:sub_type] },
+    { name: 'ExclusionZone', options: [:sub_type] }
   ]
   HorizontalCoordinateSystemType = [
-      { name: 'GeodeticModel', options: [:sub_type] },
-      { name: 'GeographicCoordinateSystem', options: [:sub_type] },
-      { name: 'LocalCoordinateSystem', options: [:sub_type] }
-  ]
+    { name: 'GeodeticModel', options: [:sub_type] },
+    { name: 'GeographicCoordinateSystem', options: [:sub_type] },
+    { name: 'LocalCoordinateSystem', options: [:sub_type] }
+]
   HorizontalSpatialDomainType = [
-      { name: 'ZoneIdentifier' },
-      { name: 'Geometry', options: [:sub_type] }
-  ]
+    { name: 'ZoneIdentifier' },
+    { name: 'Geometry', options: [:sub_type] }
+]
   InstrumentsType = [
     { name: 'ShortName' },
     { name: 'LongName' },
@@ -126,13 +124,13 @@ module TypesHelper
     { name: 'Sensors', options: [:sub_type] }
   ]
   LinesType = [
-      { name: 'CenterPoint', options: [:sub_type] },
-      { name: 'Points', options: [:sub_type] }
+    { name: 'CenterPoint', options: [:sub_type] },
+    { name: 'Points', options: [:sub_type] }
   ]
   LocalCoordinateSystemType = [
-      { name: 'GeoReferenceInformation' },
-      { name: 'Description' }
-  ]
+    { name: 'GeoReferenceInformation' },
+    { name: 'Description' }
+]
   MetadataAssociationsType = [
     { name: 'Type' },
     { name: 'Description' },
@@ -140,11 +138,11 @@ module TypesHelper
     { name: 'ProviderId' }
   ]
   OrbitParametersType = [
-      { name: 'SwathWidth' },
-      { name: 'Period' },
-      { name: 'InclinationAngle' },
-      { name: 'NumberOfOrbits' },
-      { name: 'StartCircularLatitude' }
+    { name: 'SwathWidth' },
+    { name: 'Period' },
+    { name: 'InclinationAngle' },
+    { name: 'NumberOfOrbits' },
+    { name: 'StartCircularLatitude' }
   ]
   OrganizationNameType = [
     { name: 'ShortName' },
@@ -229,7 +227,7 @@ module TypesHelper
   ]
   RelatedUrlsType = RelatedUrlType
   ResolutionType = [
-      { name: 'Resolutions', options: [:array_field]  }
+    { name: 'Resolutions', options: [:array_field]  }
   ]
   ResourceCitationType = [
     { name: 'Version' },
@@ -257,36 +255,36 @@ module TypesHelper
     { name: 'Characteristics', options: [:sub_type] }
   ]
   SpatialExtentType = [
-      { name: 'SpatialCoverageType', options: [:select_type], select_type: 'SpatialCoverageTypeOptions' },
-      { name: 'HorizontalSpatialDomain', options: [:sub_type] },
-      { name: 'VerticalSpatialDomains', options: [:sub_type] },
-      { name: 'OrbitParameters', options: [:sub_type] },
-      { name: 'GranuleSpatialRepresentation', options: [:select_type], select_type: 'GranuleSpatialRepresentationOptions' }
+    { name: 'SpatialCoverageType', options: [:select_type], select_type: 'SpatialCoverageTypeOptions' },
+    { name: 'HorizontalSpatialDomain', options: [:sub_type] },
+    { name: 'VerticalSpatialDomains', options: [:sub_type] },
+    { name: 'OrbitParameters', options: [:sub_type] },
+    { name: 'GranuleSpatialRepresentation', options: [:select_type], select_type: 'GranuleSpatialRepresentationOptions' }
   ]
   SpatialInformationType = [
-      { name: 'SpatialCoverageType', options: [:select_type], select_type: 'SpatialCoverageTypeOptions'  },
-      { name: 'HorizontalCoordinateSystem', options: [:sub_type] },
-      { name: 'VerticalCoordinateSystem', options: [:sub_type] }
+    { name: 'SpatialCoverageType', options: [:select_type], select_type: 'SpatialCoverageTypeOptions'  },
+    { name: 'HorizontalCoordinateSystem', options: [:sub_type] },
+    { name: 'VerticalCoordinateSystem', options: [:sub_type] }
   ]
   TemporalExtentsType = [
-      { name: 'PrecisionOfSeconds' },
-      { name: 'EndsAtPresentFlag' },
-      { name: 'RangeDateTimes', options: [:sub_type] },
-      { name: 'SingleDateTimes', options: [:array_field] },
-      { name: 'PeriodicDateTimes', options: [:sub_type] }
+    { name: 'PrecisionOfSeconds' },
+    { name: 'EndsAtPresentFlag' },
+    { name: 'RangeDateTimes', options: [:sub_type] },
+    { name: 'SingleDateTimes', options: [:array_field] },
+    { name: 'PeriodicDateTimes', options: [:sub_type] }
   ]
   TilingIdentificationSystemType = [
-      { name: 'TilingIdentificationSystemName' },
-      { name: 'Coordinate1', options: [:sub_type]  },
-      { name: 'Coordinate2', options: [:sub_type]  }
+    { name: 'TilingIdentificationSystemName' },
+    { name: 'Coordinate1', options: [:sub_type]  },
+    { name: 'Coordinate2', options: [:sub_type]  }
   ]
   VerticalCoordinateSystemType = [
-      { name: 'AltitudeSystemDefinition', options: [:sub_type] },
-      { name: 'DepthSystemDefinition', options: [:sub_type] }
+    { name: 'AltitudeSystemDefinition', options: [:sub_type] },
+    { name: 'DepthSystemDefinition', options: [:sub_type] }
   ]
   VerticalSpatialDomainsType = [
-      { name: 'Type' },
-      { name: 'Value' }
+    { name: 'Type' },
+    { name: 'Value' }
   ]
 
 end
