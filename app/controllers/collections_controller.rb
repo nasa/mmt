513cf5d597245e40126693c584ba459dee932cbf
Matthew Crouch <matthew@element84.com>
2015-09-24 10:29:55 -0400

    MMT-89 MMT-90 revert collections to previous revisions

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,5 +1,5 @@
 class CollectionsController < ApplicationController
-  before_action :set_collection
+  before_action :set_collection, except: [:revert]
 
   def show
   end
@@ -22,9 +22,58 @@ class CollectionsController < ApplicationController
   def revisions
   end
 
+  def revert
+    concept_id = params[:id]
+    revision_id = params[:revision_id]
+
+    # get native_id
+    revisions = cmr_client.get_collections({ concept_id: concept_id, all_revisions: true }, token).body['items']
+    native_id = revisions.first['meta']['native-id']
+    provider_id = revisions.first['meta']['provider-id']
+    latest_revision_id = revisions.first['meta']['revision-id']
+
+    # Retrieve previous revision
+    metadata = cmr_client.get_concept(concept_id, revision_id, token)
+
+    # Ingest revision
+    ingested = cmr_client.ingest_collection(metadata, provider_id, native_id, token)
+
+    unless ingested.success?
+      Rails.logger.error("Ingest Metadata Error: #{ingested.inspect}")
+    end
+
+    redirect_to collection_revisions_path(revision_id: latest_revision_id.to_i + 1)
+  end
+
   private
 
   def set_collection
+    concept_id = params[:id]
+    revision_id = params[:revision_id]
+
+    get_revisions
+
+    latest = @revisions.first
+
+    if latest
+      @native_id = latest['meta']['native-id']
+      concept_format = latest['meta']['format']
+
+      # retrieve native metadata
+      metadata = cmr_client.get_concept(concept_id, revision_id, token)
+
+      # translate to umm-json metadata
+      @collection = cmr_client.translate_collection(metadata, concept_format, 'application/umm+json').body
+    else
+      # concept wasn't found, CMR might be a little slow
+      # Take the user to a blank page with a message the collection doesn't exist yet,
+      # eventually auto refreshing the page would be cool
+      @collection = {}
+      @error = 'Your collection is being published. This page will show your published collection once it is ready. Please check back soon.'
+    end
+  end
+
+  def get_revisions
     concept_id = params[:id]
     @revision_id = params[:revision_id]
 
@@ -36,23 +85,6 @@ class CollectionsController < ApplicationController
       break if latest && latest['meta']['revision-id'] >= @revision_id.to_i
       attempts += 1
       sleep 2
-    end
-
-    if latest
-      @native_id = latest['meta']['native-id']
-      concept_format = latest['meta']['format']
-
-      # retrieve native metadata
-      metadata = cmr_client.get_concept(concept_id, @revision_id, token)
-
-      # translate to umm-json metadata
-      @collection = cmr_client.translate_collection(metadata, concept_format, 'application/umm+json').body
-    else
-      # concept wasn't found, CMR might be a little slow
-      # Take the user to a blank page with a message the collection doesn't exist yet,
-      # eventually auto refreshing the page would be cool
-      @collection = {}
-      @error = 'Your collection is being published. This page will show your published collection once it is ready. Please check back soon.'
     end
   end
 end
