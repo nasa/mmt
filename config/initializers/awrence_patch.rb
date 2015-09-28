56d7b63f38563be7501b89525703c3be36b9f773
Matthew Crouch <matthew@element84.com>
2015-09-28 13:08:07 -0400

    MMT-306 added ISOTopicCategories

--- config/initializers/awrence_patch.rb
+++ config/initializers/awrence_patch.rb
@@ -3,17 +3,23 @@
 class Hash
   private
 
-  UPCASE_WORDS = ['urls', 'isbn', 'doi']
+  UPCASE_WORDS = %w(
+    urls
+    isbn
+    doi
+    iso_topic_categories
+  )
 
   def camelize(snake_word, first_upper = true)
     # Here is the patch
     if UPCASE_WORDS.include?(snake_word)
-      return 'URLs' if snake_word == "urls"
+      return 'URLs' if snake_word == 'urls'
+      return 'ISOTopicCategories' if snake_word == 'iso_topic_categories'
       return snake_word.upcase
     end
 
     if first_upper
-      snake_word.to_s.gsub(/\/(.?)/) { "::" + $1.upcase }.gsub(/(^|_)(.)/) { $2.upcase }
+      snake_word.to_s.gsub(/\/(.?)/) { '::' + $1.upcase }.gsub(/(^|_)(.)/) { $2.upcase }
     else
       snake_word.chars.first + camelize(snake_word)[1..-1]
     end
