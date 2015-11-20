ad953478a25a6861e02266903f4213c2cb47a824
Matthew Crouch <matthew@element84.com>
2015-11-20 12:51:27 -0500

    MMT-119 fix tests

--- spec/features/drafts/forms/global_spatial_spec.rb
+++ spec/features/drafts/forms/global_spatial_spec.rb
@@ -17,6 +17,7 @@ describe 'Global spatial coverage', js: true do
 
       choose 'draft_spatial_extent_spatial_coverage_type_HORIZONTAL'
       choose 'draft_spatial_extent_horizontal_spatial_domain_geometry_geometry-bounding-rectangles'
+      select 'Cartesian', from: 'Granule Spatial Representation'
 
       click_on 'Apply global spatial coverage'
     end
@@ -45,9 +46,10 @@ describe 'Global spatial coverage', js: true do
         click_on 'Add another Bounding Rectangle'
         find('#draft_spatial_extent_horizontal_spatial_domain_geometry_bounding_rectangles_0 > .accordion-header').click
 
-        within '.bounding-rectangles-fields .multiple-item-1' do
-          click_on 'Apply global spatial coverage'
-        end
+        # Intermittent test failure hopefully solved by clicking the
+        # link with JS
+        script = "$('.bounding-rectangles-fields .multiple-item-1 .global-coverage').trigger('click')"
+        page.execute_script script
       end
 
       it 'only fills in the fields for the correct rectangle' do
