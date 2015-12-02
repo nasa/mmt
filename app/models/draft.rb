4e558570dcefaeb7eedbcb19668bd5f060c7a2f6
Matthew Crouch <matthew@element84.com>
2015-12-02 10:51:25 -0500

    MMT-349 fix editing a record when collection comes back without data

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -63,13 +63,13 @@ class Draft < ActiveRecord::Base
   end
 
   def self.create_from_collection(collection, user, native_id)
-    new_entry_title = collection['EntryTitle'].empty? ? nil : collection['EntryTitle']
+    new_entry_title = (collection['EntryTitle'] && collection['EntryTitle'].empty?) ? nil : collection['EntryTitle']
 
     if native_id
       # Edited record
       draft = Draft.find_or_create_by(native_id: native_id)
       draft.entry_title = new_entry_title
-      draft.entry_id = collection['EntryId'].empty? ? nil : collection['EntryId']
+      draft.entry_id = (collection['EntryId'] && collection['EntryId'].empty?) ? nil : collection['EntryId']
     else
       # Cloned record
       draft = Draft.create
