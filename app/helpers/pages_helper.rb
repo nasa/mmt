566b12e3600698e1a1d3944f91fd253e6562633d
Charles Huang <charles.huang@nasa.gov>
2016-01-26 20:50:04 -0500

    MMT-395. Add helper method for display_entry_id and use in page headers.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -10,4 +10,17 @@ module PagesHelper
 
     "(#{start_date} - #{end_date})"
   end
+
+  def display_entry_id(draft_or_collection)
+    # empty collection ShortName should display 'New Collection'
+    if draft_or_collection.is_a?(Draft)
+      entry_id = draft_or_collection.display_short_name
+      entry_id += ' ' + draft_or_collection.draft['Version'] unless draft_or_collection.draft['Version'].nil?
+    else
+      entry_id = draft_or_collection['ShortName'] || 'New Collection'
+      entry_id += ' ' + draft_or_collection['Version'] unless draft_or_collection['Version'].nil?
+    end
+
+    entry_id
+  end
 end
