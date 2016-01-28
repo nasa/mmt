46e7866a5dda2226e31c4e5b69463ab70f661c93
Charles Huang <charles.huang@nasa.gov>
2016-01-28 13:05:32 -0500

    MMT-395. Align Entry Id concatenation with CMR.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -15,7 +15,7 @@ module PagesHelper
     blank_short_name = type == 'draft' ? '<Blank Short Name>' : 'New Collection'
     short_name = metadata['ShortName'] || blank_short_name
 
-    version = metadata['Version'].nil? ? '' : " #{metadata['Version']}"
+    version = metadata['Version'].nil? ? '' : "_#{metadata['Version']}"
 
     entry_id = short_name + version
     entry_id
