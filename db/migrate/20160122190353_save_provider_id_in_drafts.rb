eea17ae5a2032b74698aa74e4a70f6653eca0c90
Matthew Crouch <matthew@element84.com>
2016-02-01 14:42:08 -0500

    fix migration to assign provider ids to drafts

--- db/migrate/20160122190353_save_provider_id_in_drafts.rb
+++ db/migrate/20160122190353_save_provider_id_in_drafts.rb
@@ -2,8 +2,12 @@ class SaveProviderIdInDrafts < ActiveRecord::Migration
   def change
     # Set current drafts providers to their user's current provider
     Draft.where(provider_id: nil).each do |draft|
-      draft.provider_id = draft.user.provider_id
-      draft.save
+      if draft.user_id.nil?
+        draft.destroy
+      else
+        draft.provider_id = draft.user.provider_id
+        draft.save
+      end
     end
   end
 end
