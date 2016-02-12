3b5a1a19cefa9911e1820f7deb999d60aa367921
Charles Huang <charles.huang@nasa.gov>
2016-02-12 11:17:43 -0500

    MMT-389. edit Full Metadata Search form. make search controller work on collections keyword search with search form edits.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -14,30 +14,41 @@ class SearchController < ApplicationController
     # Did the search come from quick_find or full_search
     if search_type == 'quick_find'
       # If search came from quick find, only use the quick find input
-      params.delete('search_term_type')
       params.delete('search_term')
+
       @query = {}
-      @query['keyword'] = params['keyword'] if params['keyword']
+      # use cmr keyword search for quick_find
+      @query['keyword'] = params['quick_find_keyword'] if params['quick_find_keyword']
       @query['record_state'] = 'published_records'
       @query['sort_key'] = params['sort_key'] if params['sort_key']
     elsif search_type == 'full_search'
       # If search came from full search, ignore whatever was in quick find
-      params.delete('keyword')
+      params.delete('quick_find_keyword')
+
       @query = params.clone
+
+      if params['record_state'] == 'published_records'
+        # if published collection, use cmr keyword search
+        @query['keyword'] = @query.delete('search_term')
+      elsif params['record_state'] == 'draft_records'
+        @query['draft_search_term'] = @query.delete('search_term')
+      end
+
       @query.delete('provider_id') if @query['provider_id'].blank?
 
       # Handle search term field with selector
       # if no search term exists, reset the type
-      params.delete('search_term_type') if params['search_term'].empty?
-      case params['search_term_type']
-      when 'short_name'
-        @query['short_name'] = params['search_term']
-      when 'entry_title'
-        @query['entry_title'] = "*#{params['search_term']}*"
-        @query['options[entry_title][pattern]'] = true
-      when 'concept_id'
-        @query['concept_id'] = params['search_term']
-      end
+      # TODO need for drafts search? OR DELETE when search works
+      # params.delete('search_term_type') if params['search_term'].empty?
+      # case params['search_term_type']
+      # when 'short_name'
+      #   @query['short_name'] = params['search_term']
+      # when 'entry_title'
+      #   @query['entry_title'] = "*#{params['search_term']}*"
+      #   @query['options[entry_title][pattern]'] = true
+      # when 'concept_id'
+      #   @query['concept_id'] = params['search_term']
+      # end
     end
 
     @query['page_num'] = page
@@ -63,8 +74,6 @@ class SearchController < ApplicationController
         get_published(query)
       when 'draft_records'
         get_drafts(query)
-      when 'published_and_draft_records'
-        get_published_and_drafts(query)
       else
         [[], [], 0]
       end
@@ -90,8 +99,8 @@ class SearchController < ApplicationController
   def get_drafts(query)
     query.delete('page_num')
     query.delete('page_size')
-    query.delete('_')
-    query.delete('options[entry_title][pattern]')
+    query.delete('_') # what?
+    query.delete('options[entry_title][pattern]') # what is this?
     query['entry_title'] = query['entry_title'][1..-2] if query['entry_title']
 
     drafts = Draft.where(query.permit!) # TODO Modify the query to use offset and RESULTS_PER_PAGE to support pagination
@@ -113,14 +122,14 @@ class SearchController < ApplicationController
     [drafts, [], drafts.size]
   end
 
-  def get_published_and_drafts(query)
-    published_collections, published_errors, published_hits = get_published(query.clone)
-    drafts, _draft_errors, draft_hits = get_drafts(query)
-
-    collections = published_collections.concat(drafts).sort do |x, y|
-      x['umm']['entry-title'] <=> y['umm']['entry-title']
-    end
-
-    [collections, published_errors, published_hits + draft_hits]
-  end
+  # def get_published_and_drafts(query)
+  #   published_collections, published_errors, published_hits = get_published(query.clone)
+  #   drafts, _draft_errors, draft_hits = get_drafts(query)
+  #
+  #   collections = published_collections.concat(drafts).sort do |x, y|
+  #     x['umm']['entry-title'] <=> y['umm']['entry-title']
+  #   end
+  #
+  #   [collections, published_errors, published_hits + draft_hits]
+  # end
 end
