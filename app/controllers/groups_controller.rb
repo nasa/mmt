aa1e3a7054c16e830180f3b97d4386d86e254654
Charles Huang <charles.huang@nasa.gov>
2016-02-25 15:13:28 -0500

    MMT-150. refactor groups controller. minor edits to groups views breadcrumbs and new group form helper.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,22 +19,13 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = params.clone
-    if params[:name].empty? && params[:description].empty?
-      flash[:error] = 'Group Name and Description are required.'
-      render :new
-    elsif params[:name].empty?
-      flash[:error] = 'Group Name is required.'
-      render :new
-    elsif params[:description].empty?
-      flash[:error] = 'Group Description is required.'
+    @group = { :name => params.delete(:name),
+               :description => params.delete(:description),
+               'provider-id' => @current_user.provider_id }
+    if @group[:name].empty? || @group[:description].empty?
+      flash[:error] = group_validation_error(@group)
       render :new
     else
-      @group = {}
-      @group['name'] = params[:name]
-      @group['provider-id'] = @current_user.provider_id
-      @group['description'] = params[:description]
-
       group_creation = cmr_client.create_group(@group.to_json, token)
 
       if group_creation.success?
@@ -53,5 +44,16 @@ class GroupsController < ApplicationController
 
   def groups_enabled?
     redirect_to dashboard_path unless Rails.configuration.groups_enabled
+  end
+
+  def group_validation_error(group)
+    if group[:name].empty? && group[:description].empty?
+      error = 'Group Name and Description are required.'
+    elsif group[:name].empty?
+      error = 'Group Name is required.'
+    elsif group[:description].empty?
+      error = 'Group Description is required.'
+    end
+    error
   end
 end
