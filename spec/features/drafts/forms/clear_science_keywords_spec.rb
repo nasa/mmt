28bf4feb9cf9f7156e921b7d7289283918cd4733
Charles Huang <charles.huang@nasa.gov>
2016-03-30 20:46:34 -0400

    MMT-502. Attempt to pull out clearing spatial and science keywords tests into their own files.

---
+++ spec/features/drafts/forms/clear_science_keywords_spec.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+
+describe 'Clearing saved science keywords', js: true do
+  before do
+    login
+    draft = create(:draft, user: User.where(urs_uid: 'testuser').first)
+    visit draft_path(draft)
+  end
+
+  context 'when selecting science keywords and saving the form' do
+    before do
+      within '.metadata' do
+        click_on 'Descriptive Keywords'
+      end
+
+      open_accordions
+
+      # add_science_keywords
+      choose_keyword 'EARTH SCIENCE SERVICES'
+      choose_keyword 'DATA ANALYSIS AND VISUALIZATION'
+      choose_keyword 'GEOGRAPHIC INFORMATION SYSTEMS'
+      click_on 'Add Keyword'
+
+      within '.nav-top' do
+        click_on 'Save & Done'
+      end
+
+      open_accordions
+    end
+
+    it 'displays a confirmation message' do
+      expect(page).to have_content('Draft was successfully updated')
+    end
+
+    context 'when returning to the form' do
+      before do
+        within '.metadata' do
+          click_on 'Descriptive Keywords'
+        end
+
+        open_accordions
+      end
+
+      it 'populates the form with the values' do
+        expect(page).to have_content('EARTH SCIENCE SERVICES > DATA ANALYSIS AND VISUALIZATION > GEOGRAPHIC INFORMATION SYSTEMS')
+      end
+
+      context 'when removing the science keywords and saving the form' do
+        before do
+          within '.selected-science-keywords' do
+            find('.remove').click
+            # remove_script = "$('.selected-science-keywords').find('.remove').click()"
+            # page.execute_script(remove_script)
+            sleep 1
+          end
+
+          within '.nav-top' do
+            click_on 'Save & Done'
+          end
+        end
+
+        it 'displays a confirmation message' do
+          expect(page).to have_content('Draft was successfully updated')
+        end
+
+        context 'when returning to the form' do
+          before do
+            within '.metadata' do
+              click_on 'Descriptive Keywords'
+            end
+
+            open_accordions
+          end
+
+          it 'does not display the removed science keywords' do
+            expect(page).to have_no_content('EARTH SCIENCE SERVICES > DATA ANALYSIS AND VISUALIZATION > GEOGRAPHIC INFORMATION SYSTEMS')
+          end
+        end
+      end
+    end
+
+  end
+
+end
