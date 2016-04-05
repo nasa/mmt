af174aea3ffdce2789fdfcf5d0dc164976be5823
Charles Huang <charles.huang@nasa.gov>
2016-04-05 12:55:46 -0400

    MMT-468. Fix for problems with concept_id in search_collections_results_spec

--- spec/features/search/search_collections_result_spec.rb
+++ spec/features/search/search_collections_result_spec.rb
@@ -3,14 +3,25 @@
 require 'rails_helper'
 
 describe 'Search published results', js: true do
-  short_name = 'CIESIN_SEDAC_EPI_2012'
-  version = '2012.00'
-  entry_title = '2012 Environmental Performance Index and Pilot Trend Environmental Performance Index'
+  short_name = 'CIESIN_SEDAC_EPI_2008'
+  version = '2008.00'
+  entry_title = '2008 Environmental Performance Index (EPI)'
   provider = 'SEDAC'
-  concept_id = 'C1200000008-SEDAC'
+
+  # test cmr seems to have inconsistent concept_id for these collections,
+  # so grab the concept_id in use for the tests
+  let(:concept_id) { page.current_path.delete('/collections/') }
 
   before :each do
     login
+
+    fill_in 'Quick Find', with: short_name
+    click_on 'Find'
+    click_link short_name
+
+    concept_id
+
+    visit '/dashboard'
   end
 
   context 'when performing a collection search by short name with quick find' do
@@ -42,7 +53,7 @@ describe 'Search published results', js: true do
     end
 
     it 'displays collection results' do
-      expect(page).to have_search_query(2, "Keyword: #{entry_title[0..17]}", 'Record State: Published Records')
+      expect(page).to have_search_query(1, "Keyword: #{entry_title[0..17]}", 'Record State: Published Records')
     end
 
     it 'displays expected Short Name, Entry Title and Last Modified values' do
@@ -54,7 +65,7 @@ describe 'Search published results', js: true do
     end
   end
 
-  context 'when performing a collection search by short name with concept id' do
+  context 'when performing a quick find collection search with concept id' do
     before do
       fill_in 'Quick Find', with: concept_id
       click_on 'Find'
@@ -73,7 +84,7 @@ describe 'Search published results', js: true do
     end
   end
 
-  context 'when performing a collection search by short name with provider' do
+  context 'when performing a quick find collection search with provider' do
     before do
       fill_in 'Quick Find', with: provider
       click_on 'Find'
@@ -92,7 +103,7 @@ describe 'Search published results', js: true do
     end
   end
 
-  context 'when searching by short name' do
+  context 'when performing a full metadata search by short name' do
     before do
       full_search(keyword: short_name)
     end
@@ -142,7 +153,7 @@ describe 'Search published results', js: true do
     end
   end
 
-  context 'when searching by entry title' do
+  context 'when conducting full metadata search on collections by entry title' do
     before do
       full_search(keyword: entry_title, record_state: 'Collections')
     end
@@ -174,7 +185,7 @@ describe 'Search published results', js: true do
     end
   end
 
-  context 'when searching by partial entry title' do
+  context 'when conduction full metadata search by partial entry title' do
     before do
       click_on 'Full Metadata Record Search'
       fill_in 'keyword', with: entry_title[5..17]
@@ -242,7 +253,7 @@ describe 'Search published results', js: true do
     end
   end
 
-  context 'when searching by CMR Concept Id' do
+  context 'when conducting full metadata search by CMR Concept Id' do
     before do
       click_on 'Full Metadata Record Search'
       fill_in 'keyword', with: concept_id
@@ -276,7 +287,7 @@ describe 'Search published results', js: true do
     end
   end
 
-  context 'when performing a search that has no results' do
+  context 'when performing a quick find search that has no results' do
     before do
       fill_in 'Quick Find', with: 'NO HITS'
       click_on 'Find'
