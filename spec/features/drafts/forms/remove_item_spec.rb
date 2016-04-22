b8cc06317a3bb6220858ae5a4d7dd2f07d809aee
Charles Huang <charles.huang@nasa.gov>
2016-04-22 17:33:06 -0400

    MMT-515. simplify test for remove button on multiple items

--- spec/features/drafts/forms/remove_item_spec.rb
+++ spec/features/drafts/forms/remove_item_spec.rb
@@ -14,26 +14,26 @@ describe 'Remove item link behavior', js: true do
       end
     end
 
+    it 'does not display the remove button for a single item' do
+      within '.multiple.personnel > .multiple-item-0' do
+        within '.multiple.contacts' do
+          expect(page).to have_no_css('a.remove')
+        end
+      end
+    end
+
     context 'when adding additional items to the form' do
       before do
         click_on 'Add another Contact Method'
-        click_on 'Add another URL'
-        click_on 'Add another Related URL'
 
         open_accordions
       end
 
-      it 'adds the additional items to the form' do
+      it 'adds the additional item to the form' do
         within '.multiple.personnel > .multiple-item-0' do
           within '.multiple.contacts' do
             expect(page).to have_css('.multiple-item', count: 2)
           end
-
-          within '.multiple.related-urls > .multiple-item-0' do
-            expect(page).to have_css('input.url', count: 2)
-          end
-
-          expect(page).to have_css('.multiple.related-urls > .multiple-item', count: 2)
         end
       end
 
@@ -43,46 +43,14 @@ describe 'Remove item link behavior', js: true do
             within '.multiple.contacts' do
               find('a.remove', match: :first).click
             end
-
-            within '.multiple.related-urls > .multiple-item-0 .simple-multiple.urls' do
-              find('a.remove', match: :first).click
-            end
-
-            within '.multiple.related-urls' do
-              within '.multiple-item-1 > .eui-accordion__header' do
-                find('a.remove').click
-              end
-            end
           end
         end
 
-        it 'removes the items from the form' do
+        it 'removes the item and remove button from the form' do
           within '.multiple.personnel > .multiple-item-0' do
             within '.multiple.contacts' do
               expect(page).to have_css('.multiple-item', count: 1)
-            end
-
-            within '.multiple.related-urls > .multiple-item-0' do
-              expect(page).to have_css('input.url', count: 1)
-            end
-
-            expect(page).to have_css('.multiple.related-urls > .multiple-item', count: 1)
-          end
-        end
-
-        it 'does not display remove links for the remaining multipe items' do
-          within '.multiple.personnel > .multiple-item-0' do
-            within '.multiple.contacts' do
               expect(page).to have_no_css('a.remove')
-            end
-
-            within '.multiple.related-urls' do
-              within '.multiple-item-0 .simple-multiple.urls' do
-                expect(page).to have_no_css('a.remove')
-              end
-              within '.multiple-item-0 > .eui-accordion__header' do
-                expect(page).to have_no_css('a.remove')
-              end
             end
           end
         end
