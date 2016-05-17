f3429c64160c06e548d80dd7527bbd5f95cbd2f5
Charles Huang <charles.huang@nasa.gov>
2016-05-17 11:20:27 -0400

    MMT-559: revert tests back to use Done and return to form

--- spec/features/drafts/forms/acquisition_information_form_spec.rb
+++ spec/features/drafts/forms/acquisition_information_form_spec.rb
@@ -37,7 +37,7 @@ describe 'Acquisition information form', js: true do
       end
 
       within '.nav-top' do
-        click_on 'Save'
+        click_on 'Done'
       end
       # output_schema_validation Draft.first.draft
       open_accordions
@@ -47,153 +47,163 @@ describe 'Acquisition information form', js: true do
       expect(page).to have_content('Draft was successfully updated')
     end
 
-    it 'populates the form with the values' do
-      within '.multiple.platforms' do
-        within first('.multiple-item-0') do
-          expect(page).to have_field('Type', with: 'Aircraft')
-          expect(page).to have_field('draft_platforms_0_short_name', with: 'Platform short name')
-          expect(page).to have_field('draft_platforms_0_long_name', with: 'Platform long name')
-
-          # Characteristics
-          within first('.multiple.characteristics') do
-            within '.multiple-item-0' do
-              expect(page).to have_field('Name', with: 'Characteristics name')
-              expect(page).to have_field('Description', with: 'Characteristics description')
-              expect(page).to have_field('Value', with: 'Characteristics value')
-              expect(page).to have_field('Unit', with: 'unit')
-              expect(page).to have_field('Data Type', with: 'Characteristics data type')
-            end
-            within '.multiple-item-1' do
-              expect(page).to have_field('Name', with: 'Characteristics name 1')
-              expect(page).to have_field('Description', with: 'Characteristics description 1')
-              expect(page).to have_field('Value', with: 'Characteristics value 1')
-              expect(page).to have_field('Unit', with: 'unit 1')
-              expect(page).to have_field('Data Type', with: 'Characteristics data type 1')
-            end
-          end
-        end
-        # Instruments
-        within first('.multiple.instruments') do
-          expect(page).to have_field('draft_platforms_0_instruments_0_short_name', with: 'Instrument short name')
-          expect(page).to have_field('draft_platforms_0_instruments_0_long_name', with: 'Instrument long name')
-          expect(page).to have_field('draft_platforms_0_instruments_0_technique', with: 'Instrument technique')
-          expect(page).to have_field('Number Of Sensors', with: 2468)
-          expect(page).to have_selector('input.operational-mode[value="Instrument mode 1"]')
-          expect(page).to have_selector('input.operational-mode[value="Instrument mode 2"]')
-          # Characteristics
-          within first('.multiple.characteristics') do
-            within '.multiple-item-0' do
-              expect(page).to have_field('Name', with: 'Characteristics name')
-              expect(page).to have_field('Description', with: 'Characteristics description')
-              expect(page).to have_field('Value', with: 'Characteristics value')
-              expect(page).to have_field('Unit', with: 'unit')
-              expect(page).to have_field('Data Type', with: 'Characteristics data type')
-            end
-            within '.multiple-item-1' do
-              expect(page).to have_field('Name', with: 'Characteristics name 1')
-              expect(page).to have_field('Description', with: 'Characteristics description 1')
-              expect(page).to have_field('Value', with: 'Characteristics value 1')
-              expect(page).to have_field('Unit', with: 'unit 1')
-              expect(page).to have_field('Data Type', with: 'Characteristics data type 1')
-            end
-          end
-          # Sensors
-          within first('.multiple.sensors') do
-            expect(page).to have_field('draft_platforms_0_instruments_0_sensors_0_short_name', with: 'Sensor short name')
-            expect(page).to have_field('draft_platforms_0_instruments_0_sensors_0_long_name', with: 'Sensor long name')
-            expect(page).to have_field('draft_platforms_0_instruments_0_sensors_0_technique', with: 'Sensor technique')
-            # Characteristics
-            within first('.multiple.characteristics') do
-              within '.multiple-item-0' do
-                expect(page).to have_field('Name', with: 'Characteristics name')
-                expect(page).to have_field('Description', with: 'Characteristics description')
-                expect(page).to have_field('Value', with: 'Characteristics value')
-                expect(page).to have_field('Unit', with: 'unit')
-                expect(page).to have_field('Data Type', with: 'Characteristics data type')
-              end
-              within '.multiple-item-1' do
-                expect(page).to have_field('Name', with: 'Characteristics name 1')
-                expect(page).to have_field('Description', with: 'Characteristics description 1')
-                expect(page).to have_field('Value', with: 'Characteristics value 1')
-                expect(page).to have_field('Unit', with: 'unit 1')
-                expect(page).to have_field('Data Type', with: 'Characteristics data type 1')
-              end
-            end
-
-            # Sensor 2
-            expect(page).to have_field('draft_platforms_0_instruments_0_sensors_1_short_name', with: 'Sensor short name 1')
-          end
-
-          # Instrument 2
-          expect(page).to have_field('draft_platforms_0_instruments_1_short_name', with: 'Instrument short name 1')
+    context 'when returning to the form' do
+      before do
+        within '.metadata' do
+          click_on 'Acquisition Information'
         end
 
-        # Platform 2
-        expect(page).to have_field('draft_platforms_1_short_name', with: 'Platform short name 1')
-        # Instruments
-        within all('.multiple.instruments').last do
-          expect(page).to have_field('draft_platforms_1_instruments_0_short_name', with: 'Instrument short name')
-          expect(page).to have_field('draft_platforms_1_instruments_0_long_name', with: 'Instrument long name')
-          expect(page).to have_field('draft_platforms_1_instruments_0_technique', with: 'Instrument technique')
-          expect(page).to have_field('Number Of Sensors', with: 2468)
-          expect(page).to have_selector('input.operational-mode[value="Instrument mode 1"]')
-          expect(page).to have_selector('input.operational-mode[value="Instrument mode 2"]')
-          # Characteristics
-          within first('.multiple.characteristics') do
-            within '.multiple-item-0' do
-              expect(page).to have_field('Name', with: 'Characteristics name')
-              expect(page).to have_field('Description', with: 'Characteristics description')
-              expect(page).to have_field('Value', with: 'Characteristics value')
-              expect(page).to have_field('Unit', with: 'unit')
-              expect(page).to have_field('Data Type', with: 'Characteristics data type')
-            end
-            within '.multiple-item-1' do
-              expect(page).to have_field('Name', with: 'Characteristics name 1')
-              expect(page).to have_field('Description', with: 'Characteristics description 1')
-              expect(page).to have_field('Value', with: 'Characteristics value 1')
-              expect(page).to have_field('Unit', with: 'unit 1')
-              expect(page).to have_field('Data Type', with: 'Characteristics data type 1')
-            end
-          end
-          # Sensors
-          within first('.multiple.sensors') do
-            expect(page).to have_field('draft_platforms_1_instruments_0_sensors_0_short_name', with: 'Sensor short name')
-            expect(page).to have_field('draft_platforms_1_instruments_0_sensors_0_long_name', with: 'Sensor long name')
-            expect(page).to have_field('draft_platforms_1_instruments_0_sensors_0_technique', with: 'Sensor technique')
-            # Characteristics
-            within first('.multiple.characteristics') do
-              within '.multiple-item-0' do
-                expect(page).to have_field('Name', with: 'Characteristics name')
-                expect(page).to have_field('Description', with: 'Characteristics description')
-                expect(page).to have_field('Value', with: 'Characteristics value')
-                expect(page).to have_field('Unit', with: 'unit')
-                expect(page).to have_field('Data Type', with: 'Characteristics data type')
-              end
-              within '.multiple-item-1' do
-                expect(page).to have_field('Name', with: 'Characteristics name 1')
-                expect(page).to have_field('Description', with: 'Characteristics description 1')
-                expect(page).to have_field('Value', with: 'Characteristics value 1')
-                expect(page).to have_field('Unit', with: 'unit 1')
-                expect(page).to have_field('Data Type', with: 'Characteristics data type 1')
-              end
-            end
-
-            # Sensor 2
-            expect(page).to have_field('draft_platforms_1_instruments_0_sensors_1_short_name', with: 'Sensor short name 1')
-          end
-
-          # Instrument 2
-          expect(page).to have_field('draft_platforms_1_instruments_1_short_name', with: 'Instrument short name 1')
-        end
+        open_accordions
       end
 
-      within '.projects' do
-        expect(page).to have_field('Short Name', with: 'Project short name')
-        expect(page).to have_field('Long Name', with: 'Project long name')
-        expect(page).to have_selector('input.campaign[value="Project campaign 1"]')
-        expect(page).to have_selector('input.campaign[value="Project campaign 2"]')
-        expect(page).to have_field('Start Date', with: '2015-07-01T00:00:00Z')
-        expect(page).to have_field('End Date', with: '2015-08-01T00:00:00Z')
+      it 'populates the form with the values' do
+        within '.multiple.platforms' do
+          within first('.multiple-item-0') do
+            expect(page).to have_field('Type', with: 'Aircraft')
+            expect(page).to have_field('draft_platforms_0_short_name', with: 'Platform short name')
+            expect(page).to have_field('draft_platforms_0_long_name', with: 'Platform long name')
+
+            # Characteristics
+            within first('.multiple.characteristics') do
+              within '.multiple-item-0' do
+                expect(page).to have_field('Name', with: 'Characteristics name')
+                expect(page).to have_field('Description', with: 'Characteristics description')
+                expect(page).to have_field('Value', with: 'Characteristics value')
+                expect(page).to have_field('Unit', with: 'unit')
+                expect(page).to have_field('Data Type', with: 'Characteristics data type')
+              end
+              within '.multiple-item-1' do
+                expect(page).to have_field('Name', with: 'Characteristics name 1')
+                expect(page).to have_field('Description', with: 'Characteristics description 1')
+                expect(page).to have_field('Value', with: 'Characteristics value 1')
+                expect(page).to have_field('Unit', with: 'unit 1')
+                expect(page).to have_field('Data Type', with: 'Characteristics data type 1')
+              end
+            end
+          end
+          # Instruments
+          within first('.multiple.instruments') do
+            expect(page).to have_field('draft_platforms_0_instruments_0_short_name', with: 'Instrument short name')
+            expect(page).to have_field('draft_platforms_0_instruments_0_long_name', with: 'Instrument long name')
+            expect(page).to have_field('draft_platforms_0_instruments_0_technique', with: 'Instrument technique')
+            expect(page).to have_field('Number Of Sensors', with: 2468)
+            expect(page).to have_selector('input.operational-mode[value="Instrument mode 1"]')
+            expect(page).to have_selector('input.operational-mode[value="Instrument mode 2"]')
+            # Characteristics
+            within first('.multiple.characteristics') do
+              within '.multiple-item-0' do
+                expect(page).to have_field('Name', with: 'Characteristics name')
+                expect(page).to have_field('Description', with: 'Characteristics description')
+                expect(page).to have_field('Value', with: 'Characteristics value')
+                expect(page).to have_field('Unit', with: 'unit')
+                expect(page).to have_field('Data Type', with: 'Characteristics data type')
+              end
+              within '.multiple-item-1' do
+                expect(page).to have_field('Name', with: 'Characteristics name 1')
+                expect(page).to have_field('Description', with: 'Characteristics description 1')
+                expect(page).to have_field('Value', with: 'Characteristics value 1')
+                expect(page).to have_field('Unit', with: 'unit 1')
+                expect(page).to have_field('Data Type', with: 'Characteristics data type 1')
+              end
+            end
+            # Sensors
+            within first('.multiple.sensors') do
+              expect(page).to have_field('draft_platforms_0_instruments_0_sensors_0_short_name', with: 'Sensor short name')
+              expect(page).to have_field('draft_platforms_0_instruments_0_sensors_0_long_name', with: 'Sensor long name')
+              expect(page).to have_field('draft_platforms_0_instruments_0_sensors_0_technique', with: 'Sensor technique')
+              # Characteristics
+              within first('.multiple.characteristics') do
+                within '.multiple-item-0' do
+                  expect(page).to have_field('Name', with: 'Characteristics name')
+                  expect(page).to have_field('Description', with: 'Characteristics description')
+                  expect(page).to have_field('Value', with: 'Characteristics value')
+                  expect(page).to have_field('Unit', with: 'unit')
+                  expect(page).to have_field('Data Type', with: 'Characteristics data type')
+                end
+                within '.multiple-item-1' do
+                  expect(page).to have_field('Name', with: 'Characteristics name 1')
+                  expect(page).to have_field('Description', with: 'Characteristics description 1')
+                  expect(page).to have_field('Value', with: 'Characteristics value 1')
+                  expect(page).to have_field('Unit', with: 'unit 1')
+                  expect(page).to have_field('Data Type', with: 'Characteristics data type 1')
+                end
+              end
+
+              # Sensor 2
+              expect(page).to have_field('draft_platforms_0_instruments_0_sensors_1_short_name', with: 'Sensor short name 1')
+            end
+
+            # Instrument 2
+            expect(page).to have_field('draft_platforms_0_instruments_1_short_name', with: 'Instrument short name 1')
+          end
+
+          # Platform 2
+          expect(page).to have_field('draft_platforms_1_short_name', with: 'Platform short name 1')
+          # Instruments
+          within all('.multiple.instruments').last do
+            expect(page).to have_field('draft_platforms_1_instruments_0_short_name', with: 'Instrument short name')
+            expect(page).to have_field('draft_platforms_1_instruments_0_long_name', with: 'Instrument long name')
+            expect(page).to have_field('draft_platforms_1_instruments_0_technique', with: 'Instrument technique')
+            expect(page).to have_field('Number Of Sensors', with: 2468)
+            expect(page).to have_selector('input.operational-mode[value="Instrument mode 1"]')
+            expect(page).to have_selector('input.operational-mode[value="Instrument mode 2"]')
+            # Characteristics
+            within first('.multiple.characteristics') do
+              within '.multiple-item-0' do
+                expect(page).to have_field('Name', with: 'Characteristics name')
+                expect(page).to have_field('Description', with: 'Characteristics description')
+                expect(page).to have_field('Value', with: 'Characteristics value')
+                expect(page).to have_field('Unit', with: 'unit')
+                expect(page).to have_field('Data Type', with: 'Characteristics data type')
+              end
+              within '.multiple-item-1' do
+                expect(page).to have_field('Name', with: 'Characteristics name 1')
+                expect(page).to have_field('Description', with: 'Characteristics description 1')
+                expect(page).to have_field('Value', with: 'Characteristics value 1')
+                expect(page).to have_field('Unit', with: 'unit 1')
+                expect(page).to have_field('Data Type', with: 'Characteristics data type 1')
+              end
+            end
+            # Sensors
+            within first('.multiple.sensors') do
+              expect(page).to have_field('draft_platforms_1_instruments_0_sensors_0_short_name', with: 'Sensor short name')
+              expect(page).to have_field('draft_platforms_1_instruments_0_sensors_0_long_name', with: 'Sensor long name')
+              expect(page).to have_field('draft_platforms_1_instruments_0_sensors_0_technique', with: 'Sensor technique')
+              # Characteristics
+              within first('.multiple.characteristics') do
+                within '.multiple-item-0' do
+                  expect(page).to have_field('Name', with: 'Characteristics name')
+                  expect(page).to have_field('Description', with: 'Characteristics description')
+                  expect(page).to have_field('Value', with: 'Characteristics value')
+                  expect(page).to have_field('Unit', with: 'unit')
+                  expect(page).to have_field('Data Type', with: 'Characteristics data type')
+                end
+                within '.multiple-item-1' do
+                  expect(page).to have_field('Name', with: 'Characteristics name 1')
+                  expect(page).to have_field('Description', with: 'Characteristics description 1')
+                  expect(page).to have_field('Value', with: 'Characteristics value 1')
+                  expect(page).to have_field('Unit', with: 'unit 1')
+                  expect(page).to have_field('Data Type', with: 'Characteristics data type 1')
+                end
+              end
+
+              # Sensor 2
+              expect(page).to have_field('draft_platforms_1_instruments_0_sensors_1_short_name', with: 'Sensor short name 1')
+            end
+
+            # Instrument 2
+            expect(page).to have_field('draft_platforms_1_instruments_1_short_name', with: 'Instrument short name 1')
+          end
+        end
+
+        within '.projects' do
+          expect(page).to have_field('Short Name', with: 'Project short name')
+          expect(page).to have_field('Long Name', with: 'Project long name')
+          expect(page).to have_selector('input.campaign[value="Project campaign 1"]')
+          expect(page).to have_selector('input.campaign[value="Project campaign 2"]')
+          expect(page).to have_field('Start Date', with: '2015-07-01T00:00:00Z')
+          expect(page).to have_field('End Date', with: '2015-08-01T00:00:00Z')
+        end
       end
     end
   end
--- spec/features/drafts/forms/clear_science_keywords_spec.rb
+++ spec/features/drafts/forms/clear_science_keywords_spec.rb
@@ -22,7 +22,7 @@ describe 'Clearing saved science keywords', js: true do
       click_on 'Add Keyword'
 
       within '.nav-top' do
-        click_on 'Save'
+        click_on 'Done'
       end
 
       open_accordions
@@ -32,30 +32,50 @@ describe 'Clearing saved science keywords', js: true do
       expect(page).to have_content('Draft was successfully updated')
     end
 
-    it 'populates the form with the values' do
-      expect(page).to have_content('EARTH SCIENCE SERVICES > DATA ANALYSIS AND VISUALIZATION > GEOGRAPHIC INFORMATION SYSTEMS')
-    end
-
-    context 'when removing the science keywords and saving the form' do
+    context 'when returning to the form' do
       before do
-        within '.selected-science-keywords' do
-          find('.remove').click
-          # remove_script = "$('.selected-science-keywords').find('.remove').click()"
-          # page.execute_script(remove_script)
-          sleep 1
+        within '.metadata' do
+          click_on 'Descriptive Keywords'
         end
 
-        within '.nav-top' do
-          click_on 'Save'
+        open_accordions
+      end
+
+      it 'populates the form with the values' do
+        expect(page).to have_content('EARTH SCIENCE SERVICES > DATA ANALYSIS AND VISUALIZATION > GEOGRAPHIC INFORMATION SYSTEMS')
+      end
+
+      context 'when removing the science keywords and saving the form' do
+        before do
+          within '.selected-science-keywords' do
+            find('.remove').click
+            # remove_script = "$('.selected-science-keywords').find('.remove').click()"
+            # page.execute_script(remove_script)
+            sleep 1
+          end
+
+          within '.nav-top' do
+            click_on 'Done'
+          end
         end
-      end
 
-      it 'displays a confirmation message' do
-        expect(page).to have_content('Draft was successfully updated')
-      end
+        it 'displays a confirmation message' do
+          expect(page).to have_content('Draft was successfully updated')
+        end
 
-      it 'does not display the removed science keywords' do
-        expect(page).to have_no_content('EARTH SCIENCE SERVICES > DATA ANALYSIS AND VISUALIZATION > GEOGRAPHIC INFORMATION SYSTEMS')
+        context 'when returning to the form' do
+          before do
+            within '.metadata' do
+              click_on 'Descriptive Keywords'
+            end
+
+            open_accordions
+          end
+
+          it 'does not display the removed science keywords' do
+            expect(page).to have_no_content('EARTH SCIENCE SERVICES > DATA ANALYSIS AND VISUALIZATION > GEOGRAPHIC INFORMATION SYSTEMS')
+          end
+        end
       end
     end
   end
