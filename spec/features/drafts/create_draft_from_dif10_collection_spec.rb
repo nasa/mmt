5d0fb7aed0e94116c90779b7960c042cbed72584
Charles Huang <charles.huang@nasa.gov>
2016-05-20 12:21:36 -0400

    MMT-548 ensure dif10 collections with location keywords import the location keywords properly

---
+++ spec/features/drafts/create_draft_from_dif10_collection_spec.rb
@@ -0,0 +1,63 @@
+# MMT-548
+
+require 'rails_helper'
+
+describe 'Create and edit a draft from a Dif 10 collection with location keywords', js: true, reset_provider: true do
+  short_name = 'SWDB_L310'
+
+  before do
+    login
+    user = User.first
+    user.provider_id = 'LARC'
+    user.save
+  end
+
+  context 'when searching for a Dif 10 collection' do
+    before do
+      # search for the record, by short_name
+      fill_in 'Quick Find', with: short_name
+      click_on 'Find'
+
+      click_on short_name
+    end
+
+    it 'imports the location keywords' do
+      # test for location keywords on preview page
+      within '.location-keyword-preview' do
+        expect(page).to have_content('GEOGRAPHIC REGION GLOBAL')
+      end
+    end
+
+    context 'when editing the record with more location keywords' do
+      before do
+        click_on 'Edit Record'
+
+        within '.metadata' do
+          click_on 'Spatial Information', match: :first
+        end
+
+        click_on 'Expand All'
+
+        add_location_keywords
+
+        within '.nav-top' do
+          click_on 'Done'
+        end
+      end
+
+      after(:all) do
+        # delete the draft record so it doesn't affect other tests
+        draft = Draft.first
+        draft.destroy!
+      end
+
+      it 'displays all the location keywords in the metadata preview' do
+        within '.location-keyword-preview' do
+          expect(page).to have_content('GEOGRAPHIC REGION GLOBAL')
+          expect(page).to have_content('GEOGRAPHIC REGION ARCTIC')
+          expect(page).to have_content('OCEAN ATLANTIC OCEAN NORTH ATLANTIC OCEAN BALTIC SEA')
+        end
+      end
+    end
+  end
+end
