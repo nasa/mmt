1cec8f465970e932a1beefae951ca25fbea82a5d
James Pino <jpino@innovim.com>
2016-09-28 13:09:45 -0400

    implementing changes since last comments

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -1,9 +1,5 @@
 class PermissionsController < ApplicationController
 
-  #-------------------------------------------------------------
-  # index - this provides a listing of all the existing
-  # permissions.
-  #-------------------------------------------------------------
   def index
 
     # Global provier ID for the current user. At some point we might
@@ -11,7 +7,7 @@ class PermissionsController < ApplicationController
     provider_id = @current_user.provider_id
     response = cmr_client.get_permissions_for_provider(provider_id, token)
 
-    if ! response.success?
+    unless response.success?
       Rails.logger.error("Error getting permissions: #{response.inspect}")
       error = Array.wrap(response.body['errors'])[0]
       flash[:error] = error
@@ -23,37 +19,32 @@ class PermissionsController < ApplicationController
     # This block searches through the permissions of each
     # group and creates a summary for display on the page
     @permissions.each do |p|
-      permissionSummary = {}
-      permissionSummaryList = []
-      groupPermissions = p["acl"]["group_permissions"]
-      groupPermissions.each do |g|
-        permList = g["permissions"]
-        permList.each do |i|
-            permissionSummary[i] = i
+      permission_summary = {}
+      permission_summary_list = []
+      group_permissions = p['acl']['group_permissions']
+      group_permissions.each do |g|
+        perm_list = g['permissions']
+        perm_list.each do |i|
+            permission_summary[i] = i
         end
       end
 
-      permissionSummary.keys.each do |a|
-        if a == "read"
-          a = "search"
+      permission_summary.keys.each do |a|
+        if a == 'read'
+          a = 'search'
         end
-        permissionSummaryList.push(a.capitalize)
+        permission_summary_list.push(a.capitalize)
       end
 
-      p["permisssion_summary"] = permissionSummaryList.join " & "
+      p['permisssion_summary'] = permission_summary_list.join ' & '
     end
 
   end
 
-
-  #-------------------------------------------------------------
-  # new - method for handling adding of a new permission
-  #-------------------------------------------------------------
   def new
-
-    #@collectionsOptions = []
-    @collectionIDs = []
-    @granulesOptions = []
+    @collection_ids = []
+    @granules_options = []
+    @collections_options = []
     @groups = []
 
     @filters = params[:filters] || {}
@@ -72,7 +63,7 @@ class PermissionsController < ApplicationController
 
       @tmp_groups = groups_response.body['items']
       for i in @tmp_groups
-        opt = [i["name"], i["concept_id"] ]
+        opt = [i['name'], i['concept_id'] ]
         @groups.push(opt)
       end
 
@@ -83,7 +74,7 @@ class PermissionsController < ApplicationController
       @groups = nil
     end
 
-    @granulesOptions = [
+    @granules_options = [
       ['- Select -', 'select'],
       ['No Access to Granules', 'no-access'],
       ['All Granules', 'all-granules']
@@ -91,47 +82,72 @@ class PermissionsController < ApplicationController
       #['Granules with an Access Constraint Value','access-constraint-granule']
     ]
 
-    @collectionsOptions = [
+    @collections_options = [
       ['- Select -', 'select'],
       ['All Collections','all-collections']
       #['Selected Collection Entry IDs', 'selected-ids-collections'],
       #['Collections with an Access Constraint Value', 'access-constraint-collections' ]
     ]
-
   end
 
-
-  #-------------------------------------------------------------
-  # show - shows a specific permission detail
-  #-------------------------------------------------------------
   def show
-
+    # stub
   end
 
-
-  #-------------------------------------------------------------
-  # create - woker method to create a new permission
-  #-------------------------------------------------------------
   def create
-
     #add_group_permissions(provider_id, permission_name, collections, granules, search_groups, search_and_order_groups, token)
 
     # Global provier ID for the current user. At some point we might
     # allow the user to specify a different provider.
     provider_id = @current_user.provider_id
 
-    searchGroups = params["searchGroupsVal"].split ','
-    searchAndOrderGroups = params["searchAndOrderGroupsVal"].split ','
+    search_groups = params[:searchGroupsVal].split ','
+    search_and_order_groups = params[:searchAndOrderGroupsVal].split ','
+    collections = params[:collections]
+    granules = params[:granules]
 
+    request_object = {
+      'group_permissions' => Array.new,
+      'catalog_item_identity' => {
+        'name' => params['permissionName'],
+        'provider_id' => provider_id,
+        'granule_applicable' => true
+      }
+    }
 
-    response = cmr_client.add_group_permissions(provider_id,
-      params["permissionName"],
-      params["collections"],
-      params["granules"],
-      searchGroups,
-      searchAndOrderGroups,
-      token
-    )
+    # TODO -- we will add another condition here to add
+    # specifc collection IDs
+    collection_applicable = false
+    if collections == 'all-collections'
+      collection_applicable = true
+    end
+
+    request_object['catalog_item_identity']['collection_applicable'] = collection_applicable
+
+    granule_applicable = false
+    if granules == 'all-granules'
+        granule_applicable = true
+    end
+
+    request_object['catalog_item_identity']['granule_applicable'] = granule_applicable
+
+    search_groups.each do |i|
+      search_permission = {
+        'group_id'=> i,
+        'permissions'=> ['read'] # aka "search"
+      }
+      request_object['group_permissions'] << search_permission
+    end
+
+    search_and_order_groups.each do |i|
+      search_and_order_permission = {
+        'group_id'=> i,
+        'permissions'=> ['read', 'order'] # aka "search"
+      }
+      request_object['group_permissions'] << search_and_order_permission
+    end
+
+    response = cmr_client.add_group_permissions(request_object, token)
 
     if response.success?
       flash[:success] = 'Permission was successfully created.'
@@ -140,11 +156,6 @@ class PermissionsController < ApplicationController
       Rails.logger.error("Permission Creation Error: #{response.inspect}")
       permission_creation_error = Array.wrap(response.body['errors'])[0]
       flash[:error] = permission_creation_error
-      #render :index
     end
-
   end
-
-
-
-end # end class
+end
