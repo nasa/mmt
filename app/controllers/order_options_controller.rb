92da14bb65b4f3c93dadb01fb0d8e12630a35332
James Pino <jpino@innovim.com>
2016-11-15 12:04:06 -0500

    MMT-592: Removing unused helper, adding max length of 5 to sort key, commenting out Track Orders link, cleaned up ECHO Client code.

--- app/controllers/order_options_controller.rb
+++ app/controllers/order_options_controller.rb
@@ -1,5 +1,4 @@
 class OrderOptionsController < ApplicationController
-  include OrderOptionsHelper
 
   def index
   end
@@ -9,18 +8,15 @@ class OrderOptionsController < ApplicationController
   end
 
   def create
-
     @order_option = params[:order_option]
     @order_option.delete(:sort_key) if @order_option[:sort_key].blank?
 
     response = temp_response = cmr_client.create_order_option(@order_option, echo_provider_token)
 
     if response.success?
-      flash[:success] = 'Order Option was successfully created.'
-
       order_option_response = Hash.from_xml(response.body)
       order_option_id = order_option_response['option_definition']['id']
-
+      flash[:success] = 'Order Option was successfully created.'
       redirect_to order_option_path(order_option_id)
     else
       Rails.logger.error("Create Order Option Error: #{response.inspect}")
--- lib/cmr/echo_client.rb
+++ lib/cmr/echo_client.rb
@@ -24,17 +24,12 @@ module Cmr
       get('/echo-rest/providers.json')
     end
 
-    # Order Option Definitions
     def create_order_option(order_option, token)
       url = '/echo-rest/option_definitions'
       content_type = { 'Content-Type' => 'application/json' }
       body = { 'option_definition' => order_option }
-
-      # temp, using echo security token
       echo_security_token = { 'Echo-Token' => token }
       post(url, body.to_json, content_type.merge(echo_security_token))
-
-      # post(url, body.to_json, content_type.merge(token_header(token, true)))
     end
 
     def get_order_option(id, token)
