7e2514aed4f25003932a052575e516d51499f2a0
Charles Huang <charles.huang@nasa.gov>
2016-11-29 09:35:00 -0500

    MMT-155-pagination add and edit tests

--- spec/features/groups/groups_list_spec.rb
+++ spec/features/groups/groups_list_spec.rb
@@ -8,16 +8,17 @@ describe 'Groups list page', reset_provider: true do
       end
 
       context 'when viewing the groups page' do
-        before do
-          visit groups_path
-        end
 
         context 'when there are no groups' do
+          before do
+            visit groups_path
+          end
+
           it 'does not show any groups' do
             expect(page).to have_content('No groups found at this time.')
           end
 
-          context 'when there are groups' do
+          context 'when there are groups under the pagination limit' do
             before :all do
               # TODO: We could mock this response as we're only testing that they list properly
               # Create a few groups to list
@@ -37,39 +38,22 @@ describe 'Groups list page', reset_provider: true do
 
             before do
               wait_for_cmr
-              
+
               visit groups_path
             end
 
             it 'displays a list of groups' do
               within '.groups-table' do
                 within all('tr')[1] do
-                  expect(page).to have_content('Group 1 MMT_2 0')
+                  expect(page).to have_content('Group 1 test group MMT_2 0')
                 end
                 within all('tr')[2] do
-                  expect(page).to have_content('Group 2 MMT_2 1')
+                  expect(page).to have_content('Group 2 test group 2 MMT_2 1')
                 end
               end
 
               within '.groups-table' do
-                expect(page).to have_no_content('Administrators_2 CMR 2')
-              end
-            end
-
-            context 'when clicking on the group name' do
-              before do
-                click_on 'Group 2'
-              end
-
-              it 'displays the group information' do
-                within '#main-content header' do
-                  expect(page).to have_content('Group 2')
-                  expect(page).to have_content('test group 2')
-                end
-
-                within '#groups-table' do
-                  expect(page).to have_content('Alien Bobcat')
-                end
+                expect(page).to have_no_content('Administrators_2 The group of users that manages the CMR. CMR 2')
               end
             end
 
@@ -88,14 +72,68 @@ describe 'Groups list page', reset_provider: true do
                 it 'displays the provider and system level groups' do
                   within '.groups-table' do
                     # Provider level groups
-                    expect(page).to have_content('Group 1 MMT_2 0')
-                    expect(page).to have_content('Group 2 MMT_2 1')
+                    expect(page).to have_content('Group 1 test group MMT_2 0')
+                    expect(page).to have_content('Group 2 test group 2 MMT_2 1')
 
                     # System level groups
-                    expect(page).to have_content('Administrators_2 SYS CMR 2')
+                    expect(page).to have_content('Administrators_2 SYS The group of users that manages the CMR. CMR 2')
                   end
                 end
               end
+            end
+          end
+        end
+
+        context 'when there are paginated groups' do
+          before do
+            # mock for group list page 1
+            groups_page_1_response = Cmr::Response.new(Faraday::Response.new(status: 200, body: JSON.parse(File.read('spec/fixtures/groups/groups_index_page_1.json'))))
+            page_1_options = { page_size: 25, page_num: 1 }
+            allow_any_instance_of(Cmr::CmrClient).to receive(:get_cmr_groups).with(page_1_options, 'access_token').and_return(groups_page_1_response)
+
+            # mock for group list page 2
+            groups_page_2_response = Cmr::Response.new(Faraday::Response.new(status: 200, body: JSON.parse(File.read('spec/fixtures/groups/groups_index_page_2.json'))))
+            page_2_options = { page_size: 25, page_num: 2 }
+            allow_any_instance_of(Cmr::CmrClient).to receive(:get_cmr_groups).with(page_2_options, 'access_token').and_return(groups_page_2_response)
+
+            visit groups_path
+          end
+
+          it 'lists the first page of groups' do
+            within '.groups-table' do
+              expect(page).to have_selector('tbody tr', count: 25)
+              # cmr controls the order so we should not test specific groups
+            end
+          end
+
+          it 'displays the pagination information for page 1' do
+            expect(page).to have_content('Showing groups 1 - 25 of 31')
+            within '.eui-pagination' do
+              # first, 1, 2, next, last
+              expect(page).to have_selector('li', count: 5)
+            end
+            expect(page).to have_css('.active-page', text: '1')
+          end
+
+          context 'when clicking to the second page' do
+            before do
+              click_link '2'
+            end
+
+            it 'lists the second page of groups' do
+              within '.groups-table' do
+                expect(page).to have_selector('tbody tr', count: 6)
+                # cmr controls the order so we should not test specific groups
+              end
+            end
+
+            it 'displays the pagination information for page 2' do
+              expect(page).to have_content('Showing groups 26 - 31 of 31')
+              within '.eui-pagination' do
+                # first, previous, 1, 2, last
+                expect(page).to have_selector('li', count: 5)
+              end
+              expect(page).to have_css('.active-page', text: '2')
             end
           end
         end
@@ -110,8 +148,7 @@ describe 'Groups list page', reset_provider: true do
 
     context 'when viewing the groups index page' do
       before do
-        click_on 'Manage CMR'
-        click_on 'Groups'
+        visit groups_path
       end
 
       it 'displays the system level groups' do
@@ -121,8 +158,8 @@ describe 'Groups list page', reset_provider: true do
         # may need different solution
         page.document.synchronize do
           within '.groups-table' do
-            expect(page).to have_content('Administrators_2 SYS CMR 2')
-            expect(page).to have_content('SEDAC Test Group SEDAC 0')
+            expect(page).to have_content('Administrators_2 SYS The group of users that manages the CMR. CMR 2')
+            expect(page).to have_content('SEDAC Test Group Test group for provider SEDAC 0')
           end
         end
       end
