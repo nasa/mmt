1703367591099797151d2323aac011f6b5ad4cef
James Pino <jpino@innovim.com>
2017-01-11 15:25:13 -0500

    MMT-595: Removing unnecessary lines (from RuboCop reports), simplifying logic for error handling in deprecate function (always sending error_message to the user).

--- spec/features/order_options/deprecate_order_options_spec.rb
+++ spec/features/order_options/deprecate_order_options_spec.rb
@@ -1,49 +1,40 @@
 # MMT-595
-
 require 'rails_helper'
 
 describe 'Deprecating Order Options' do
   context 'when viewing the index page', js: true do
     before do
       login
-
       VCR.use_cassette('echo_rest/order_options/list_deprecated', record: :none) do
         visit order_options_path
       end
     end
 
     it 'lists available order options with \'Deprecate\' links.' do
-        page.all('.order-options-table tr') do |tr|
-          within tr do
-            expect(tr).to have_link 'Deprecate'
-          end
+      page.all('.order-options-table tr') do |tr|
+        within tr do
+          expect(tr).to have_link 'Deprecate'
         end
+      end
     end
 
     context 'When clicking on a Deprecate link, it asks for confirmation before deprecating.' do
       before do
-
         within '.order-options-table tbody tr:first-child' do
           click_on 'Deprecate'
         end
       end
-
       it 'Asks for confirmation before deprecating' do
         cell_text = find('.order-options-table tbody tr:first-child td:first-child').text
-
         expect(page).to have_selector('#deprecate-option-modal-0', visible: true)
-
         expect(page).to have_content("Are you sure you want to deprecate the order option named '#{cell_text}'?")
         expect(page).to have_link('No')
         expect(page).to have_link('Yes')
       end
-
     end
-
 
     context 'When clicking the No button on the confirmation dialog, it does not deprecate the order option.' do
       before do
-
         VCR.use_cassette('echo_rest/order_options/list', record: :none) do
           visit order_options_path
         end
@@ -81,11 +72,8 @@ describe 'Deprecating Order Options' do
     end
 
     context 'When trying to deprecate an order option that cannot be deprecated' do
-
       let(:cell_text) {''}
-
       before do
-
         within '.order-options-table tbody tr:first-child' do
           click_on 'Deprecate'
         end
@@ -95,9 +83,7 @@ describe 'Deprecating Order Options' do
             click_on 'Yes'
           end
         end
-
         cell_text = find('.order-options-table tbody tr:first-child td:first-child').text
-
       end
 
       it 'Displays an error message and lists the order options' do
