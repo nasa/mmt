6fab903c5e63ac2f4b35299a64c6d866fea988f7
Matthew Crouch <matthew@element84.com>
2017-02-22 21:20:25 -0500

    MMT-708 Rails migrations for each step from UMM 1.2 to 1.6

---
+++ db/migrate/20170222205951_umm_schema_1_4_to_1_5.rb
@@ -0,0 +1,6 @@
+class UmmSchema14To15 < ActiveRecord::Migration
+  # The only change from 1.4 to 1.5 was making a field required in AdditionAttributeType. This doesn't need a change to the data
+
+  def change
+  end
+end
