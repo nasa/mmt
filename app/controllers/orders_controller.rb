2a427c8c5367a02f5ea28b482c7a7771081fc17e
Ryan Abbott <ryan@element84.com>
2017-04-08 09:29:26 -0400

    MMT-909: Support guest orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,126 +2,62 @@
 class OrdersController < ManageCmrController
   add_breadcrumb 'Track Orders', :orders_path
 
-  def index
-  end
+  def index; end
 
   def show
-    @order = generate_order(params['id'])
-  end
+    @order = echo_client.get_orders(echo_provider_token, params['id']).parsed_body.fetch('Item', {})
 
-  def edit
+    add_breadcrumb @order.fetch('Guid'), order_path(@order.fetch('Guid', nil))
   end
 
   def search
     @orders = []
 
-    # Order Guid takes precedence over filters, if an order_guid is present
-    # search for that rather than using the filters
-    order_guids = if params['order_guid'].present?
-                    params['order_guid']
-                  else
-                    # Search for orders based on the provided filters
-                    payload = {
-                      'states'    => (params['states'] || OrdersHelper::ORDER_STATES),
-                      'date_type' => params['date_type'],
-                      'from_date' => params['from_date'],
-                      'to_date'   => params['to_date']
-                    }
-
-                    # Request orders from ECHO
-                    order_search_result = echo_client.get_provider_order_guids_by_state_date_and_provider(echo_provider_token, payload)
-
-                    # Pull out just the Guids for the returned orders
-                    Array.wrap(order_search_result.parsed_body.fetch('Item', [])).map { |guid| guid['OrderGuid'] }
-                  end
-
     # Request the returned objects from ECHO
-    order_objects = echo_client.get_orders(echo_provider_token, order_guids).parsed_body
+    order_response = echo_client.get_orders(echo_provider_token, determine_order_guids)
 
-    # Construct the full order hashes
-    @orders = Array.wrap(order_objects.fetch('Item', [])).map do |order_obj|
-      order = {
-        'guid'           => order_obj['Guid'],
-        'state'          => order_obj['State'],
-        'creation_date'  => order_obj['CreationDate'],
-        'submitted_date' => order_obj['SubmissionDate'],
-        'updated_date'   => order_obj['LastUpdateDate']
-      }
+    if order_response.success?
+      @orders = Array.wrap(order_response.parsed_body.fetch('Item', []))
 
-      contact_address = order_obj.fetch('ContactAddress', {})
-      order['contact_name'] = "#{contact_address['FirstName']} #{contact_address['LastName']}".strip
+      # if user_id param is supplied and we're not searching by guid, filter orders by given user_id
+      if params['order_guid'].blank? && params['user_id'].present?
+        @orders.each do |order|
+          # Don't ask for owner information for guest orders
+          next if order['OwnerGuid'].nil?
 
-      # Add user_id to order information
-      user = echo_client.get_user_names(echo_provider_token, order_obj.fetch('OwnerGuid')).parsed_body
+          order['OwnerName'] = echo_client.get_user_names(echo_provider_token, order['OwnerGuid']).parsed_body.fetch('Item', {}).fetch('Name', '')
+        end
 
-      order['user_id'] = user.fetch('Item', {}).fetch('Name', '')
+        @orders.select! { |order| order['OwnerName'] == params['user_id'] }
+      end
 
-      order
+      @orders.sort_by! { |order| order['CreationDate'] }
+    else
+      Rails.logger.error "Error searching for errors: #{order_response.error_message}"
     end
-
-    # if user_id param is supplied and we're not searching by guid, filter orders by given user_id
-    unless params['order_guid'].present? || params['user_id'].empty?
-      @orders.select! { |order| order['user_id'] == params['user_id'] }
-    end
-
-    @orders.sort_by! { |order| order['creation_date'] }
   end
 
   private
 
-  def generate_order(guid)
-    order_info = echo_client.get_orders(echo_provider_token, guid).parsed_body.fetch('Item', {})
-    order = {}
+  def determine_order_guids
+    # Order Guid takes precedence over filters, if an order_guid is present
+    # search for that rather than using the filters
+    if params['order_guid'].present?
+      params['order_guid']
+    else
+      # Search for orders based on the provided filters
+      payload = {
+        'states'    => (params['states'] || OrdersHelper::ORDER_STATES),
+        'date_type' => params['date_type'],
+        'from_date' => params['from_date'],
+        'to_date'   => params['to_date']
+      }
 
-    order['guid'] = guid
-    order['state'] = order_info.fetch('State', '')
-    order['order_price'] = order_info.fetch('OrderPrice', '')
-    order['creation_date'] = order_info.fetch('CreationDate', '')
-    order['submission_date'] = order_info.fetch('SubmissionDate', '')
-    order['last_update_date'] = order_info.fetch('LastUpdateDate', '')
-    user = echo_client.get_user_names(echo_provider_token, order_info.fetch('OwnerGuid', '')).parsed_body
+      # Request orders from ECHO
+      order_search_result = echo_client.get_provider_order_guids_by_state_date_and_provider(echo_provider_token, payload)
 
-    order['owner'] = user.fetch('Item', {}).fetch('Name', '')
-
-    order['notify_level'] = order_info.fetch('NotifyLevel', '')
-    order['user_domain'] = order_info.fetch('UserDomain', '')
-    order['user_region'] = order_info.fetch('UserRegion', '')
-    order['client_identity'] = order_info.fetch('ClientIdentity', '')
-
-    order['contact_information'] = generate_contact_info(order_info.fetch('ContactAddress', {}))
-
-    order['shipping_information'] = generate_contact_info(order_info.fetch('ShippingAddress', {}))
-
-    order['billing_information'] = generate_contact_info(order_info.fetch('BillingAddress', {}))
-
-    order
-  end
-
-  def generate_contact_info(contact_address)
-    contact_information = {}
-
-    contact_information['role'] = contact_address.fetch('Role', '')
-    contact_information['name'] = "#{contact_address.fetch('FirstName', '')} #{contact_address.fetch('LastName', '')}".strip
-    contact_information['organization'] = contact_address.fetch('Organization', '')
-
-    address = contact_address.fetch('Address', {})
-    contact_information['address'] = {}
-    contact_information['address']['street1'] = address.fetch('Street1', '')
-    contact_information['address']['street2'] = address.fetch('Street2', '')
-    contact_information['address']['street3'] = address.fetch('Street3', '')
-    contact_information['address']['city'] = address.fetch('City', '')
-    contact_information['address']['state'] = address.fetch('State', '')
-    contact_information['address']['zip'] = address.fetch('Zip', '')
-    contact_information['address']['country'] = address.fetch('Country', '')
-    contact_information['address']['special_instructions'] = address.fetch('SpecialInstructions', '')
-
-    phones = contact_address.fetch('Phones', {}).fetch('Item', {})
-    contact_information['phone'] = {}
-    contact_information['phone']['number'] = phones.fetch('Number', '')
-    contact_information['phone']['type'] = phones.fetch('PhoneNumberType', '')
-
-    contact_information['email'] = contact_address.fetch('Email', '')
-
-    contact_information
+      # Pull out just the Guids for the returned orders
+      Array.wrap(order_search_result.parsed_body.fetch('Item', [])).map { |guid| guid['OrderGuid'] }
+    end
   end
 end
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -26,4 +26,25 @@ module OrdersHelper
   def state_is_terminal(order_state)
     OrdersHelper::TERMINAL_ORDER_STATES.include?(order_state)
   end
+
+  def order_owner(owner_guid)
+    if owner_guid.blank?
+      '(guest)'
+    else
+      user = echo_client.get_user_names(echo_provider_token, owner_guid).parsed_body
+
+      user.fetch('Item', {}).fetch('Name', '')
+    end
+  end
+
+  def contact_info(contact_address)
+    contact_address['Name'] = contact_name(contact_address)
+    contact_address['Phone'] = contact_address.fetch('Phones', {}).fetch('Item', {})
+
+    contact_address
+  end
+
+  def contact_name(contact)
+    [contact['FirstName'], contact['LastName']].reject(&:empty?).join(' ')
+  end
 end
