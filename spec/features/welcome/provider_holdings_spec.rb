a86f2354565588a9b27adb9e92979fa5a1b34570
Ryan Abbott <ryan@element84.com>
2017-04-10 16:27:01 -0400

    MMT-867: Remove provider holdings from the homepage

--- spec/features/welcome/provider_holdings_spec.rb
+++ spec/features/welcome/provider_holdings_spec.rb
@@ -1,32 +1,32 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-describe 'Public provider holdings', js: true do
-  context 'when visiting the MMT homepage' do
-    before do
-      visit root_path
-    end
+# describe 'Public provider holdings', js: true do
+#   context 'when visiting the MMT homepage' do
+#     before do
+#       visit root_path
+#     end
 
-    it 'displays a list of available providers' do
-      within '#data-providers' do
-        within all('tr')[2] do
-          expect(page).to have_content('MMT_1')
-        end
-        within all('tr')[3] do
-          expect(page).to have_content('MMT_2')
-        end
-      end
-    end
+#     it 'displays a list of available providers' do
+#       within '#data-providers' do
+#         within all('tr')[2] do
+#           expect(page).to have_content('MMT_1')
+#         end
+#         within all('tr')[3] do
+#           expect(page).to have_content('MMT_2')
+#         end
+#       end
+#     end
 
-    context 'when selecting a provider' do
-      before do
-        VCR.use_cassette('provider_holdings/mmt_2', record: :none) do
-          click_on 'MMT_2'
-        end
-      end
+#     context 'when selecting a provider' do
+#       before do
+#         VCR.use_cassette('provider_holdings/mmt_2', record: :none) do
+#           click_on 'MMT_2'
+#         end
+#       end
 
-      it 'displays the available provider holdings' do
-        expect(page).to have_content('MMT_2 Holdings')
-      end
-    end
-  end
-end
+#       it 'displays the available provider holdings' do
+#         expect(page).to have_content('MMT_2 Holdings')
+#       end
+#     end
+#   end
+# end
