b8cd8dfc25f10444b723fd15f90611799afcb270
Charles Huang <charles.huang@nasa.gov>
2017-04-25 17:34:16 -0400

    MMT-886: update views because of layout changes. add controller tests.

--- app/controllers/bulk_updates_controller.rb
+++ app/controllers/bulk_updates_controller.rb
@@ -1,25 +1,26 @@
-# nodoc
-class BulkUpdatesController < ApplicationController
+# :nodoc:
+class BulkUpdatesController < ManageMetadataController
 
   add_breadcrumb 'Bulk Updates', :bulk_updates_path
-
-  # RESULTS_PER_PAGE = 25
 
   def index
     @provider_id = current_user.provider_id
 
     @tasks = []
-    # pagination? Response does not have `hits`
-    # @filters = { page_size: RESULTS_PER_PAGE }
-    # # default page to 1
+
+    # We cannot implement pagination yet, because the cmr dummy responses do not include it
+    # when the response has `hits` and accepts page information, we should implement it in accordance with other resources (i.e. groups)
+    filters = {}
+
+    # filters[:page_size] = RESULTS_PER_PAGE
+    # default page to 1
     # page = params.fetch('page', 1)
     # @filters[:page_num] = page.to_i
 
-    bulk_updates_jobs_response = cmr_client.get_bulk_updates_tasks_list(@provider_id, token) # TODO add filters for pagination?
+    bulk_updates_jobs_response = cmr_client.get_bulk_updates_tasks_list(@provider_id, filters, token)
 
     if bulk_updates_jobs_response.success?
       @tasks = bulk_updates_jobs_response.body.fetch('tasks', [])
-      @http_status = bulk_updates_jobs_response.body.fetch('status', nil)
     else
       Rails.logger.error("Error retrieving Bulk Updates Jobs List: #{bulk_updates_jobs_response.inspect}")
       flash[:error] = Array.wrap(bulk_updates_jobs_response.body['errors'])[0]
@@ -30,15 +31,11 @@ class BulkUpdatesController < ApplicationController
     @concept_id = params[:id]
     @provider_id = current_user.provider_id
 
-    task = {}
+    @task = {}
 
     task_status_response = cmr_client.bulk_updates_task_status(@provider_id, @concept_id, token)
     if task_status_response.success?
-      task = task_status_response.body
-      @http_status = task.fetch('status', nil)
-      @task_status = task.fetch('task-status', 'unknown')
-      @status_message = task.fetch('status-message', 'unknown')
-      @task_errors = task.fetch('collection-statuses', [])
+      @task = task_status_response.body
 
       add_breadcrumb @concept_id, bulk_update_path(@concept_id)
     else
