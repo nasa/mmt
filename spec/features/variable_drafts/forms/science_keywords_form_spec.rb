290e983b885e24187e96b8451aaf62c5ac437d5c
Charles Huang <charles.huang@nasa.gov>
2017-07-13 21:04:29 -0400

    MMT-998: add science keywords form test

---
+++ spec/features/variable_drafts/forms/science_keywords_form_spec.rb
@@ -0,0 +1,461 @@
+require 'rails_helper'
+
+describe 'Science Keywords Form', reset_provider: true, js: true do
+  before do
+    login
+  end
+
+  context 'When viewing the form with no stored values' do
+    before do
+      draft = create(:empty_variable_draft, user: User.where(urs_uid: 'testuser').first)
+      visit edit_variable_draft_path(draft, 'science_keywords')
+    end
+
+    it 'displays the correct title and description' do
+      within '.umm-form' do
+        expect(page).to have_content('Science Keywords')
+        expect(page).to have_content('Controlled Science Keywords describing the collection. The controlled vocabulary for Science Keywords is maintained in the Keyword Management System (KMS).')
+      end
+    end
+
+    it 'displays the form title in the breadcrumbs' do
+      within '.eui-breadcrumbs' do
+        expect(page).to have_content('Variable Drafts')
+        expect(page).to have_content('Science Keywords')
+      end
+    end
+
+    it 'has 1 required fields' do
+      expect(page).to have_selector('label.eui-required-o', count: 1)
+    end
+
+    # TODO: tests for help icons
+
+    it 'displays the nested item picker' do
+      expect(page).to have_css('div.eui-nested-item-picker')
+    end
+
+    it 'does not list any selected science keywords' do
+      within '.selected-science-keywords ul' do
+        expect(page).to have_no_css('li')
+      end
+    end
+
+    context 'When clicking `Previous` without making any changes' do
+      before do
+        within '.nav-top' do
+          click_button 'Previous'
+        end
+      end
+
+      it 'saves the draft and loads the previous form' do
+        within '.eui-banner--success' do
+          expect(page).to have_content('Variable Draft Updated Successfully!')
+        end
+
+        within '.eui-breadcrumbs' do
+          expect(page).to have_content('Variable Drafts')
+          expect(page).to have_content('Variable Characteristics')
+        end
+
+        within '.umm-form' do
+          expect(page).to have_content('Variable Characteristics')
+        end
+
+        within '.nav-top' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('variable_characteristics')
+        end
+
+        within '.nav-bottom' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('variable_characteristics')
+        end
+      end
+    end
+
+    context 'When clicking `Next` without making any changes' do
+      before do
+        within '.nav-top' do
+          click_on 'Next'
+        end
+      end
+
+      it 'saves the draft and loads the next form' do
+        within '.eui-banner--success' do
+          expect(page).to have_content('Variable Draft Updated Successfully!')
+        end
+
+        within '.eui-breadcrumbs' do
+          expect(page).to have_content('Variable Drafts')
+          expect(page).to have_content('Service')
+        end
+
+        within '.umm-form' do
+          expect(page).to have_content('Service')
+        end
+
+        within '.nav-top' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('service')
+        end
+
+        within '.nav-bottom' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('service')
+        end
+      end
+    end
+
+    context 'When clicking `Save` without making any changes' do
+      before do
+        within '.nav-top' do
+          click_button 'Save'
+        end
+      end
+
+      it 'saves the draft and reloads the form' do
+        within '.eui-banner--success' do
+          expect(page).to have_content('Variable Draft Updated Successfully!')
+        end
+
+        within '.eui-breadcrumbs' do
+          expect(page).to have_content('Variable Drafts')
+          expect(page).to have_content('Science Keywords')
+        end
+
+        within '.umm-form' do
+          expect(page).to have_content('Science Keywords')
+        end
+
+        within '.nav-top' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('science_keywords')
+        end
+
+        within '.nav-bottom' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('science_keywords')
+        end
+      end
+    end
+
+    context 'When selecting the previous form from the navigation dropdown' do
+      before do
+        within '.nav-top' do
+          select 'Variable Characteristics', from: 'Save & Jump To:'
+        end
+      end
+
+      it 'saves the draft and loads the previous form' do
+        within '.eui-banner--success' do
+          expect(page).to have_content('Variable Draft Updated Successfully!')
+        end
+
+        within '.eui-breadcrumbs' do
+          expect(page).to have_content('Variable Drafts')
+          expect(page).to have_content('Variable Characteristics')
+        end
+
+        within '.umm-form' do
+          expect(page).to have_content('Variable Characteristics')
+        end
+
+        within '.nav-top' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('variable_characteristics')
+        end
+
+        within '.nav-bottom' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('variable_characteristics')
+        end
+      end
+    end
+
+    context 'When selecting the next form from the navigation dropdown' do
+      before do
+        within '.nav-top' do
+          select 'Service', from: 'Save & Jump To:'
+        end
+      end
+
+      it 'saves the draft and loads the next form' do
+        within '.eui-banner--success' do
+          expect(page).to have_content('Variable Draft Updated Successfully!')
+        end
+
+        within '.eui-breadcrumbs' do
+          expect(page).to have_content('Variable Drafts')
+          expect(page).to have_content('Service')
+        end
+
+        within '.umm-form' do
+          expect(page).to have_content('Service')
+        end
+
+        within '.nav-top' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('service')
+        end
+
+        within '.nav-bottom' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('service')
+        end
+      end
+    end
+  end
+
+  context 'When viewing the form with 1 stored value' do
+    before do
+      draft_science_keywords = [{
+        'Category': 'EARTH SCIENCE SERVICES',
+        'Topic': 'DATA ANALYSIS AND VISUALIZATION',
+        'Term': 'GEOGRAPHIC INFORMATION SYSTEMS'
+      }]
+      draft = create(:empty_variable_draft, user: User.where(urs_uid: 'testuser').first, draft: { 'ScienceKeywords': draft_science_keywords})
+      visit edit_variable_draft_path(draft, 'science_keywords')
+    end
+
+    it 'displays one selected science keyword' do
+      expect(page).to have_css('.selected-science-keywords ul > li', count: 1)
+    end
+
+    it 'displays the correct selected science keyword value' do
+      within '.selected-science-keywords' do
+        '{"Category"=>"EARTH SCIENCE SERVICES", "Topic"=>"DATA ANALYSIS AND VISUALIZATION", "Term"=>"GEOGRAPHIC INFORMATION SYSTEMS"}'
+        # science keywords are not yet being converted between strings with ' > ' delimiters and hashes,
+        # so we are matching the keywords as a string displaying a hash, but should be modified match the string version with the ' > ' delimiter
+        # expect(page).to have_content('EARTH SCIENCE SERVICES > DATA ANALYSIS AND VISUALIZATION > GEOGRAPHIC INFORMATION SYSTEMS')
+      end
+    end
+
+    context 'When clicking `Previous` without making any changes' do
+      before do
+        within '.nav-top' do
+          click_button 'Previous'
+        end
+      end
+
+      it 'saves the draft and loads the previous form' do
+        within '.eui-banner--success' do
+          expect(page).to have_content('Variable Draft Updated Successfully!')
+        end
+
+        within '.eui-breadcrumbs' do
+          expect(page).to have_content('Variable Drafts')
+          expect(page).to have_content('Variable Characteristics')
+        end
+
+        within '.umm-form' do
+          expect(page).to have_content('Variable Characteristics')
+        end
+
+        within '.nav-top' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('variable_characteristics')
+        end
+
+        within '.nav-bottom' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('variable_characteristics')
+        end
+      end
+    end
+
+    context 'When clicking `Next` without making any changes' do
+      before do
+        within '.nav-top' do
+          click_on 'Next'
+        end
+      end
+
+      it 'saves the draft and loads the next form' do
+        within '.eui-banner--success' do
+          expect(page).to have_content('Variable Draft Updated Successfully!')
+        end
+
+        within '.eui-breadcrumbs' do
+          expect(page).to have_content('Variable Drafts')
+          expect(page).to have_content('Service')
+        end
+
+        within '.umm-form' do
+          expect(page).to have_content('Service')
+        end
+
+        within '.nav-top' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('service')
+        end
+
+        within '.nav-bottom' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('service')
+        end
+      end
+    end
+
+    context 'When clicking `Save` without making any changes' do
+      before do
+        within '.nav-top' do
+          click_button 'Save'
+        end
+      end
+
+      it 'saves the draft and reloads the form' do
+        within '.eui-banner--success' do
+          expect(page).to have_content('Variable Draft Updated Successfully!')
+        end
+
+        within '.eui-breadcrumbs' do
+          expect(page).to have_content('Variable Drafts')
+          expect(page).to have_content('Science Keywords')
+        end
+
+        within '.umm-form' do
+          expect(page).to have_content('Science Keywords')
+        end
+
+        within '.nav-top' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('science_keywords')
+        end
+
+        within '.nav-bottom' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('science_keywords')
+        end
+      end
+
+      it 'displays the correct selected science keyword value' do
+        within '.selected-science-keywords' do
+          '{"Category"=>"EARTH SCIENCE SERVICES", "Topic"=>"DATA ANALYSIS AND VISUALIZATION", "Term"=>"GEOGRAPHIC INFORMATION SYSTEMS"}'
+          # science keywords are not yet being converted between strings with ' > ' delimiters and hashes,
+          # so we are matching the keywords as a string displaying a hash, but should be modified match the string version with the ' > ' delimiter
+          # expect(page).to have_content('EARTH SCIENCE SERVICES > DATA ANALYSIS AND VISUALIZATION > GEOGRAPHIC INFORMATION SYSTEMS')
+        end
+      end
+    end
+  end
+
+  context 'When viewing the form with 2 stored values' do
+    before do
+      draft_science_keywords = [{
+        'Category': 'EARTH SCIENCE SERVICES',
+        'Topic': 'DATA ANALYSIS AND VISUALIZATION',
+        'Term': 'GEOGRAPHIC INFORMATION SYSTEMS'
+      }, {
+        'Category': 'EARTH SCIENCE',
+        'Topic': 'ATMOSPHERE',
+        'Term': 'ATMOSPHERIC TEMPERATURE'
+      }]
+      draft = create(:empty_variable_draft, user: User.where(urs_uid: 'testuser').first, draft: { 'ScienceKeywords': draft_science_keywords})
+      visit edit_variable_draft_path(draft, 'science_keywords')
+    end
+
+    it 'displays 2 selected science keywords' do
+      expect(page).to have_css('.selected-science-keywords ul > li', count: 2)
+    end
+
+    it 'displays the correct selected science keyword values' do
+      within '.selected-science-keywords' do
+        '{"Category"=>"EARTH SCIENCE SERVICES", "Topic"=>"DATA ANALYSIS AND VISUALIZATION", "Term"=>"GEOGRAPHIC INFORMATION SYSTEMS"}'
+        '{"Category"=>"EARTH SCIENCE", "Topic"=>"ATMOSPHERE", "Term"=>"ATMOSPHERIC TEMPERATURE"}'
+        # science keywords are not yet being converted between strings with ' > ' delimiters and hashes,
+        # so we are matching the keywords as a string displaying a hash, but should be modified match the string version with the ' > ' delimiter
+        # expect(page).to have_content('EARTH SCIENCE SERVICES > DATA ANALYSIS AND VISUALIZATION > GEOGRAPHIC INFORMATION SYSTEMS')
+        # expect(page).to have_content('EARTH SCIENCE > ATMOSPHERE > ATMOSPHERIC TEMPERATURE')
+      end
+    end
+
+    context 'When clicking `Previous` without making any changes' do
+      before do
+        within '.nav-top' do
+          click_button 'Previous'
+        end
+      end
+
+      it 'saves the draft and loads the previous form' do
+        within '.eui-banner--success' do
+          expect(page).to have_content('Variable Draft Updated Successfully!')
+        end
+
+        within '.eui-breadcrumbs' do
+          expect(page).to have_content('Variable Drafts')
+          expect(page).to have_content('Variable Characteristics')
+        end
+
+        within '.umm-form' do
+          expect(page).to have_content('Variable Characteristics')
+        end
+
+        within '.nav-top' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('variable_characteristics')
+        end
+
+        within '.nav-bottom' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('variable_characteristics')
+        end
+      end
+    end
+
+    context 'When clicking `Next` without making any changes' do
+      before do
+        within '.nav-top' do
+          click_on 'Next'
+        end
+      end
+
+      it 'saves the draft and loads the next form' do
+        within '.eui-banner--success' do
+          expect(page).to have_content('Variable Draft Updated Successfully!')
+        end
+
+        within '.eui-breadcrumbs' do
+          expect(page).to have_content('Variable Drafts')
+          expect(page).to have_content('Service')
+        end
+
+        within '.umm-form' do
+          expect(page).to have_content('Service')
+        end
+
+        within '.nav-top' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('service')
+        end
+
+        within '.nav-bottom' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('service')
+        end
+      end
+    end
+
+    context 'When clicking `Save` without making any changes' do
+      before do
+        within '.nav-top' do
+          click_button 'Save'
+        end
+      end
+
+      it 'saves the draft and reloads the form' do
+        within '.eui-banner--success' do
+          expect(page).to have_content('Variable Draft Updated Successfully!')
+        end
+
+        within '.eui-breadcrumbs' do
+          expect(page).to have_content('Variable Drafts')
+          expect(page).to have_content('Science Keywords')
+        end
+
+        within '.umm-form' do
+          expect(page).to have_content('Science Keywords')
+        end
+
+        within '.nav-top' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('science_keywords')
+        end
+
+        within '.nav-bottom' do
+          expect(find(:css, 'select[name=jump_to_section]').value).to eq('science_keywords')
+        end
+      end
+
+      it 'displays the correct selected science keyword values' do
+        within '.selected-science-keywords' do
+          '{"Category"=>"EARTH SCIENCE SERVICES", "Topic"=>"DATA ANALYSIS AND VISUALIZATION", "Term"=>"GEOGRAPHIC INFORMATION SYSTEMS"}'
+          '{"Category"=>"EARTH SCIENCE", "Topic"=>"ATMOSPHERE", "Term"=>"ATMOSPHERIC TEMPERATURE"}'
+          # science keywords are not yet being converted between strings with ' > ' delimiters and hashes,
+          # so we are matching the keywords as a string displaying a hash, but should be modified match the string version with the ' > ' delimiter
+          # expect(page).to have_content('EARTH SCIENCE SERVICES > DATA ANALYSIS AND VISUALIZATION > GEOGRAPHIC INFORMATION SYSTEMS')
+          # expect(page).to have_content('EARTH SCIENCE > ATMOSPHERE > ATMOSPHERIC TEMPERATURE')
+        end
+      end
+    end
+  end
+end
