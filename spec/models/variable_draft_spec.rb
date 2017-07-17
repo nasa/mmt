50154f718d0b6c0efd7063851d1952385399cb9a
Charles Huang <charles.huang@nasa.gov>
2017-07-17 04:57:34 -0400

    MMT-998: add validation and model tests

--- spec/models/variable_draft_spec.rb
+++ spec/models/variable_draft_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 describe VariableDraft do
-  # test creating a new one
+  # display_entry_title method
+  it '"display_entry_title" returns a draft title if available' do
+    variable_draft = build(:full_variable_draft, entry_title: 'Title Example')
+    expect(variable_draft.display_entry_title).to eq('Title Example')
+  end
+
+  it '"display_entry_title" returns <Untitled Variable Record> if there is no entry title' do
+    variable_draft = build(:empty_variable_draft)
+    expect(variable_draft.display_entry_title).to eq('<Untitled Variable Record>')
+  end
 end
