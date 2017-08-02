570dd611c2a6c08f7b829b84bdcd8e7b37fcacc8
Ryan Abbott <ryan@element84.com>
2017-08-02 16:25:01 -0400

    MMT-1036: Implements Variable HTML Preview

--- app/controllers/variables_controller.rb
+++ app/controllers/variables_controller.rb
@@ -1,5 +1,13 @@
-class VariablesController < ApplicationController
+# :nodoc:
+class VariablesController < ManageMetadataController
+  before_action :set_variable, only: [:show]
+  before_action :set_schema, only: [:show]
+  before_action :set_form, only: [:show]
+
+  add_breadcrumb 'Variables' # there is no variables index action, so not providing a link
+
   def show
+    add_breadcrumb @variable.fetch('Name', '<Blank Name>'), variable_path(params[:id])
   end
 
   def create
@@ -30,5 +38,20 @@ class VariablesController < ApplicationController
 
       redirect_to variable_draft_path(variable_draft, flash: { error: I18n.t('controllers.variables.create.flash.error') })
     end
+  end
+
+  private
+
+  def set_variable
+    @variable = cmr_client.get_concept(params[:id], token, {}, params[:revision_id]).body
+  end
+
+  def set_schema
+    @schema = UmmJsonSchema.new('umm-var-json-schema.json')
+    @schema.fetch_references(@schema.parsed_json)
+  end
+
+  def set_form
+    @json_form = UmmJsonForm.new('umm-var-form.json', @schema, @variable, 'field_prefix' => 'variable_draft/draft')
   end
 end
