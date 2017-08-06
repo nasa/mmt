168d85c17ed2055dbc992d98a960db979c484976
ryan.d.cody <ryan.d.cody@nasa.gov>
2017-08-06 13:36:41 -0400

    mmt-1032: Fixed search helper to select provider correctly

--- spec/support/search_helper.rb
+++ spec/support/search_helper.rb
@@ -2,14 +2,14 @@ module Helpers
   module SearchHelpers
     def full_search(keyword: '', record_type: 'Collections', provider: nil)
       # Display the full search modal
-      click_on 'Full Metadata Record Search'
+      click_on 'search-drop'
 
       # Populate the form with the provided attributes
       choose record_type
       select provider, from: 'provider_id' unless provider.nil?
       fill_in 'keyword', with: keyword
 
-      click_button 'Submit'
+      click_button 'Search Collections'
     end
   end
 end
