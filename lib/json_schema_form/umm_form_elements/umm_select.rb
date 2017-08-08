75ac9fe650ef41d48b292c526eed039f43fd8017
Ryan Abbott <ryan@element84.com>
2017-08-08 14:53:42 -0400

    MMT-1066: Adds 'Select' prompt to select elements

--- lib/json_schema_form/umm_form_elements/umm_select.rb
+++ lib/json_schema_form/umm_form_elements/umm_select.rb
@@ -7,6 +7,15 @@ class UmmSelect < UmmFormElement
     select_tag(keyify_property_name(form_fragment), ui_options, element_properties(schema_fragment))
   end
 
+  def element_properties(element)
+    properties = super(element)
+
+    # Prevent children of this class from adding further properties
+    return properties unless self.class.to_s == 'UmmSelect'
+
+    properties.merge(prompt: "Select a #{title}")
+  end
+
   def ui_options
     options_for_select(schema_fragment['enum'], element_value)
   end
