e17b88afddda8268532ba2a9c569119ce21a8d94
Charles Huang <charles.huang@nasa.gov>
2017-08-25 13:11:44 -0400

    MMT-1121: makes Drafts listings show all drafts of that type for current provider

--- app/controllers/manage_services_controller.rb
+++ app/controllers/manage_services_controller.rb
@@ -4,7 +4,7 @@ class ManageServicesController < ManageMetadataController
     # If you change this number you must also change it in the corresponding test file - features/manage_metadata/open_drafts_spec.rb.
     @draft_display_max_count = 5
 
-    @drafts = current_user.drafts.where(draft_type: 'ServiceDraft').where(provider_id: current_user.provider_id)
+    @drafts = Draft.where(draft_type: 'ServiceDraft').where(provider_id: current_user.provider_id)
                           .order('updated_at DESC')
                           .limit(@draft_display_max_count + 1)
   end
--- spec/features/manage_collections/open_drafts_spec.rb
+++ spec/features/manage_collections/open_drafts_spec.rb
@@ -3,34 +3,54 @@
 require 'rails_helper'
 # Basic draft creation and retrieval via the list on the Manage Collections page are tested in draft_creation_spec.rb
 
-describe 'Open Drafts listings on the Manage Collections page', reset_provider: true do
+describe 'Collection Drafts listed on the Manage Collections page', reset_provider: true do
   draft_display_max_count = 5 # Should agree with @draft_display_max_count found in manage_collections_controller
 
   before do
+    login_admin
+    @other_user_id = User.where(urs_uid: 'adminuser').first.id
+
     login
+    @current_user_id = User.where(urs_uid: 'testuser').first.id
   end
 
   context 'when no drafts exist' do
     it 'no drafts are displayed' do # Test correct display of edge condition of zero drafts
       expect(page).to have_content('MMT_2 Collection Drafts')
-      within('.open-drafts') do
-        expect(page).to_not have_content('|')
+
+      within '.open-drafts' do
+        expect(page).to have_content('MMT_2 has no drafts to display.')
+      end
+    end
+  end
+
+  context 'when there are open drafts from multiple users' do
+    before do
+      # create 2 drafts per user
+      2.times { create(:collection_draft, user_id: @current_user_id) }
+      2.times { create(:collection_draft, user_id: @other_user_id) }
+
+      visit manage_collections_path
+    end
+
+    it 'displays all the drafts' do
+      within '.open-drafts' do
+        expect(page).to have_content('<Blank Short Name>', count: 4)
       end
     end
   end
 
   context "when more than #{draft_display_max_count} open drafts exist" do
     before do
-      current_user_id = User.where(urs_uid: 'testuser').first.id
-      (draft_display_max_count + 1).times do
-        create(:collection_draft, user_id: current_user_id)
-      end
+      # create draft_display_max_count drafts per user
+      draft_display_max_count.times { create(:collection_draft, user_id: @current_user_id) }
+      draft_display_max_count.times { create(:collection_draft, user_id: @other_user_id) }
 
       visit manage_collections_path
     end
 
     it '"More" is displayed' do
-      within('.open-drafts') do
+      within '.open-drafts' do
         expect(page).to have_link('More')
       end
     end
@@ -42,8 +62,8 @@ describe 'Open Drafts listings on the Manage Collections page', reset_provider: 
         end
       end
 
-      it 'the Your Open Drafts Page is displayed with all the open drafts' do
-        expect(page).to have_content('<Blank Short Name>', count: draft_display_max_count + 1)
+      it 'the Collection Drafts index page is displayed with all the open drafts' do
+        expect(page).to have_content('<Blank Short Name>', count: (draft_display_max_count * 2))
       end
 
       context 'when "<Blank Short Name>" is clicked on' do
@@ -53,7 +73,7 @@ describe 'Open Drafts listings on the Manage Collections page', reset_provider: 
 
         it 'the record is opened for view/edit' do
           within '.eui-breadcrumbs' do
-            expect(page).to have_content('Drafts')
+            expect(page).to have_content('Collection Drafts')
           end
           expect(page).to have_content('Entry Title Not Provided')
         end
