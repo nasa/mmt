7468967aa61b7469b6f4d74c604131935b36f2d8
Charles Huang <charles.huang@nasa.gov>
2017-10-25 10:11:56 -0400

    MMT-982: remove comments

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -3,5 +3,3 @@ Time::DATE_FORMATS[:default_with_time_zone] = '%Y-%m-%d %H:%M %Z'
 Time::DATE_FORMATS[:date] = '%Y-%m-%d'
 Time::DATE_FORMATS[:metadata_dates_format] = '%Y-%m-%dT%H:%M:00.000Z'
 Time::DATE_FORMATS[:echo_format] = '%A, %B %d, %Y at %l:%M %P'
-
-# CMR format: yyyy-MM-ddTHH:mm:ssZ format
