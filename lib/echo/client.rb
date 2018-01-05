b745823de7eb55d48aa2f13770d55560d82c22ea
Matthew Crouch <matthew@element84.com>
2018-01-05 09:31:50 -0500

    Revert "MMT-1281 move soap request classes to a new module"
    This reverts commit bbe644943787662134963d71b0e6056958f9e32d.

--- lib/echo/client.rb
+++ lib/echo/client.rb
@@ -5,37 +5,37 @@ module Echo
       @base_url = url
 
       services = []
-      services << Services::Authentication.new(
+      services << Authentication.new(
         [@base_url, services_configs['authentication']['path']].join('/'),
         [@base_url, services_configs['authentication']['wsdl']].join('/')
       )
 
-      services << Services::Provider.new(
+      services << Provider.new(
         [@base_url, services_configs['provider']['path']].join('/'),
         [@base_url, services_configs['provider']['wsdl']].join('/')
       )
 
-      services << Services::DataManagement.new(
+      services << DataManagement.new(
         [@base_url, services_configs['data_management']['path']].join('/'),
         [@base_url, services_configs['data_management']['wsdl']].join('/')
       )
 
-      services << Services::ServiceManagement.new(
+      services << ServiceManagement.new(
         [@base_url, services_configs['service_management']['path']].join('/'),
         [@base_url, services_configs['service_management']['wsdl']].join('/')
       )
 
-      services << Services::OrderManagement.new(
+      services << OrderManagement.new(
         [@base_url, services_configs['order_management']['path']].join('/'),
         [@base_url, services_configs['order_management']['wsdl']].join('/')
       )
 
-      services << Services::OrderProcessing.new(
+      services << OrderProcessing.new(
         [@base_url, services_configs['order_processing']['path']].join('/'),
         [@base_url, services_configs['order_processing']['wsdl']].join('/')
       )
 
-      services << Services::UserService.new(
+      services << UserService.new(
         [@base_url, services_configs['user_service']['path']].join('/'),
         [@base_url, services_configs['user_service']['wsdl']].join('/')
       )
