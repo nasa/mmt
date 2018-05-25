40a1e58dd6a5f8386d7a8d20855b67057aa9cdab
Charles Huang <charles.huang@nasa.gov>
2018-05-25 10:04:46 -0400

    MMT-1386: comment out potentially problematic vcr config

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -6,7 +6,7 @@ VCR.configure do |c|
   c.allow_http_connections_when_no_cassette = false
   c.ignore_localhost = true
   c.default_cassette_options = { allow_playback_repeats: true }
-  c.after_http_request do |request,response| 
-  	Rails.logger.debug("Response #{request.method} #{request.uri} result : Headers: #{response.headers} - Body Size (bytes): #{response.body.to_s.bytesize} Status: #{response.status}")
-  end
+  # c.after_http_request do |request,response|
+  # 	Rails.logger.debug("Response #{request.method} #{request.uri} result : Headers: #{response.headers} - Body Size (bytes): #{response.body.to_s.bytesize} Status: #{response.status}")
+  # end
 end
