de8bfb99d074789a42b36dc2734a3e0b9e75985c
Monica Lyles <lamonica.h.lyles@nasa.gov>
2018-06-13 14:55:24 -0400

    MMT-1347 updated text for deleting a collection with granules

--- spec/features/collections/delete_collection_spec.rb
+++ spec/features/collections/delete_collection_spec.rb
@@ -69,7 +69,7 @@ describe 'Delete collection', js: true do
       end
 
       it 'does not allow the user to delete the collection' do
-        expect(page).to have_content('Collections with granules cannot be deleted')
+        expect(page).to have_content('This collection cannot be deleted using the MMT because it has associated granules.  Use the CMR API to delete the collection and its granules.')
       end
     end
   end
