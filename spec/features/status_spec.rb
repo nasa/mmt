d77402093dccc0b2f9a73b77ee29dafc74d302e3
Ziyan Zhang <ziyanone@gmail.com>
2018-06-28 11:09:01 -0400

    MMT-873 changed the status page to implement a middleware healthcheck for critical dependencies and fixed test cases for it

--- spec/features/status_spec.rb
+++ spec/features/status_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 describe 'Status check' do
   before do
-    visit '/status'
+    VCR.use_cassette('launchpad_status', record: :once) do
+      visit '/status'
+    end
   end
 
-  it 'displays "true" when the app is running' do
-    expect(page).to have_content('true')
+  it 'displays true for database and launchpad when both are healthy' do
+    expect(page).to have_text('{"database": true, "launchpad": true}')
   end
 end
