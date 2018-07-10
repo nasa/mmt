cf7f61bc8d95bacbc370a631695b53640752dc58
Ziyan Zhang <ziyanone@gmail.com>
2018-07-10 12:19:16 -0400

    MMT-873 added a timeout for the launchpad health page and made stlye changes

--- spec/features/status_spec.rb
+++ spec/features/status_spec.rb
@@ -9,5 +9,6 @@ describe 'Status check' do
 
   it 'displays true for database and launchpad when both are healthy' do
     expect(page).to have_text('{"database": true, "launchpad": true}')
+    expect(page.status_code).to eq(200)
   end
 end
