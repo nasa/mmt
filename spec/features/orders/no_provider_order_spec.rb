c64c2218c9903bba1f90f70d1d91f794585cf6bd
Thomas Cherry <thomas.a.cherry@nasa.gov>
2018-07-10 16:17:17 -0400

    MMT-1339 - fixing the issue related to non-existing provider orders

---
+++ spec/features/orders/no_provider_order_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe 'Viewing Provider Orders that do not exist' do
+  let(:order_guid) { 'not_a_real_order' }
+
+  before do
+    login(provider: 'DEV07', providers: %w[MMT_2 DEV07])
+  end
+
+  after do
+    login
+  end
+
+  context "when viewing a provider order that doesn't exist" do
+    before do
+      VCR.use_cassette('echo_soap/order_management_service/provider_orders/fake_order', record: :none) do
+        visit provider_order_path(order_guid)
+      end
+    end
+
+    it 'displays an error message about not existing' do
+      expect(page).to have_content("Could not find order with guid [#{order_guid}]")
+    end
+  end
+
+  context "when editing a provider order that doesn't exist" do
+    before do
+      VCR.use_cassette('echo_soap/order_management_service/provider_orders/fake_order', record: :none) do
+        visit edit_provider_order_path(order_guid)
+      end
+    end
+
+    it 'displays an error message about not existing' do
+      expect(page).to have_content("Could not find order with guid [#{order_guid}]")
+    end
+  end
+end
