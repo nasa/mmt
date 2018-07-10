d965b6fcd30f583a7daad863a54bd3d88d3ff928
Ziyan Zhang <ziyanone@gmail.com>
2018-07-10 12:19:16 -0400

    MMT-873 added a timeout for the launchpad health page and made stlye changes

--- app/middleware/middleware_healthcheck.rb
+++ app/middleware/middleware_healthcheck.rb
@@ -5,24 +5,28 @@ class MiddlewareHealthcheck
 
   def call(env)
     if env['PATH_INFO'.freeze] == '/status'.freeze
-      response = [200, { 'Content-Type' => 'application/json' }]
+      response = [503, { 'Content-Type' => 'application/json' }]
 
       # checks the database health
       begin
         db_healthy = ActiveRecord::Migrator.current_version != 0
-      rescue StandardError => e
-        Rails.logger.info "Database error: #{e}"
+      rescue StandardError
         db_healthy = false
       end
 
       # checks the health of launchpad
       cmr_client = Cmr::Client.client_for_environment(Rails.configuration.cmr_env, Rails.configuration.services)
-      launchpad_healthy = cmr_client.launchpad_healthcheck.body == 'OK'.freeze
+      launchpad_healthy = true
+      begin
+        timeout(2) { launchpad_healthy = cmr_client.launchpad_healthcheck.body == 'OK'.freeze }
+      rescue Timeout::Error
+        launchpad_healthy = false
+      end
       response[2] = ["{\"database\": #{db_healthy}, \"launchpad\": #{launchpad_healthy}}"]
 
-      # If launchpad is disabled then we will not report a 500 error if launchpad still fails
-      unless (ENV['launchpad_login_required'] != 'true' || launchpad_healthy) && db_healthy
-        response[0] = 503
+      # If launchpad is disabled then we will not report a 503 error if launchpad still fails
+      if (ENV['launchpad_login_required'] != 'true' || launchpad_healthy) && db_healthy
+        response[0] = 200
       end
       response
     else
