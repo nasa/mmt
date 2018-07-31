29685e7ffa7b2a434ae0852264bae2bdc60e1fd8
Ziyan Zhang <ziyanone@gmail.com>
2018-07-31 10:36:03 -0400

    MMT-1361 refactored the code in services and variables controller

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,14 +1,11 @@
 # :nodoc:
-class ServicesController < BaseManageController
+class ServicesController < BasePublishedRecordController
   include ManageMetadataHelper
 
-  before_action :set_service, only: %i[show edit clone destroy revisions revert download_json]
-  before_action :set_schema, only: %i[show edit clone destroy]
-  before_action :ensure_correct_service_provider, only: %i[edit clone destroy]
+  before_action :set_service, only: [:show, :edit, :clone, :destroy, :revisions, :revert, :download_json]
+  before_action :set_schema, only: [:show, :edit, :clone, :destroy]
+  before_action :ensure_correct_provider, only: [:edit, :clone, :destroy]
   before_action :set_preview, only: [:show]
-
-  # there is no variables index action, so not providing a link
-  add_breadcrumb 'Services'
 
   # https://stackoverflow.com/questions/34735540/action-defined-in-applicationcontroller-can-not-be-found
   def clone
@@ -17,28 +14,7 @@ class ServicesController < BaseManageController
 
   private
 
-  def set_schema
-    @schema = UmmJsonSchema.new('services', 'umm-s-json-schema.json')
-    @schema.fetch_references(@schema.parsed_json)
-  end
-
-  def set_preview
-    @preview = UmmPreview.new(
-      schema_type: 'service',
-      preview_filename: 'umm-s-preview.json',
-      data: @service
-    )
-  end
-
-  def ensure_correct_service_provider
-    return if current_provider?(@provider_id)
-
-    set_record_action
-
-    set_user_permissions
-
-    set_preview
-
-    render :show
+  def resource_schema
+    's'
   end
 end
--- app/controllers/variables_controller.rb
+++ app/controllers/variables_controller.rb
@@ -1,15 +1,12 @@
 # :nodoc:
-class VariablesController < BaseManageController
+class VariablesController < BasePublishedRecordController
   include ManageMetadataHelper
 
-  before_action :set_variable, only: %i[show edit clone destroy revisions revert download_json]
-  before_action :set_schema, only: %i[show edit clone destroy]
-  before_action :set_form, only: %i[show edit clone destroy]
-  before_action :ensure_correct_variable_provider, only: %i[edit clone destroy]
+  before_action :set_variable, only: [:show, :edit, :clone, :destroy, :revisions, :revert, :download_json]
+  before_action :set_schema, only: [:show, :edit, :clone, :destroy]
+  before_action :set_form, only: [:show, :edit, :clone, :destroy]
+  before_action :ensure_correct_provider, only: [:edit, :clone, :destroy]
   before_action :set_preview, only: [:show]
-
-  # there is no variables index action, so not providing a link
-  add_breadcrumb 'Variables'
 
   # https://stackoverflow.com/questions/34735540/action-defined-in-applicationcontroller-can-not-be-found
   def clone
@@ -18,40 +15,7 @@ class VariablesController < BaseManageController
 
   private
 
-  def set_schema
-    @schema = UmmJsonSchema.new('variables', 'umm-var-json-schema.json')
-    @schema.fetch_references(@schema.parsed_json)
-  end
-
-  def set_form
-    @json_form = UmmJsonForm.new('variables', 'umm-var-form.json', @schema, @variable, field_prefix: 'variable_draft/draft')
-  end
-
-  def set_preview
-    @preview = UmmPreview.new(
-      schema_type: 'variable',
-      preview_filename: 'umm-var-preview.json',
-      data: @variable
-    )
-  end
-
-  def ensure_correct_variable_provider
-    return if current_provider?(@provider_id)
-
-    set_record_action
-
-    set_user_permissions
-
-    set_preview
-
-    render :show
-  end
-
-  def draft_path(draft)
-    variable_draft_path(draft)
-  end
-
-  def create_from
-    VariableDraft.create_from_variable(@variable, current_user, @native_id)
+  def resource_schema
+    'var'
   end
 end
