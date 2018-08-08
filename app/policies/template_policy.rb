d0e54be855fc4a0342e132651d17590d0e708d8e
Ziyan Zhang <ziyanone@gmail.com>
2018-08-08 16:25:49 -0400

    MMT-37 added a index page and created tests for the controllers

--- app/policies/template_policy.rb
+++ app/policies/template_policy.rb
@@ -3,4 +3,11 @@ class TemplatePolicy < ApplicationPolicy
   def create_draft?
     user.user.provider_id == target.provider_id
   end
+
+  # :nodoc
+  class Scope < Scope
+    def resolve
+      scope.where(provider_id: user.user.provider_id)
+    end
+  end
 end
