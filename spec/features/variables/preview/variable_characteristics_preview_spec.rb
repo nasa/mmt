ea7feedf6a3d95dce338510d41026807fadb4a81
Hoan <hv.tranho@gmail.com>
2018-11-02 18:07:11 -0400

    MMT-1381 Add tests

--- spec/features/variables/preview/variable_characteristics_preview_spec.rb
+++ spec/features/variables/preview/variable_characteristics_preview_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 describe 'Valid Variable Variable Characteristics Preview', reset_provider: true do
   before do
     login
@@ -11,73 +9,29 @@ describe 'Valid Variable Variable Characteristics Preview', reset_provider: true
     it 'displays the stored values correctly within the preview' do
       within '.umm-preview.variable_characteristics' do
         expect(page).to have_css('h4', text: 'Variable Characteristics')
-        expect(page).to have_css('.umm-preview-field-container', count: 12)
+        expect(page).to have_css('.umm-preview-field-container', count: 5)
 
-        within '#variable_characteristics_standard_name_preview' do
-          expect(page).to have_css('h5', text: 'Standard Name')
-
-          expect(page).to have_css('p', text: 'Tortor Ultricies Nibh Adipiscing')
+        within '#variable_characteristics_index_ranges_lat_range_preview' do
+          expect(page).to have_css('h5', text: 'Lat Range')
+          expect(page).to have_css('h6', text: 'Lat Range 1')
+          expect(page).to have_css('p', text: '-90.0')
+          expect(page).to have_css('h6', text: 'Lat Range 2')
+          expect(page).to have_css('p', text: '90.0')
         end
 
-        within '#variable_characteristics_reference_preview' do
-          expect(page).to have_css('h5', text: 'Reference')
-
-          expect(page).to have_css('p', text: 'https://developer.earthdata.nasa.gov/')
+        within '#variable_characteristics_index_ranges_lon_range_preview' do
+          expect(page).to have_css('h5', text: 'Lon Range')
+          expect(page).to have_css('h6', text: 'Lon Range 1')
+          expect(page).to have_css('p', text: '-180.0')
+          expect(page).to have_css('h6', text: 'Lon Range 2')
+          expect(page).to have_css('p', text: '180.0')
         end
 
-        within '#variable_characteristics_coordinates_preview' do
-          expect(page).to have_css('h5', text: 'Coordinates')
-
-          expect(page).to have_css('p', text: '38.8059922,-77.0435327')
+        within '#variable_characteristics_group_path_preview' do
+          expect(page).to have_css('h5', text: 'Group Path')
+          expect(page).to have_css('p', text: '/Data_Fields/')
         end
 
-        within '#variable_characteristics_grid_mapping_preview' do
-          expect(page).to have_css('h5', text: 'Grid Mapping')
-
-          expect(page).to have_css('p', text: 'Mercator')
-        end
-
-        within '#variable_characteristics_size_preview' do
-          expect(page).to have_css('h5', text: 'Size')
-
-          expect(page).to have_css('p', text: 10.0)
-        end
-
-        within '#variable_characteristics_size_units_preview' do
-          expect(page).to have_css('h5', text: 'Size Units')
-
-          expect(page).to have_css('p', text: 'nm')
-        end
-
-        within '#variable_characteristics_bounds_preview' do
-          expect(page).to have_css('h5', text: 'Bounds')
-
-          expect(page).to have_css('p', text: 'UpperLeftPointMtrs = -180.0, 89.5; LowerRightMtrs = 177.5, -89.5')
-        end
-
-        within '#variable_characteristics_chunk_size_preview' do
-          expect(page).to have_css('h5', text: 'Chunk Size')
-
-          expect(page).to have_css('p', text: '100.0')
-        end
-
-        within '#variable_characteristics_structure_preview' do
-          expect(page).to have_css('h5', text: 'Structure')
-
-          expect(page).to have_css('p', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
-        end
-
-        within '#variable_characteristics_measurement_conditions_preview' do
-          expect(page).to have_css('h5', text: 'Measurement Conditions')
-
-          expect(page).to have_css('p', text: 'Nulla vitae elit libero, a pharetra augue.')
-        end
-
-        within '#variable_characteristics_reporting_conditions_preview' do
-          expect(page).to have_css('h5', text: 'Reporting Conditions')
-
-          expect(page).to have_css('p', text: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam.')
-        end
       end
     end
   end
--- spec/features/variables/preview/variable_information_preview_spec.rb
+++ spec/features/variables/preview/variable_information_preview_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 describe 'Valid Variable Variable Information Preview', reset_provider: true do
   before do
     login
@@ -10,7 +8,7 @@ describe 'Valid Variable Variable Information Preview', reset_provider: true do
   context 'When examining the Variable Information section' do
     it 'displays the stored values correctly within the preview' do
       within '.umm-preview.variable_information' do
-        expect(page).to have_css('.umm-preview-field-container', count: 11)
+        expect(page).to have_css('.umm-preview-field-container', count: 19)
 
         within '#variable_name_preview' do
           expect(page).to have_css('h5', text: 'Name')
@@ -18,10 +16,10 @@ describe 'Valid Variable Variable Information Preview', reset_provider: true do
           expect(page).to have_css('p', text: 'PNs_LIF')
         end
 
-        within '#variable_definition_preview' do
-          expect(page).to have_css('h5', text: 'Definition')
+        within '#variable_alias_preview' do
+          expect(page).to have_css('h5', text: 'Alias')
 
-          expect(page).to have_css('p', text: 'Volume mixing ratio of sum of peroxynitrates in air measured in units of Npptv (parts per trillion by volume)')
+          expect(page).to have_css('p', text: 'An Alias')
         end
 
         within '#variable_long_name_preview' do
@@ -30,10 +28,22 @@ describe 'Valid Variable Variable Information Preview', reset_provider: true do
           expect(page).to have_css('p', text: 'Volume mixing ratio of sum of peroxynitrates in air')
         end
 
+        within '#variable_definition_preview' do
+          expect(page).to have_css('h5', text: 'Definition')
+
+          expect(page).to have_css('p', text: 'Volume mixing ratio of sum of peroxynitrates in air measured in units of Npptv (parts per trillion by volume)')
+        end
+
         within '#variable_variable_type_preview' do
           expect(page).to have_css('h5', text: 'Variable Type')
 
           expect(page).to have_css('p', text: 'SCIENCE_VARIABLE')
+        end
+
+        within '#variable_variable_sub_type_preview' do
+          expect(page).to have_css('h5', text: 'Variable Sub Type')
+
+          expect(page).to have_css('p', text: 'SCIENCE_SCALAR')
         end
 
         within '#variable_units_preview' do
@@ -74,6 +84,45 @@ describe 'Valid Variable Variable Information Preview', reset_provider: true do
             expect(page).to have_css('h5', text: 'Max')
             expect(page).to have_css('p', text: '8836')
           end
+
+          within '#variable_valid_ranges_0_code_system_identifier_meaning_preview' do
+            expect(page).to have_css('h5', text: 'Code System Identifier Meaning')
+            expect(page).to have_css('h6', text: 'Code System Identifier Meaning 1')
+            expect(page).to have_css('h6', text: 'Code System Identifier Meaning 2')
+          end
+
+          within '#variable_valid_ranges_0_code_system_identifier_value_preview' do
+            expect(page).to have_css('h5', text: 'Code System Identifier Value')
+            expect(page).to have_css('h6', text: 'Code System Identifier Value 1')
+            expect(page).to have_css('h6', text: 'Code System Identifier Value 2')
+            expect(page).to have_css('h6', text: 'Code System Identifier Value 3')
+          end
+
+          expect(page).to have_css('h6', text: 'Valid Range 2')
+
+          within '#variable_valid_ranges_1_min_preview' do
+            expect(page).to have_css('h5', text: 'Min')
+            expect(page).to have_css('p', text: '0.0')
+          end
+
+          within '#variable_valid_ranges_1_max_preview' do
+            expect(page).to have_css('h5', text: 'Max')
+            expect(page).to have_css('p', text: '1.0')
+          end
+
+          within '#variable_valid_ranges_1_code_system_identifier_meaning_preview' do
+            expect(page).to have_css('h5', text: 'Code System Identifier Meaning')
+            expect(page).to have_css('h6', text: 'Code System Identifier Meaning 1')
+            expect(page).to have_css('h6', text: 'Code System Identifier Meaning 2')
+            expect(page).to have_css('h6', text: 'Code System Identifier Meaning 3')
+          end
+
+          within '#variable_valid_ranges_1_code_system_identifier_value_preview' do
+            expect(page).to have_css('h5', text: 'Code System Identifier Value')
+            expect(page).to have_css('h6', text: 'Code System Identifier Value 1')
+            expect(page).to have_css('h6', text: 'Code System Identifier Value 2')
+          end
+
         end
       end
     end
