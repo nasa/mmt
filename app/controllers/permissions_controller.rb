b914b7b3fc33bb1fd253ef49d83d1069e3684197
Hoan <hv.tranho@gmail.com>
2018-11-08 09:42:57 -0500

    MMT-1366 Add check for 'hits'

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -85,8 +85,9 @@ class PermissionsController < ManageCmrController
       }
 
       permission_search_response = cmr_client.get_permissions(@opts, token)
+      permission_found = permission_search_response.body.fetch("hits", 0) > 0
 
-      if permission_search_response.success?
+      if permission_search_response.success? && permission_found
         @revision_id = permission_search_response.body.fetch('items', [{}]).fetch(0, {})['revision_id']
 
         hydrate_groups(@permission)
