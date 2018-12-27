a7e55e5c7a905fc2cfb81b30364530ac53ed37fe
Thomas Cherry <thomas.a.cherry@nasa.gov>
2018-12-27 13:28:21 -0500

    MMT-1642 - adding settings to prevent X-Forward-Host manipluation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,9 +57,12 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  # https://github.com/ankane/secure_rails against host header injection
+  config.action_controller.default_url_options = {host: 'https://mmt.earthdata.nasa.gov'}
+  
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
-
+  config.action_controller.asset_host = 'https://mmt.earthdata.nasa.gov'
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -80,6 +83,8 @@ Rails.application.configure do
 
   # default host for url helpers
   Rails.application.routes.default_url_options[:host] = 'https://mmt.earthdata.nasa.gov'
+
+  
 
   # Feature Toggle for groups
   config.groups_enabled = true
--- config/environments/sit.rb
+++ config/environments/sit.rb
@@ -57,8 +57,11 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  # https://github.com/ankane/secure_rails against host header injection
+  config.action_controller.default_url_options = {host: 'https://mmt.sit.earthdata.nasa.gov'}
+  
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'https://mmt.sit.earthdata.nasa.gov'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
