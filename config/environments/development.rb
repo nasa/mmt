b5a01176aed0655b97264ba2b5f9f9a8be6c1043
Thomas Cherry <thomas.a.cherry@nasa.gov>
2018-12-27 13:28:21 -0500

    MMT-1642 - adding settings to prevent X-Forward-Host manipluation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,11 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # Next two properties should be uncommented if your testing X-Forward-Host protection
+  # https://github.com/ankane/secure_rails against host header injection
+  # config.action_controller.default_url_options = {host: 'mmt.localtest.earthdata.nasa.gov', port: '3000'}
+  # config.action_controller.asset_host = 'http://mmt.localtest.earthdata.nasa.gov:3000'
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: '3000' }
