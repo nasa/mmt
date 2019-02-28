2d463217ee2f8fe1b793ea61c8eb5670936d0c73
Charles Huang <charles.huang@nasa.gov>
2019-02-28 17:27:15 -0500

    MMT-1750: paginate generated variables
    * add pagination plugin
    * implement pagination
    * cleanup
    * add tests

--- app/helpers/variable_generation_helper.rb
+++ app/helpers/variable_generation_helper.rb
@@ -1,23 +1,20 @@
 module VariableGenerationHelper
-  # def long_name_stats(statistics)
-  #   "#{(statistics.fetch('long_names', 0).to_f / statistics.fetch('count', 0).to_f) * 100}% (#{statistics.fetch('long_names', 0)} of #{statistics.fetch('count', 0)})"
-  # end
-  #
-  # def definitions_stats(statistics)
-  #   "#{(statistics.fetch('definitions', 0).to_f / statistics.fetch('count', 0).to_f) * 100}% (#{statistics.fetch('definitions', 0)} of #{statistics.fetch('count', 0)})"
-  # end
-  #
-  # def science_keywords_stats(statistics)
-  #   "#{(statistics.fetch('keywords', 0).to_f / statistics.fetch('count', 0).to_f) * 100}% (#{statistics.fetch('keywords', 0)} of #{statistics.fetch('count', 0)})"
-  # end
-
   def self.define_stats_method(name)
     define_method("#{name}_stats") do |statistics|
-      "#{((statistics.fetch(name, 0).to_f / statistics.fetch('count', 0).to_f) * 100).round}% (#{statistics.fetch(name, 0)} of #{statistics.fetch('count', 0)})"
+      divisor = statistics.fetch('count', 0)
+      value = divisor.zero? ? 0 : (statistics.fetch(name, 0).to_f / divisor)
+
+      "#{(value * 100).round}% (#{statistics.fetch(name, 0)} of #{statistics.fetch('count', 0)})"
     end
   end
 
   define_stats_method 'long_names'
   define_stats_method 'definitions'
   define_stats_method 'keywords'
+
+  def display_single_page_info
+    if @variables.count > 0 && @variables.count <= 25
+      "Showing <b>all #{@variables.count}</b> Generated Variables".html_safe
+    end
+  end
 end
--- spec/features/variable_generation/naive_variable_generation_spec.rb
+++ spec/features/variable_generation/naive_variable_generation_spec.rb
@@ -31,20 +31,224 @@ describe 'Naive Variable Generation', reset_provider: true do
         within '#collection-search-results' do
           choose "selected_collection_#{@naive_uvg_col_ingest_response['concept-id']}"
         end
-
-        # stubbing the naive endpoint until it is live and we can determine
-        # if we should use VCR
-        response_path = File.join(Rails.root, 'spec', 'fixtures', 'variable_generation', 'small_stubbed_naive_response.json')
-        success_response_body = File.read(response_path)
-        uvg_generate_response = cmr_success_response(success_response_body)
-        allow_any_instance_of(Cmr::UvgClient).to receive(:uvg_generate).and_return(uvg_generate_response)
-
-        click_on 'Generate Variables'
       end
 
-      it 'generates the variables and displays them on the page' do
-        expect(page).to have_content('UMM Variable Generation')
-        expect(page).to have_content('Naive Variables')
+      context 'when there are no variables generated' do
+        before do
+          # stubbing the naive endpoint until it is live and we can determine if we should use VCR
+          response_path = File.join(Rails.root, 'spec', 'fixtures', 'variable_generation', 'zero_stubbed_naive_response.json')
+          success_response_body = File.read(response_path)
+          uvg_generate_response = cmr_success_response(success_response_body)
+          allow_any_instance_of(Cmr::UvgClient).to receive(:uvg_generate).and_return(uvg_generate_response)
+
+          click_on 'Generate Variables'
+        end
+
+        it 'displays the Variable Generation show (results) page' do
+          expect(page).to have_content('Variable Generation for MMT_2')
+          expect(page).to have_content('Naive Variables generated for collection C1238517344-GES_DISC')
+        end
+
+        it 'displays statistics for the generated Variables' do
+          expect(page).to have_content('Statistics')
+
+          within '.eui-info-box' do
+            expect(page).to have_content('Long Names: 0% (0 of 0)')
+            expect(page).to have_content('Definitions: 0% (0 of 0)')
+            expect(page).to have_content('Science Keywords: 0% (0 of 0)')
+          end
+        end
+
+        it 'indicates no variables were generated' do
+          within 'table#uvg-results-table' do
+            expect(page).to have_css('tr', count: 1, text: 'No variables generated.')
+          end
+        end
+
+        it 'does not display pagination information or links' do
+          expect(page).to have_no_css('.uvg-pagination-header')
+          expect(page).to have_no_css('.eui-pagination')
+        end
+      end
+
+      context 'when there are a handful of variables generated' do
+        before do
+          # stubbing the naive endpoint until it is live and we can determine if we should use VCR
+          response_path = File.join(Rails.root, 'spec', 'fixtures', 'variable_generation', 'small_stubbed_naive_response.json')
+          success_response_body = File.read(response_path)
+          uvg_generate_response = cmr_success_response(success_response_body)
+          allow_any_instance_of(Cmr::UvgClient).to receive(:uvg_generate).and_return(uvg_generate_response)
+
+          click_on 'Generate Variables'
+        end
+
+        it 'displays the Variable Generation show (results) page' do
+          expect(page).to have_content('Variable Generation for MMT_2')
+          expect(page).to have_content('Naive Variables generated for collection C1238517344-GES_DISC')
+        end
+
+        it 'displays statistics for the generated Variables' do
+          expect(page).to have_content('Statistics')
+
+          within '.eui-info-box' do
+            expect(page).to have_content('Long Names: 13% (2 of 16)')
+            expect(page).to have_content('Definitions: 56% (9 of 16)')
+            expect(page).to have_content('Science Keywords: 81% (13 of 16)')
+          end
+        end
+
+        it 'generates the variables and displays them on the page' do
+          expect(page).to have_content('Showing all 16 Generated Variables')
+
+          within '#uvg-results-table tbody' do
+            expect(page).to have_css('tr', count: 16)
+          end
+        end
+
+        it 'does not display pagination links' do
+          expect(page).to have_no_css('ul.eui-pagination')
+        end
+
+        context 'when clicking on a variable name' do
+          let(:modal_variable_json) { File.read(File.join(Rails.root, 'spec', 'fixtures', 'variable_generation', 'modal_variable_json.json')) }
+
+          before do
+            within '#uvg-results-table tbody' do
+              click_on 'TotalCounts_A'
+            end
+          end
+
+          it 'shows a modal with the variable json' do
+            within '#variable-preview-modal' do
+              expect(page).to have_content('Generated Variable Record: TotalCounts_A')
+              expect(page).to have_content(modal_variable_json)
+            end
+          end
+        end
+      end
+
+      context 'when there are a large number of variables generated' do
+        before do
+          # stubbing the naive endpoint until it is live and we can determine if we should use VCR
+          response_path = File.join(Rails.root, 'spec', 'fixtures', 'variable_generation', 'full_stubbed_naive_response.json')
+          success_response_body = File.read(response_path)
+          uvg_generate_response = cmr_success_response(success_response_body)
+          allow_any_instance_of(Cmr::UvgClient).to receive(:uvg_generate).and_return(uvg_generate_response)
+
+          click_on 'Generate Variables'
+        end
+
+        it 'displays the Variable Generation show (results) page' do
+          expect(page).to have_content('Variable Generation for MMT_2')
+          expect(page).to have_content('Naive Variables generated for collection C1238517344-GES_DISC')
+        end
+
+        it 'displays statistics for the generated Variables' do
+          expect(page).to have_content('Statistics')
+
+          within '.eui-info-box' do
+            expect(page).to have_content('Long Names: 0% (0 of 777)')
+            expect(page).to have_content('Definitions: 13% (100 of 777)')
+            expect(page).to have_content('Science Keywords: 12% (96 of 777)')
+          end
+        end
+
+        it 'generates the variables and displays them on the page' do
+          within '#uvg-results-table tbody' do
+            expect(page).to have_css('tr', count: 25)
+          end
+        end
+
+        it 'displays pagination information and links' do
+          within '.uvg-pagination-header' do
+            expect(page).to have_content('Showing Generated Variables 1 - 25 of 777')
+          end
+
+          within '.eui-pagination' do
+            expect(page).to have_no_css('a', text: 'Previous')
+            expect(page).to have_css('a', text: '1')
+            expect(page).to have_css('.active-page a', text: '1')
+            expect(page).to have_css('a', text: '2')
+            expect(page).to have_css('a', text: '3')
+            expect(page).to have_css('a', text: '4')
+            expect(page).to have_css('a', text: '5')
+            expect(page).to have_css('a', text: '32')
+            expect(page).to have_css('a', text: 'Next')
+          end
+        end
+
+        context 'when clicking on the Next link' do
+          before do
+            within '#uvg-pagination' do
+              click_on 'Next'
+            end
+          end
+
+          it 'displays the correct page and information' do
+            within '.uvg-pagination-header' do
+              expect(page).to have_content('Showing Generated Variables 26 - 50 of 777')
+            end
+
+            within '#uvg-results-table tbody' do
+              expect(page).to have_css('tr', count: 25)
+            end
+
+            within '.eui-pagination' do
+              expect(page).to have_css('.active-page a', text: '2')
+            end
+          end
+        end
+
+        context 'when clicking on the last page' do
+          before do
+            within '#uvg-pagination' do
+              click_on '32'
+            end
+          end
+
+          it 'displays the correct page and information' do
+            within '.uvg-pagination-header' do
+              expect(page).to have_content('Showing Generated Variables 776 - 777 of 777')
+            end
+
+            within '#uvg-results-table tbody' do
+              expect(page).to have_css('tr', count: 2)
+            end
+
+            within '.eui-pagination' do
+              expect(page).to have_css('a', text: 'Previous')
+              expect(page).to have_css('a', text: '1')
+              expect(page).to have_css('a', text: '28')
+              expect(page).to have_css('a', text: '29')
+              expect(page).to have_css('a', text: '30')
+              expect(page).to have_css('a', text: '31')
+              expect(page).to have_css('.active-page a', text: '32')
+              expect(page).to have_no_css('a', text: 'Next')
+            end
+          end
+
+          context 'when then clicking on the previous page' do
+            before do
+              within '#uvg-pagination' do
+                click_on 'Previous'
+              end
+            end
+
+            it 'displays the correct page and information' do
+              within '.uvg-pagination-header' do
+                expect(page).to have_content('Showing Generated Variables 751 - 775 of 777')
+              end
+
+              within '#uvg-results-table tbody' do
+                expect(page).to have_css('tr', count: 25)
+              end
+
+              within '.eui-pagination' do
+                expect(page).to have_css('.active-page a', text: '31')
+              end
+            end
+          end
+        end
       end
     end
   end
