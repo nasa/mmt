deee2b1d247e93006b7c7da131d834fe4836c65d
Hoan <hv.tranho@gmail.com>
2019-03-27 09:34:00 -0400

    MMT-1490 Update for ummc v1.10 to v1.12 migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190306143000) do
+ActiveRecord::Schema.define(version: 20190317214047) do
 
   create_table "drafts", force: :cascade do |t|
     t.integer  "user_id"
