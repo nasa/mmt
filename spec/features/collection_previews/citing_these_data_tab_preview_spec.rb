6195b30f148b623c891eab0682a2e297bb83789c
Hoan <hv.tranho@gmail.com>
2019-06-12 12:25:55 -0400

    MMT-1804 Update for 3 new tabs in preview gem

---
+++ spec/features/collection_previews/citing_these_data_tab_preview_spec.rb
@@ -0,0 +1,48 @@
+describe 'Citing These Data Tab preview' do
+  context 'when viewing the preview page' do
+    context 'when there is no metadata' do
+      before do
+        login
+        draft = create(:collection_draft, user: User.where(urs_uid: 'testuser').first)
+        visit collection_draft_path(draft)
+        find('.tab-label', text: 'Citing These Data').click
+      end
+
+      it 'does not display metadata' do
+        expect(page).to have_content('Citation information is not available for this collection.')
+      end
+
+    end
+
+    context 'when there is metadata' do
+      before do
+        login
+        draft = create(:full_collection_draft, user: User.where(urs_uid: 'testuser').first)
+        visit collection_draft_path(draft)
+        find('.tab-label', text: 'Citing These Data').click
+      end
+
+      it 'display metadata' do
+        within '#citing-these-data-panel' do
+          expect(page).to have_content('Title: Citation title')
+          expect(page).to have_content('Version: v1')
+          expect(page).to have_content('Creator: Citation creator')
+          expect(page).to have_content('Editor: Citation editor')
+          expect(page).to have_content('Publisher: Citation publisher')
+          expect(page).to have_content('Release Date: 2015-07-01T00:00:00Z')
+          expect(page).to have_content('Other Citation Details: Citation other details')
+          expect(page).to have_content('Linkage: http://example.com')
+
+          expect(page).to have_content('Title: Citation title 1')
+          expect(page).to have_content('Version: v2')
+          expect(page).to have_content('Creator: Citation creator 1')
+          expect(page).to have_content('Linkage: http://example2.com')
+
+          expect(page).to have_link('https://example.com/data-citation-policy')
+        end
+      end
+
+    end
+
+  end
+end
