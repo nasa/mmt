5cccba7398ac234238e4c57ff9e2bf4adbb01f75
Charles Huang <charles.huang@nasa.gov>
2019-07-01 00:17:15 -0400

    MMT-1825: uncomment tests and make them work

--- spec/features/collection_previews/collection_information_preview_spec.rb
+++ spec/features/collection_previews/collection_information_preview_spec.rb
@@ -1,43 +1,48 @@
-# NOTE: Most tests are being commented out because of preview gem overhaul
-# We should keep the tests to ensure everything is being tested, but old
-# tests should be removed when final changes are made.
+describe 'Collection information preview' do
+  context 'when viewing the preview page' do
+    context 'when there is no metadata' do
+      before do
+        login
+        draft = create(:collection_draft, user: User.where(urs_uid: 'testuser').first)
+        visit collection_draft_path(draft)
 
-# describe 'Collection information preview' do
-#   context 'when viewing the preview page' do
-#     context 'when there is no metadata' do
-#       before do
-#         login
-#         draft = create(:collection_draft, user: User.where(urs_uid: 'testuser').first)
-#         visit collection_draft_path(draft)
-#       end
-#
-#       it 'does not display metadata' do
-#         within 'ul.collection-information-preview' do
-#           expect(page).to have_no_content('Abstract')
-#           expect(page).to have_no_content('Purpose')
-#         end
-#       end
-#     end
-#
-#     context 'when there is metadata' do
-#       before do
-#         login
-#         draft = create(:full_collection_draft, user: User.where(urs_uid: 'testuser').first)
-#
-#         visit collection_draft_path(draft)
-#       end
-#
-#       it 'displays the metadata' do
-#         within 'ul.collection-information-preview' do
-#           within 'li.abstract' do
-#             expect(page).to have_content('This is a long description of the collection')
-#           end
-#
-#           within 'li.purpose' do
-#             expect(page).to have_content('This is the purpose field')
-#           end
-#         end
-#       end
-#     end
-#   end
-# end
+        find('.tab-label', text: 'Additional Information').click
+      end
+
+      it 'does not display metadata' do
+        within 'ul.collection-information-preview' do
+          expect(page).to have_no_content('Purpose')
+          expect(page).to have_no_content('Version Description')
+          expect(page).to have_no_content('DOI')
+        end
+      end
+    end
+
+    context 'when there is metadata' do
+      before do
+        login
+        draft = create(:full_collection_draft, user: User.where(urs_uid: 'testuser').first)
+        visit collection_draft_path(draft)
+
+        find('.tab-label', text: 'Additional Information').click
+      end
+
+      it 'displays the metadata' do
+        within 'ul.collection-information-preview' do
+          within 'li.purpose' do
+            expect(page).to have_content('This is the purpose field')
+          end
+
+          within 'li.version-description' do
+            expect(page).to have_content('Version 1 Description')
+          end
+
+          within 'li.doi' do
+            expect(page).to have_content('Citation DOI')
+            expect(page).to have_content('Citation DOI Authority')
+          end
+        end
+      end
+    end
+  end
+end
--- spec/features/collections/other_native_format_collections_spec.rb
+++ spec/features/collections/other_native_format_collections_spec.rb
@@ -5,32 +5,40 @@ describe 'Viewing Collections with other native formats', js: true do
     before do
       login
       visit manage_collections_path
-
-      fill_in 'keyword', with: short_name
-      click_on 'Search Collections'
-      click_link short_name
     end
 
-    it 'displays the collection and data center with correct role' do
-      expect(page).to have_content(short_name)
-      expect(page).to have_content('AIRS/Aqua L2G Precipitation Estimate (AIRS-only) V006 (AIRG2SSD) at GES DISC')
-
-      within '.collection-overview-table' do
-        expect(page).to have_content('Data Center(s): NASA/GSFC/SED/ESD/GCDC/GESDISC')
+    context 'when viewing the collection preview page' do
+      before do
+        fill_in 'keyword', with: short_name
+        click_on 'Search Collections'
+        click_link short_name
       end
-      # within '.data-centers-cards' do
-      #   within all('li.card')[0] do
-      #     within '.card-header' do
-      #       expect(page).to have_content('NASA/GSFC/SED/ESD/GCDC/GESDISC')
-      #       expect(page).to have_content('ARCHIVER')
-      #     end
-      #     within all('.card-body')[0] do
-      #       within '.card-body-details' do
-      #         expect(page).to have_content('Goddard Earth Sciences Data and Information Services Center (formerly Goddard DAAC), Global Change Data Center, Earth Sciences Division, Science and Exploration Directorate, Goddard Space Flight Center, NASA')
-      #       end
-      #     end
-      #   end
-      # end
+
+      it 'displays the data center in the collection preview overview table' do
+        expect(page).to have_content(short_name)
+        expect(page).to have_content('AIRS/Aqua L2G Precipitation Estimate (AIRS-only) V006 (AIRG2SSD) at GES DISC')
+
+        within '.collection-overview-table' do
+          expect(page).to have_content('Data Center(s): NASA/GSFC/SED/ESD/GCDC/GESDISC')
+        end
+      end
+
+      context 'when clicking to view the additional information tab' do
+        before do
+          find('.tab-label', text: 'Additional Information').click
+        end
+
+        it 'displays the data center and correct role' do
+          within '.data-centers-preview' do
+            within all('li.data-center')[0] do
+              expect(page).to have_content('NASA/GSFC/SED/ESD/GCDC/GESDISC')
+              expect(page).to have_content('ARCHIVER')
+
+              expect(page).to have_content('Goddard Earth Sciences Data and Information Services Center (formerly Goddard DAAC), Global Change Data Center, Earth Sciences Division, Science and Exploration Directorate, Goddard Space Flight Center, NASA')
+            end
+          end
+        end
+      end
     end
   end
 end
