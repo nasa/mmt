9fe08636f357e1d673b04f528e8de2d5bb752fc1
Charles Huang <charles.huang@nasa.gov>
2019-07-01 00:17:15 -0400

    MMT-1825: uncomment tests and make them work

--- spec/features/collection_previews/acquisition_information_preview_spec.rb
+++ spec/features/collection_previews/acquisition_information_preview_spec.rb
@@ -1,70 +1,43 @@
-# NOTE: Most tests are being commented out because of preview gem overhaul
-# We should keep the tests to ensure everything is being tested, but old
-# tests should be removed when final changes are made.
+describe 'Acquisition information preview' do
+  context 'when viewing the preview page' do
+    context 'when there is no metadata' do
+      before do
+        login
+        draft = create(:collection_draft, user: User.where(urs_uid: 'testuser').first)
+        visit collection_draft_path(draft)
+      end
 
-# describe 'Acquisition information preview' do
-#   context 'when viewing the preview page' do
-#     context 'when there is no metadata' do
-#       before do
-#         login
-#         draft = create(:collection_draft, user: User.where(urs_uid: 'testuser').first)
-#         visit collection_draft_path(draft)
-#       end
-#
-#       it 'does not display metadata' do
-#         expect(page).to have_content('No platforms or instruments have been added to this associated with this collection.')
-#       end
-#     end
-#
-#     context 'when there is metadata' do
-#       before do
-#         login
-#         draft = create(:full_collection_draft, user: User.where(urs_uid: 'testuser').first)
-#
-#         visit collection_draft_path(draft)
-#       end
-#
-#       it 'displays the metadata' do
-#         within '.platform-cards' do
-#           within all('li.card')[0] do
-#             within '.card-header' do
-#               expect(page).to have_content('Aircraft')
-#               expect(page).to have_content('A340-600')
-#             end
-#             within '.card-body' do
-#               expect(page).to have_content('Instruments')
-#               expect(page).to have_content('ATM')
-#               expect(page).to have_content('LVIS')
-#             end
-#           end
-#           within all('li.card')[1] do
-#             within '.card-header' do
-#               expect(page).to have_content('Earth Observation Satellites')
-#               expect(page).to have_content('SMAP')
-#             end
-#             within '.card-body' do
-#               expect(page).to have_content('Instruments')
-#               expect(page).to have_content('ADS')
-#               expect(page).to have_content('SMAP L-BAND RADIOMETER')
-#             end
-#           end
-#         end
-#
-#         within '.projects-table' do
-#           within all('tr')[1] do
-#             expect(page).to have_content('test 1 ShortName')
-#             expect(page).to have_content('test 1a Campaign')
-#             expect(page).to have_content('test 1b Campaign')
-#             expect(page).to have_content('2015-07-01 to 2015-12-25')
-#           end
-#           within all('tr')[2] do
-#             expect(page).to have_content('test 2 ShortName')
-#             expect(page).to have_content('test 2a Campaign')
-#             expect(page).to have_content('test 2b Campaign')
-#             expect(page).to have_content('2015-07-01 to 2015-12-25')
-#           end
-#         end
-#       end
-#     end
-#   end
-# end
+      it 'does not display metadata' do
+        within '#additional-information-panel' do
+          expect(page).to have_no_content('Projects')
+        end
+      end
+    end
+
+    context 'when there is metadata' do
+      before do
+        login
+        draft = create(:full_collection_draft, user: User.where(urs_uid: 'testuser').first)
+
+        visit collection_draft_path(draft)
+      end
+
+      it 'displays the metadata' do
+        within '.projects-table' do
+          within all('tr')[1] do
+            expect(page).to have_content('test 1 ShortName')
+            expect(page).to have_content('test 1a Campaign')
+            expect(page).to have_content('test 1b Campaign')
+            expect(page).to have_content('2015-07-01 to 2015-12-25')
+          end
+          within all('tr')[2] do
+            expect(page).to have_content('test 2 ShortName')
+            expect(page).to have_content('test 2a Campaign')
+            expect(page).to have_content('test 2b Campaign')
+            expect(page).to have_content('2015-07-01 to 2015-12-25')
+          end
+        end
+      end
+    end
+  end
+end
