f7726d49b2c966406a223dd42fc679fcf2378820
Ryan Miller <ryan.miller-1@nasa.gov>
2019-07-16 14:36:09 -0400

    hotfix/MMT-1838-2 fixing how the error message was being displayed for zero results for order tracking.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -35,7 +35,12 @@ class OrdersController < ManageCmrController
           orders_obj = Echo::Orders.new(client: echo_client, echo_provider_token: echo_provider_token, guids: guids)
 
           if orders_obj.errors
-            redirect_to orders_path, flash: { error: "#{I18n.t('controllers.orders.search.flash.error', error: orders_obj.errors)}Please refer to the ID: #{request.uuid} when contacting #{view_context.mail_to('support@earthdata.nasa.gov', 'Earthdata Support')}" }
+            if orders_obj.errors.match(/Could not find order with guid/)
+              err_message = I18n.t('controllers.orders.search.flash.guid', error: orders_obj.errors)
+            else
+              err_message = I18n.t('controllers.orders.search.flash.error', error: orders_obj.errors)
+            end
+            redirect_to orders_path, flash: { error: "#{err_message}Please refer to the ID: #{request.uuid} when contacting #{view_context.mail_to('support@earthdata.nasa.gov', 'Earthdata Support')}" }
             return
           end
 
