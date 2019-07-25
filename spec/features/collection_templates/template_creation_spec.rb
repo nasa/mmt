e9c65f260e3041531fe9b3f368b92617a9463170
Ryan Miller <ryan.miller-1@nasa.gov>
2019-07-25 08:14:18 -0400

    feature/MMT-36 Fixed show and clone from collection tests.

--- spec/features/collection_templates/template_creation_spec.rb
+++ spec/features/collection_templates/template_creation_spec.rb
@@ -1,6 +1,6 @@
 describe 'Create new collection template from cloning a collection', js: true do
   before :all do
-    @ingest_response, @concept_response = publish_collection_draft
+    @ingest_response, @concept_response = publish_collection_draft(short_name: 'Example1')
   end
 
   context 'when cloning a CMR collection as a template' do
@@ -18,11 +18,11 @@ describe 'Create new collection template from cloning a collection', js: true do
       end
 
       expect(page).to have_field('draft_template_name')
-      expect(page).to have_field('draft_short_name', with: 'stuff')
+      expect(page).to have_field('draft_short_name', with: 'Example1')
     end
 
     it 'cannot be saved without adding a name' do
-      before do
+      within '.nav-top' do
         click_on 'Save'
       end
 
@@ -33,53 +33,77 @@ describe 'Create new collection template from cloning a collection', js: true do
 end
 
 describe 'Create new collection template from cloning a collection with a different provider', js: true do
-  let(:short_name)  { 'Draft Title' }
-  let(:entry_title) { 'Tropical Forest Observation Record' }
-  let(:provider)    { 'MMT_2' }
+  modal_text = 'requires you change your provider context to MMT_2'
 
-  before do
-    login
-
-    create(:full_collection_draft, entry_title: entry_title, short_name: short_name, draft_entry_title: entry_title, draft_short_name: short_name, provider_id: provider)
-  end
-
-  let(:draft) { Draft.first }
-
-  context 'when the draft provider is in the users available providers', js: true do
+  context 'when viewing a collection' do
     before do
-      login(provider: 'MMT_1', providers: %w(MMT_1 MMT_2))
+      login
     end
 
-    context 'and the user tries to create a template' do
+    context 'when the collections provider is in the users available providers' do
       before do
-        visit collection_draft_path(draft)
-        click_on 'Save as Template'
+        ingest_response, _concept_response = publish_collection_draft(revision_count: 2)
+
+        login(provider: 'MMT_1', providers: %w(MMT_1 MMT_2))
+
+        visit collection_path(ingest_response['concept-id'])
       end
 
-      it 'has the right provider-switch message' do
-        expect(page).to have_content('Creating a template from this collection')
+      it 'displays the action links' do
+        expect(page).to have_link('Save as Template')
       end
 
-      it 'displays the new template page with data' do
-        click_on 'Yes'
+      context 'when clicking the save as template link' do
+        before do
+          click_on 'Save as Template'
+        end
 
-        expect(page).to have_field('draft_template_name')
-        expect(page).to have_field('draft_short_name', with: 'stuff')
+        it 'displays a modal informing the user they need to switch providers' do
+          expect(page).to have_content("Creating a template from this collection #{modal_text}")
+        end
+
+        context 'when clicking Yes' do
+          before do
+            # click_on 'Yes'
+            find('.not-current-provider-link').click
+            wait_for_jQuery
+          end
+
+          it 'switches the provider context' do
+            expect(User.first.provider_id).to eq('MMT_2')
+          end
+
+          context 'takes the user to the "new" page' do
+            it 'arrives at the "new" page' do
+              expect(page).to have_content('Template Name')
+            end
+
+            it 'and it can be saved with a name' do
+              fill_in 'Template Name', with: 'A unique name'
+              within '.nav-top' do
+                click_on 'Done'
+              end
+
+              expect(page).to have_content('A unique name')
+              expect(page).to have_content('Collection Template Created Successfully')
+            end
+          end
+        end
       end
     end
   end
 end
 
-describe 'Create new collection template from cloning a draft', js: true do
+#describe 'Create new collection template from cloning a draft', js: true do
   #click ze button
-end
+#end
 
-describe 'Create new collection template from scratch', js: true do
+#describe 'Create new collection template from scratch', js: true do
   #click ze button
   #error validation
   #invalid template modal
   #go back and try to make another
-end
+#end
 
 #collection_draft_edit does not have template name
 #collection_draft_edit does not lock a user from submitting for not having a template name
