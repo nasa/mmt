ba359fa36f9f6f6c4f5aa2683d5d2fddb5e9a878
Hoan <hv.tranho@gmail.com>
2019-08-06 14:25:28 -0400

    MMT-1666 Update collection draft proposal

---
+++ spec/features/collection_draft_proposals/collection_draft_proposal_update_spec.rb
@@ -0,0 +1,27 @@
+describe 'Collection Draft Proposal Update', js: true do
+  before do
+    login
+  end
+  context 'when updating an existing collection draft proposal' do
+    before do
+      set_as_draft_only_mmt
+      @collection_draft_proposal = create(:full_collection_draft_proposal)
+      visit edit_collection_draft_proposal_path(@collection_draft_proposal)
+    end
+
+    context 'when updating data' do
+      before do
+        fill_in 'Short Name', with: '123'
+        fill_in 'Abstract', with: 'collection abstract'
+        within '.nav-top' do
+          click_on 'Save'
+        end
+      end
+
+      it 'displays a confirmation message' do
+        save_screenshot '/tmp/s.png'
+        expect(page).to have_content('Collection Draft Proposal Updated Successfully!')
+      end
+    end
+  end
+end
