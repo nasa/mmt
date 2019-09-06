5e03795aad214b958d6f04a138a49239bfb63d38
Charles Huang <charles.huang@nasa.gov>
2019-09-06 14:34:25 -0400

    MMT-1862: fix filler, minor cleanup, add tests

--- spec/features/users/user_access_spec.rb
+++ spec/features/users/user_access_spec.rb
@@ -9,62 +9,78 @@ describe 'User Access' do
         require_urs_login
       end
 
-      context 'when a logged out user tries to view an access restricted page', js: true do
+      context 'when a user is logged out' do
         before do
-          visit search_path
-        end
-
-        it 'redirects them to Earthdata Login to log in' do
-          expect(page).to have_content('EARTHDATA LOGIN')
-        end
-      end
-
-      context 'when a logged out user tries to view a proposals page', js: true do
-        before do
-          visit manage_collection_proposals_path
-        end
-
-        it 'redirects them to Earthdata Login to log in' do
-          expect(page).to have_content('EARTHDATA LOGIN')
-        end
-      end
-
-      context 'when a logged in user tries to view an access restricted page' do
-        before do
-          login
-
-          visit search_path
-        end
-
-        after do
           visit logout_path
         end
 
-        it 'displays the page' do
-          expect(page).to have_content('Search Results')
-        end
-      end
+        context 'when a logged out user tries to view an access restricted page', js: true do
+          before do
+            visit search_path
+          end
 
-      context 'when a logged in user tries to view a proposals page' do
-        before do
-          login
-
-          visit manage_collection_proposals_path
+          it 'redirects them to Earthdata Login to log in' do
+            expect(page).to have_content('EARTHDATA LOGIN')
+          end
         end
 
-        after do
-          visit logout_path
-        end
+        context 'when a logged out user tries to view a proposals page', js: true do
+          before do
+            visit manage_collection_proposals_path
+          end
 
-        it 'redirects to the manage collections page' do
-          within 'main header' do
-            expect(page).to have_css('h2.current', text: 'Manage Collections')
-            expect(page).to have_content('Manage Variables')
-            expect(page).to have_content('Manage Services')
-            expect(page).to have_content('Manage CMR')
+          it 'redirects them to Earthdata Login to log in' do
+            expect(page).to have_content('EARTHDATA LOGIN')
           end
         end
       end
+
+      context 'when a user is logged in' do
+        before do
+          login
+        end
+
+        context 'when a logged in user tries to view an access restricted page' do
+          before do
+            visit search_path
+          end
+
+          it 'displays the page' do
+            expect(page).to have_content('Search Results')
+          end
+        end
+
+        context 'when a logged in user tries to view a proposals page' do
+          before do
+            visit manage_collection_proposals_path
+          end
+
+          it 'redirects to the manage collections page' do
+            within 'main header' do
+              expect(page).to have_css('h2.current', text: 'Manage Collections')
+              expect(page).to have_content('Manage Variables')
+              expect(page).to have_content('Manage Services')
+              expect(page).to have_content('Manage CMR')
+            end
+          end
+        end
+
+        context 'when a logged in user visits the welcome page' do
+          before do
+            visit root_url
+          end
+
+          it 'redirects to the manage collections page' do
+            within 'main header' do
+              expect(page).to have_css('h2.current', text: 'Manage Collections')
+              expect(page).to have_content('Manage Variables')
+              expect(page).to have_content('Manage Services')
+              expect(page).to have_content('Manage CMR')
+            end
+          end
+        end
+      end
+
     end
 
     context 'when Launchpad authentication is required' do
@@ -72,86 +88,104 @@ describe 'User Access' do
       # because launchpad will refuse the connection from the test server
       # so we test it in the controller test
 
-      context 'when a logged in user tries to view an access restricted page' do
+      context 'when a user is logged in' do
         before do
           require_launchpad_login
 
           login
-
-          visit search_path
         end
 
-        after do
-          visit logout_path
+        context 'when a logged in user tries to view an access restricted page' do
+          before do
+            visit search_path
+          end
+
+          it 'displays the page' do
+            expect(page).to have_content('Search Results')
+          end
         end
 
-        it 'displays the page' do
-          expect(page).to have_content('Search Results')
+        context 'when a logged in user tries to view a proposals page' do
+          before do
+            visit manage_collection_proposals_path
+          end
+
+          it 'redirects to the manage collections page' do
+            within 'main header' do
+              expect(page).to have_css('h2.current', text: 'Manage Collections')
+              expect(page).to have_content('Manage Variables')
+              expect(page).to have_content('Manage Services')
+              expect(page).to have_content('Manage CMR')
+
+              expect(page).to have_no_content('Manage Collection Proposals')
+            end
+          end
         end
-      end
 
-      context 'when a logged in user tries to view a proposals page' do
-        before do
-          login
+        context 'when a logged in user visits the welcome page' do
+          before do
+            visit root_url
+          end
 
-          visit manage_collection_proposals_path
-        end
-
-        after do
-          visit logout_path
-        end
-
-        it 'redirects to the manage collections page' do
-          within 'main header' do
-            expect(page).to have_css('h2.current', text: 'Manage Collections')
-            expect(page).to have_content('Manage Variables')
-            expect(page).to have_content('Manage Services')
-            expect(page).to have_content('Manage CMR')
-
-            expect(page).to have_no_content('Manage Collection Proposals')
+          it 'redirects to the manage collections page' do
+            within 'main header' do
+              expect(page).to have_css('h2.current', text: 'Manage Collections')
+              expect(page).to have_content('Manage Variables')
+              expect(page).to have_content('Manage Services')
+              expect(page).to have_content('Manage CMR')
+            end
           end
         end
       end
     end
 
     context 'when both URS and Launchpad login methods are available' do
-      context 'when a logged out user tries to view an access restricted page' do
+      before do
+        require_launchpad_and_urs_login
+      end
+
+      context 'when a user is logged out' do
         before do
-          make_launchpad_button_hidden(false)
-          require_launchpad_and_urs_login
-
-          visit search_path
-        end
-
-        after do
           visit logout_path
         end
 
-        it 'redirects the user to the landing page' do
-          expect(page).to have_content('About the Metadata Management Tool')
-        end
+        context 'when a logged out user tries to view an access restricted page' do
+          before do
+            visit search_path
+          end
 
-        it 'displays both login options' do
-          expect(page).to have_link('Login with Launchpad', href: login_path(login_method: 'launchpad'))
-          expect(page).to have_link('Login with Earthdata Login', href: login_path(login_method: 'urs'))
+          it 'redirects the user to the landing page' do
+            expect(page).to have_content('About the Metadata Management Tool')
+          end
+
+          it 'displays both login options' do
+            expect(page).to have_link('Login with Launchpad', href: login_path(login_method: 'launchpad'))
+            expect(page).to have_link('Login with Earthdata Login', href: login_path(login_method: 'urs'))
+          end
         end
       end
 
-      context 'when a logged in user tries to view an access restricted page' do
+      context 'when a user is logged in' do
         before do
-          require_launchpad_and_urs_login
-
           login
-
-          visit search_path
         end
 
-        after do
-          visit logout_path
-        end
+        context 'when a logged in user tries to view an access restricted page' do
+          before do
+            require_launchpad_and_urs_login
 
-        it 'displays the page' do
-          expect(page).to have_content('Search Results')
+            login
+
+            visit search_path
+          end
+
+          after do
+            visit logout_path
+          end
+
+          it 'displays the page' do
+            expect(page).to have_content('Search Results')
+          end
         end
       end
     end
@@ -162,56 +196,78 @@ describe 'User Access' do
       set_as_proposal_mode_mmt
     end
 
-    context 'when a logged out user tries to view an access restricted page', js: true do
+    context 'when a user is logged out' do
       before do
-        visit manage_collection_proposals_path
-      end
-
-      it 'redirects them to Earthdata Login to log in' do
-        expect(page).to have_content('EARTHDATA LOGIN')
-      end
-    end
-
-    context 'when a logged in user tries to view the manage collection proposals page' do
-      before do
-        login
-
-        visit manage_collection_proposals_path
-      end
-
-      after do
         visit logout_path
       end
 
-      it 'displays manage collection proposals page' do
-        within 'main header' do
-          expect(page).to have_css('h2.current', text: 'Manage Collection Proposals')
-          expect(page).to have_no_content('Manage Collections')
-          expect(page).to have_no_content('Manage Variables')
-          expect(page).to have_no_content('Manage Services')
-          expect(page).to have_no_content('Manage CMR')
+      context 'when a logged out user tries to view an access restricted page', js: true do
+        before do
+          visit manage_collection_proposals_path
+        end
+
+        it 'redirects them to Earthdata Login to log in' do
+          expect(page).to have_content('EARTHDATA LOGIN')
         end
       end
     end
 
-    context 'when a logged in user tries to view a MMT proper page' do
+    context 'when a user is logged in' do
       before do
         login
-
-        visit manage_variables_path
       end
 
-      after do
-        visit logout_path
+      context 'when a logged in user tries to view the manage collection proposals page' do
+        before do
+          visit manage_collection_proposals_path
+        end
+
+        it 'displays manage collection proposals page' do
+          within 'main header' do
+            expect(page).to have_css('h2.current', text: 'Manage Collection Proposals')
+            expect(page).to have_no_content('Manage Collections')
+            expect(page).to have_no_content('Manage Variables')
+            expect(page).to have_no_content('Manage Services')
+            expect(page).to have_no_content('Manage CMR')
+          end
+        end
       end
 
-      it 'redirects to the manage collection proposals page' do
-        within 'main header' do
-          expect(page).to have_css('h2.current', text: 'Manage Collection Proposals')
-          expect(page).to have_no_content('Manage Collections')
-          expect(page).to have_no_content('Manage Variables')
-          expect(page).to have_no_content('Manage Services')
-          expect(page).to have_no_content('Manage CMR')
+      context 'when a logged in user tries to view a MMT proper page' do
+        before do
+          login
+
+          visit manage_variables_path
+        end
+
+        after do
+          visit logout_path
+        end
+
+        it 'redirects to the manage collection proposals page' do
+          within 'main header' do
+            expect(page).to have_css('h2.current', text: 'Manage Collection Proposals')
+            expect(page).to have_no_content('Manage Collections')
+            expect(page).to have_no_content('Manage Variables')
+            expect(page).to have_no_content('Manage Services')
+            expect(page).to have_no_content('Manage CMR')
+          end
+        end
+      end
+
+      context 'when a logged in user visits the welcome page' do
+        before do
+          visit root_url
+        end
+
+        it 'redirects to the manage collection proposals page' do
+          within 'main header' do
+            expect(page).to have_css('h2.current', text: 'Manage Collection Proposals')
+            expect(page).to have_no_content('Manage Collections')
+            expect(page).to have_no_content('Manage Variables')
+            expect(page).to have_no_content('Manage Services')
+            expect(page).to have_no_content('Manage CMR')
+          end
         end
       end
     end
