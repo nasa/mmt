fa98e7d7b750c269054875583a4c9f127ffc5f8f
Ryan Miller <ryan.miller-1@nasa.gov>
2019-09-10 12:04:35 -0400

    MMT-1659 Updated tests; enabled JS and not_to => to have_no_x

--- spec/features/proposal/manage_collection_proposals/cannot_manage_cmr_spec.rb
+++ spec/features/proposal/manage_collection_proposals/cannot_manage_cmr_spec.rb
@@ -1,4 +1,4 @@
-describe 'Attempting to access the Manage CMR functionalities' do
+describe 'Attempting to access the Manage CMR functionalities', js: true do
   before do
     set_as_proposal_mode_mmt
     login
@@ -7,26 +7,26 @@ describe 'Attempting to access the Manage CMR functionalities' do
   it 'does not have a link to manage cmr' do
     visit manage_collection_proposals_path
 
-    expect(page).to have_content('Manage Collection Proposals')
+    expect(page).to have_content('MANAGE COLLECTION PROPOSALS')
     expect(page).to have_content('Create Collection Proposal')
-    expect(page).not_to have_link('Manage CMR')
+    expect(page).to have_no_link('Manage CMR')
   end
 
   it 'does not let the user reach the Manage CMR page with a direct link' do
     visit manage_cmr_path
 
-    expect(page).to have_content('Manage Collection Proposals')
+    expect(page).to have_content('MANAGE COLLECTION PROPOSALS')
     expect(page).to have_content('Create Collection Proposal')
-    expect(page).not_to have_content('Orders')
-    expect(page).not_to have_content('Data Quality Summaries')
+    expect(page).to have_no_content('Orders')
+    expect(page).to have_no_content('Data Quality Summaries')
   end
 
   it 'does not let the user reach the groups page' do
     visit groups_path
 
-    expect(page).to have_content('Manage Collection Proposals')
+    expect(page).to have_content('MANAGE COLLECTION PROPOSALS')
     expect(page).to have_content('Create Collection Proposal')
-    expect(page).not_to have_content('Orders')
-    expect(page).not_to have_content('Data Quality Summaries')
+    expect(page).to have_no_content('Orders')
+    expect(page).to have_no_content('Data Quality Summaries')
   end
 end
