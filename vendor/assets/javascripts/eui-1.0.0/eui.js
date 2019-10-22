a813387a0259eeba5a0a02de0adc80291510054d
Hoan <hv.tranho@gmail.com>
2019-10-22 13:37:18 -0400

    MMT-1998 Remove Google Analytics Javascript

--- vendor/assets/javascripts/eui-1.0.0/eui.js
+++ vendor/assets/javascripts/eui-1.0.0/eui.js
@@ -183,17 +183,6 @@
     });
   });
 }(jQuery));
-;(function () {
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','euiga');
-
-	euiga('create', 'UA-62340125-3', 'auto', {'name': 'eui_tracker'});
-	euiga('eui_tracker.send', 'pageview');
-	euiga('eui_tracker.send', 'event', 'eui', 'loaded', '1.0.0');
-})();;// leanModal v1.1 by Ray Stone - http://finelysliced.com.au
-// Dual licensed under the MIT and GPL
 
 (function($){$.fn.extend({leanModal:function(options){var defaults={top:100,overlay:0.5,closeButton:null};var overlay=$("<div id='lean_overlay'></div>");$("body").append(overlay);options=$.extend(defaults,options);return this.each(function(){var o=options;$(this).click(function(e){var modal_id=$(this).attr("href");$("#lean_overlay").click(function(){close_modal(modal_id)});$(o.closeButton).click(function(){close_modal(modal_id)});var modal_height=$(modal_id).outerHeight();var modal_width=$(modal_id).outerWidth();
 $("#lean_overlay").css({"display":"block",opacity:0});$("#lean_overlay").fadeTo(200,o.overlay);$(modal_id).css({"display":"block","position":"fixed","opacity":0,"z-index":11000,"left":50+"%","margin-left":-(modal_width/2)+"px","top":o.top+"px"});$(modal_id).fadeTo(200,1);e.preventDefault()})});function close_modal(modal_id){$("#lean_overlay").fadeOut(200);$(modal_id).css({"display":"none"})}}})})(jQuery);
