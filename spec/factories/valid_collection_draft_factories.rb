edd1174bffe78216e72a82054d5989875d86e141
John Farley <54851481+jdf-eos@users.noreply.github.com>
2019-11-21 11:32:58 -0500

    Mmt 2033 (#444)
    * 1) Reverted bourbon and neat gems to earlier an version that will work with Rails 5.
    2) Fixed problem with factory_girl double load (see Gemfile and spec/rails_helper.rb)
    3) Changed skip_filter method to R5's skip_before_action method to continue rails server to allow WelcomeController to initialize.
    
    * Fix race condition with helpers.
    
    * Revert to capybara 3.12.0,   same as master branch
    
    * Resetting capybara to 3.12.0
    
    * MMT-2033 fixes:
    1) Workaround for Rails changing datetime -> datetime-local.
    2) Capybara version upgrade to 3.19.0 to eliminate capybara warnings.
    3) Fixed use of eliminated Faker::Name fields. Changed to Faker::Superhero.
    
    * Changed comment for filter name change, for readability.
    
    * Updated comments to point out why certain changes were made.

--- spec/factories/valid_collection_draft_factories.rb
+++ spec/factories/valid_collection_draft_factories.rb
@@ -33,8 +33,8 @@ FactoryGirl.define do
       collection_one.merge(
         'CollectionDataType' => collection_data_type || 'SCIENCE_QUALITY',
         'Version'            => version || '1',
-        'EntryTitle'         => draft_entry_title || "#{Faker::Number.number(6)}_#{Faker::Job.title}",
-        'ShortName'          => draft_short_name || "#{Faker::Number.number(6)}_#{Faker::Superhero.name}"
+        'EntryTitle'         => draft_entry_title || "#{Faker::Number.number(digits: 6)}_#{Faker::Job.title}",
+        'ShortName'          => draft_short_name || "#{Faker::Number.number(digits: 6)}_#{Faker::Superhero.name}"
       )
     }
   end
