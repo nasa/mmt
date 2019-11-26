35c6b622cb70f0c2778f2aedac6f336bd1654c76
Ryan Miller <ryan.miller-1@nasa.gov>
2019-11-26 08:30:14 -0500

    MMT-1967 Updating migraiton comment to be accurate.

--- db/migrate/20191120153904_migration_for_proposal_publishing.rb
+++ db/migrate/20191120153904_migration_for_proposal_publishing.rb
@@ -1,10 +1,10 @@
 class MigrationForProposalPublishing < ActiveRecord::Migration
   def change
+    # The delete part of the migration will always work, but the update only
+    # works if in proposal mode.
     CollectionDraftProposal.where(request_type: 'delete', provider_id: nil).delete_all
     create_proposals = CollectionDraftProposal.where(request_type: 'create')
     create_proposals.each do |proposal|
-      # This makes the migration work for local environments without having to
-      # be concerned with which mode we are in.
       proposal.update_attribute('native_id', "dmmt_collection_#{proposal['id']}")
     end
   end
