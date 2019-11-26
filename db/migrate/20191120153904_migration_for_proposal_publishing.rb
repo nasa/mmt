d684b6eeefe59731bdf0ea1adad14e3fac991c48
Ryan Miller <ryan.miller-1@nasa.gov>
2019-11-26 15:45:57 -0500

    MMT-1967 Updating migration to be proposal_mode agnostic.

--- db/migrate/20191120153904_migration_for_proposal_publishing.rb
+++ db/migrate/20191120153904_migration_for_proposal_publishing.rb
@@ -1,11 +1,9 @@
 class MigrationForProposalPublishing < ActiveRecord::Migration
   def change
-    # The delete part of the migration will always work, but the update only
-    # works if in proposal mode.
     CollectionDraftProposal.where(request_type: 'delete', provider_id: nil).delete_all
     create_proposals = CollectionDraftProposal.where(request_type: 'create')
     create_proposals.each do |proposal|
-      proposal.update_attribute('native_id', "dmmt_collection_#{proposal['id']}")
+      proposal.update_column('native_id', "dmmt_collection_#{proposal['id']}")
     end
   end
 end
