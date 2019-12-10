fcfc3ef37143b089da24c6e4e61456b49ee802d7
Ryan Miller <ryan.miller-1@nasa.gov>
2019-12-10 09:42:45 -0500

    MMT-1930 Initial commit

--- lib/json_schema_form/umm_form_elements/umm_text_field.rb
+++ lib/json_schema_form/umm_form_elements/umm_text_field.rb
@@ -1,6 +1,7 @@
 # :nodoc:
 class UmmTextField < UmmFormElement
   def render_markup
+    byebug
     text_field_tag(keyify_property_name, text_value, element_properties(schema_fragment))
   end
 end
