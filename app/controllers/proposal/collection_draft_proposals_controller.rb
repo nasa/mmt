d4bcaa7f4ccbdce97b99b955499c2c9704943988
Ryan Miller <ryan.miller-1@nasa.gov>
2020-01-06 15:08:25 -0500

    MMT-1719 adding emails for approvers to submission.

--- app/controllers/proposal/collection_draft_proposals_controller.rb
+++ app/controllers/proposal/collection_draft_proposals_controller.rb
@@ -104,6 +104,10 @@ module Proposal
         Rails.logger.info("Audit Log: User #{current_user.urs_uid} successfully submitted #{resource_name.titleize} with title: '#{get_resource.entry_title}' and id: #{get_resource.id} (a #{get_resource.request_type} metadata request).")
 
         ProposalMailer.proposal_submitted_notification(get_user_info, get_resource.draft['ShortName'], get_resource.draft['Version'], params['id'], get_resource.request_type).deliver_now
+        approver_users = get_approver_emails(ENV['approvers_urs'].split(','))
+        approver_users.each do |approver|
+          ProposalMailer.proposal_submitted_approvers_notification(approver, get_resource, get_user_info).deliver_now
+        end
 
         flash[:success] = I18n.t("controllers.draft.#{plural_resource_name}.submit.flash.success")
       else
@@ -224,6 +228,15 @@ module Proposal
     def submitter_from_resource
       proposal_urs_user = retrieve_urs_users([get_resource.submitter_id])[0]
       { name: "#{proposal_urs_user['first_name']} #{proposal_urs_user['last_name']}", email: proposal_urs_user['email_address'] } unless proposal_urs_user.blank?
+    end
+
+    def get_approver_emails(approvers_urs)
+      approver_urs_users = retrieve_urs_users(approvers_urs)
+      approver_emails = []
+      approver_urs_users.each do |approver|
+        approver_emails.push(name: "#{approver['first_name']} #{approver['last_name']}", email: approver['email_address'])
+      end
+      approver_emails
     end
 
     # TODO: Investigate if this can be used to provide sorting to all drafts.
