63ae085d5ece8e3dc9fa1c5f67b3728e260c9c5c
Ryan Miller <ryan.miller-1@nasa.gov>
2020-01-06 08:38:35 -0500

    MMT-1718 Adding e-mail to reject proposal.

--- spec/mailers/previews/proposal_mailer_preview.rb
+++ spec/mailers/previews/proposal_mailer_preview.rb
@@ -33,4 +33,25 @@ class ProposalMailerPreview < ActionMailer::Preview
 
     ProposalMailer.proposal_published_notification(user, cmr_response_body, proposal)
   end
+
+  def new_proposal_rejected_notification
+    user = { name: 'Captain Planet', email: 'supergreen@bluemarble.com' }
+    proposal = CollectionDraftProposal.new
+    proposal.draft = { 'ShortName' => 'CIESIN_SEDAC_EPI_2010', 'Version' => '2010' }
+    # Every possible reason and a long note to show a 'worst case scenario' kind of e-mail
+    proposal.approver_feedback =
+      {
+        'reasons' => ['Missing Keywords', 'Insufficient Content', 'Misspellings/Grammar', 'Invalid/Incorrect Content', 'Broken Links', 'Duplicate Metadata', 'Other'],
+        'note' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam ut porttitor leo a diam. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Praesent elementum facilisis leo vel fringilla. Nisl condimentum id venenatis a. Proin libero nunc consequat interdum varius sit. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Aliquet bibendum enim facilisis gravida neque convallis. Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet. Egestas purus viverra accumsan in nisl nisi. Purus in mollis nunc sed id semper risus. Quis blandit turpis cursus in hac habitasse platea dictumst. Pellentesque elit eget gravida cum sociis natoque penatibus et.
+
+        Egestas congue quisque egestas diam. Libero volutpat sed cras ornare arcu dui vivamus arcu. Amet consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Varius duis at consectetur lorem donec massa sapien faucibus. Dolor purus non enim praesent. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend. Consectetur lorem donec massa sapien. Sed arcu non odio euismod lacinia at quis risus sed. Semper quis lectus nulla at. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis. In ante metus dictum at tempor commodo. Venenatis a condimentum vitae sapien pellentesque. Eu nisl nunc mi ipsum faucibus. Lorem sed risus ultricies tristique nulla aliquet enim. Nulla pharetra diam sit amet nisl.
+
+        Eget gravida cum sociis natoque penatibus et. Blandit turpis cursus in hac habitasse platea dictumst quisque. Curabitur gravida arcu ac tortor dignissim convallis aenean et tortor. Convallis a cras semper auctor neque. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla. At in tellus integer feugiat scelerisque varius morbi enim nunc. Leo vel fringilla est ullamcorper eget nulla facilisi etiam dignissim. Malesuada fames ac turpis egestas sed. Nunc scelerisque viverra mauris in aliquam sem fringilla. In fermentum posuere urna nec tincidunt praesent semper feugiat nibh. Mauris sit amet massa vitae tortor. Vitae et leo duis ut diam. Donec ac odio tempor orci dapibus ultrices in iaculis. Amet consectetur adipiscing elit duis tristique sollicitudin. Magna eget est lorem ipsum dolor.'
+      }
+
+    proposal.request_type = 'create'
+    proposal.id = 1
+
+    ProposalMailer.proposal_rejected_notification(user, proposal)
+  end
 end
