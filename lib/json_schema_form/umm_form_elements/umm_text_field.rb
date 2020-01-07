2f022e36b83b6095224b63954b89c61fb2afe5e6
Ryan Miller <ryan.miller-1@nasa.gov>
2020-01-07 11:23:53 -0500

    MMT-1930 Doing something else and don't want to stash

--- lib/json_schema_form/umm_form_elements/umm_text_field.rb
+++ lib/json_schema_form/umm_form_elements/umm_text_field.rb
@@ -1,7 +1,6 @@
 # :nodoc:
 class UmmTextField < UmmFormElement
   def render_markup
-    byebug
     text_field_tag(keyify_property_name, text_value, element_properties(schema_fragment))
   end
 end
