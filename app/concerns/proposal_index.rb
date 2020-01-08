dd138bd07c88e8819b1b52e0d61aef0bd9fccded
Ryan Miller <ryan.miller-1@nasa.gov>
2020-01-08 12:13:43 -0500

    MMT-2067 Adding comments to concern

--- app/concerns/proposal_index.rb
+++ app/concerns/proposal_index.rb
@@ -1,7 +1,11 @@
+# Helper methods used in both dMMT and MMT to help display proposal lists
+
 module ProposalIndex
   extend ActiveSupport::Concern
   include GroupEndpoints
 
+  # Pass in an array of proposals to extract and query URS for user names
+  # set a variable for use in other functions/views
   def set_urs_user_hash(proposals)
     submitters = proposals.map { |proposal| proposal['submitter_id'] }.uniq
     urs_users = retrieve_urs_users(submitters)
@@ -16,6 +20,7 @@ module ProposalIndex
     @query['sort_key'] = params['sort_key'] unless params['sort_key'].blank?
 
     sorted_proposals = proposals.all.sort do |a, b|
+                         # Making these arrays allows empty submitter_ids to sort last/first for ASC/DESC
                          a_name = @urs_user_hash[a.submitter_id] ? [0, @urs_user_hash[a.submitter_id]] : [1, @urs_user_hash[a.submitter_id]]
                          b_name = @urs_user_hash[b.submitter_id] ? [0, @urs_user_hash[b.submitter_id]] : [1, @urs_user_hash[b.submitter_id]]
                          if params['sort_key'] == 'submitter_id'
