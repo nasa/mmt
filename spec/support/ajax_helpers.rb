cb9370d5b36b7a7c5dca96aafcf888c08b7cbd33
Ryan Miller <ryan.miller-1@nasa.gov>
2020-01-29 08:19:45 -0500

    MMT-2125 removing debugging statement

--- spec/support/ajax_helpers.rb
+++ spec/support/ajax_helpers.rb
@@ -29,7 +29,6 @@ module Helpers
     end
 
     def finished_all_jQuery_requests?
-      puts "I am looping"
       # puts "checking jQuery requests. no active calls? #{page.evaluate_script('jQuery.active').zero?}"
       page.execute_script('return !!window.jQuery && jQuery.active === 0')
     end
