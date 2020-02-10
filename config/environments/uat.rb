0346f8a12cdbabc80e0be67cd50f8c7ab78cfdb6
Ryan Miller <ryan.miller-1@nasa.gov>
2020-02-10 14:18:09 -0500

    MMT-2137 Turning on mmt approver workflow

--- config/environments/uat.rb
+++ config/environments/uat.rb
@@ -103,7 +103,7 @@ Rails.application.configure do
   config.templates_enabled = true
 
   # Feature toggle for approver workflow in MMT.
-  config.mmt_approver_workflow_enabled = false
+  config.mmt_approver_workflow_enabled = true
 
   config.cmr_env = 'uat'
   config.echo_env = 'uat'
