e5e8dc47003516ef1b293b3b796d4fdd78db4c94
Charles Huang <charles.huang@nasa.gov>
2020-03-02 11:01:44 -0500

    MMT-2095: deal with deprecation warning for ActiveRecord::ConnectionAdapters::SQLite3Adapter.represent_boolean_as_integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,40 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200107163307) do
+ActiveRecord::Schema.define(version: 2020_03_02_154436) do
 
   create_table "draft_proposals", force: :cascade do |t|
-    t.integer  "user_id"
-    t.text     "draft"
-    t.string   "short_name"
-    t.string   "entry_title"
-    t.string   "provider_id"
-    t.string   "native_id"
-    t.string   "draft_type"
-    t.string   "proposal_status",   null: false
-    t.text     "approver_feedback"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.text     "status_history"
-    t.string   "request_type",      null: false
-    t.string   "submitter_id"
+    t.integer "user_id"
+    t.text "draft"
+    t.string "short_name"
+    t.string "entry_title"
+    t.string "provider_id"
+    t.string "native_id"
+    t.string "draft_type"
+    t.string "proposal_status", null: false
+    t.text "approver_feedback"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "status_history"
+    t.string "request_type", null: false
+    t.string "submitter_id"
   end
 
   create_table "drafts", force: :cascade do |t|
-    t.integer  "user_id"
-    t.text     "draft"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "short_name"
-    t.string   "entry_title"
-    t.string   "provider_id"
-    t.string   "native_id"
-    t.string   "draft_type"
+    t.integer "user_id"
+    t.text "draft"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "short_name"
+    t.string "entry_title"
+    t.string "provider_id"
+    t.string "native_id"
+    t.string "draft_type"
   end
 
   create_table "sessions", force: :cascade do |t|
-    t.string   "session_id", null: false
-    t.text     "data"
+    t.string "session_id", null: false
+    t.text "data"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
@@ -51,41 +51,41 @@ ActiveRecord::Schema.define(version: 20200107163307) do
   end
 
   create_table "templates", force: :cascade do |t|
-    t.integer  "user_id"
-    t.text     "draft"
-    t.string   "entry_title"
-    t.string   "provider_id"
-    t.string   "draft_type"
-    t.string   "template_name"
-    t.string   "short_name"
-    t.string   "native_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.integer "user_id"
+    t.text "draft"
+    t.string "entry_title"
+    t.string "provider_id"
+    t.string "draft_type"
+    t.string "template_name"
+    t.string "short_name"
+    t.string "native_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["provider_id", "template_name"], name: "index_templates_on_provider_id_and_template_name", unique: true
   end
 
   create_table "user_invites", force: :cascade do |t|
-    t.string   "manager_name"
-    t.string   "manager_email"
-    t.string   "user_first_name"
-    t.string   "user_last_name"
-    t.string   "user_email"
-    t.string   "group_id"
-    t.string   "group_name"
-    t.string   "provider"
-    t.string   "token"
-    t.boolean  "active",          default: true
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.string "manager_name"
+    t.string "manager_email"
+    t.string "user_first_name"
+    t.string "user_last_name"
+    t.string "user_email"
+    t.string "group_id"
+    t.string "group_name"
+    t.string "provider"
+    t.string "token"
+    t.boolean "active", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "urs_uid"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "echo_id"
-    t.string   "provider_id"
-    t.text     "available_providers"
+    t.string "urs_uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "echo_id"
+    t.string "provider_id"
+    t.text "available_providers"
   end
 
 end
