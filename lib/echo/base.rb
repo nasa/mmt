eda3dcee491e0eba865fd977ab3c7b49ffdbf673
jdfarley <john.farley@nasa.gov>
2020-03-02 08:57:55 -0500

    Removed extraneous debug messages.

--- lib/echo/base.rb
+++ lib/echo/base.rb
@@ -33,10 +33,8 @@ module Echo
       response = connection.post do |req|
         req.headers['Content-Type'] = 'text/xml'
         req.body = body
-        Rails.logger.debug("TBD connnection=#{connection.inspect}")
       end
 
-      Rails.logger.debug("TBD connnection response=#{response.inspect}")
       echo_response = Echo::Response.new(response)
       begin
 
