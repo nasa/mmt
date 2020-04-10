51009f087eb8970ca00c9ae6d4e72764dedc68fd
Hoan-Vu Tran-Ho <hoan.v.tranho@nasa.gov>
2020-04-10 14:16:33 -0400

    MMT-2077 Format env in email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,9 +3,9 @@ class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
 
   def add_env_if_needed(email_subject)
-    env = Rails.configuration.cmr_env
-    if env.downcase != 'ops'
-      email_subject = email_subject + " (#{env.upcase})"
+    env = Rails.configuration.cmr_env.upcase
+    if env != 'OPS'
+      email_subject = email_subject + " (#{env})"
     end
   end
 end
