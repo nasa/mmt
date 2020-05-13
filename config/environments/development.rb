f94f64c49cf60e7b0c3802014ed6a2d919591572
Ryan Miller <ryan.miller-1@nasa.gov>
2020-05-13 12:25:37 -0400

    MMT-2254 updating proposal publish logging and adding comment to facilitate development on the manage proposals page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,11 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  ##############################################################################
+  # need cache_classes to be true when trying to access the manage proposals   #
+  # page in dev, but it should generally be false for ease of development on   #
+  # any other page                                                             #
+  ##############################################################################
 
   # eager load on boot to avoid a Rails hang when using mmt/dmmt in the same rails app
   # as we do in testing.
