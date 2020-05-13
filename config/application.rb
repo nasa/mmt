b326ad7f09dd0c1da0c5103138bac45444f96fd2
John Farley <54851481+jdf-eos@users.noreply.github.com>
2020-05-13 15:19:16 -0400

    MMT-2216 Turned on Rails 5  CSRF improvements (per-form-csrf-tokens & forgery-… (#562)
    * Turned on Rails 5  CSRF improvements (per-form-csrf-tokens & forgery-protection-with-origin-check).
    Removed old framework initializers and replaced with a single config param, config.load_defaults 5.2
    
    * Revert framework initializers.
    
    * Removed redundant initializers.  Moved appropriate initializations from config/initializers to config/application.rb.
    
    * Removed extraneous spacing.

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ Bundler.require(*Rails.groups)
 module Mmt
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -66,6 +66,16 @@ module Mmt
 
     # Caches user information in orders for the specified period of time
     config.orders_user_cache_expiration = 15.minutes
+    
+    # Make `form_with` generate non-remote forms.
+    config.action_view.form_with_generates_remote_forms = false
+    
+    # Store boolean values are in sqlite3 databases as 1 and 0 instead of 't' and
+    # 'f' after migrating old data.
+    config.active_record.sqlite3.represent_boolean_as_integer = true
+  
+    # Don't require `belongs_to` associations by default. In Rails 5 default was changed to true.
+    config.active_record.belongs_to_required_by_default = false  
 
     def load_version
       version_file = "#{config.root}/version.txt"
