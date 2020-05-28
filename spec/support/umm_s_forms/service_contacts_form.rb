d6f04caef9b18ec1fc0047ed48a8eb3364a05c48
Charles Huang <charles.huang@nasa.gov>
2020-05-28 09:23:57 -0400

    MMT-2220: fix tests

--- spec/support/umm_s_forms/service_contacts_form.rb
+++ spec/support/umm_s_forms/service_contacts_form.rb
@@ -9,7 +9,7 @@ shared_examples_for 'Service Contacts Form with confirmation' do
   it 'displays the form with values along with a confirmation message' do
     expect(page).to have_content('Service Draft Updated Successfully!')
 
-    contact_groups_assertions
-    contact_persons_assertions
+    service_contact_groups_assertions
+    service_contact_persons_assertions
   end
 end
