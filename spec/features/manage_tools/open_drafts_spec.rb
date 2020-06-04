cbaf8f6afc2b3434a3b43f1e883d33054668ef0e
Thomas Cherry <thomas.a.cherry@nasa.gov>
2020-06-04 16:13:16 -0400

    MMT-2222 - initial spec file copied from services

--- spec/features/manage_tools/open_drafts_spec.rb
+++ spec/features/manage_tools/open_drafts_spec.rb
@@ -1,5 +1,5 @@
-describe 'Drafts listed on the Manage Tools page' do
-  draft_display_max_count = 5 # Should agree with @draft_display_max_count found in manage_tools_controller
+describe 'Drafts listed on the Manage Services page' do
+  draft_display_max_count = 5 # Should agree with @draft_display_max_count found in manage_services_controller
 
   before do
     @other_user_id = User.create(urs_uid: 'adminuser').id
@@ -10,14 +10,14 @@ describe 'Drafts listed on the Manage Tools page' do
 
   context 'when no drafts exist' do
     before do
-      visit manage_tools_path
+      visit manage_services_path
     end
 
     it 'does not display any drafts' do
-      expect(page).to have_content('MMT_2 Tool Drafts')
+      expect(page).to have_content('MMT_2 Service Drafts')
 
       within '.open-drafts' do
-        expect(page).to have_content('MMT_2 has no drafts to display')
+        expect(page).to have_content('MMT_2 has no drafts to display.')
       end
     end
   end
@@ -25,14 +25,14 @@ describe 'Drafts listed on the Manage Tools page' do
   context 'when there are open drafts from multiple users' do
     before do
       # create 2 drafts per user
-      2.times { create(:empty_tool_draft, user_id: @current_user_id) }
-      2.times { create(:empty_tool_draft, user_id: @other_user_id) }
+      2.times { create(:empty_service_draft, user_id: @current_user_id) }
+      2.times { create(:empty_service_draft, user_id: @other_user_id) }
 
-      visit manage_tools_path
+      visit manage_services_path
     end
 
     it 'displays all the drafts' do
-      expect(page).to have_content('MMT_2 Tool Drafts')
+      expect(page).to have_content('MMT_2 Service Drafts')
 
       within '.open-drafts' do
         expect(page).to have_content('<Blank Name>', count: 4)
@@ -46,14 +46,40 @@ describe 'Drafts listed on the Manage Tools page' do
       draft_display_max_count.times { create(:empty_service_draft, user_id: @current_user_id) }
       draft_display_max_count.times { create(:empty_service_draft, user_id: @other_user_id) }
 
-      visit manage_tools_path
+      visit manage_services_path
     end
 
-    # TODO this should be available when the index page is added, MMT_2222
-    it 'the "More" is displayed' # do
-    #   within '.open-drafts' do
-    #     expect(page).to have_link('More')
-    #   end
-    # end
+    it 'the "More" is displayed' do
+      within '.open-drafts' do
+        expect(page).to have_link('More')
+      end
+    end
+
+    context 'when clicking on the "More" link' do
+      before do
+        within '.open-drafts' do
+          click_on 'More'
+        end
+      end
+
+      it 'the Service Drafts index page is displayed with all open drafts for the provider' do
+        expect(page).to have_content('<Blank Name>', count: (draft_display_max_count * 2))
+      end
+
+      context 'when "<Blank Name>" is clicked on' do
+        before do
+          click_on '<Blank Name>', match: :first
+        end
+
+        it 'the draft is opened for view/edit' do
+          within '.eui-breadcrumbs' do
+            expect(page).to have_content('Service Drafts')
+          end
+
+          expect(page).to have_css('p', text: 'No value for Name provided.')
+          expect(page).to have_css('p', text: 'No value for Long Name provided.')
+        end
+      end
+    end
   end
 end
