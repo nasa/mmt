58bc4f36fef4c54ad39a54d0fbe140bb41724fbf
Christian Trummer <ctrummer@mbp-ctrummer.edn.ecs.nasa.gov>
2020-06-09 14:22:36 -0400

    DirectoryNames unchanged test

--- spec/features/collection_drafts/forms/hidden_fields_spec.rb
+++ spec/features/collection_drafts/forms/hidden_fields_spec.rb
@@ -4,16 +4,16 @@ describe 'Removed field', js: true do
   before do
     login
     draft = create(:full_collection_draft, user: User.where(urs_uid: 'testuser').first)
+    @draft1 = CollectionDraft.first
     visit edit_collection_draft_path(draft)
 
     within '.nav-top' do
       click_on "Save"
     end
-    @draft1 = CollectionDraft.first
   end
 
   it 'collection draft should contain the same DirectoryNames' do
-    @draft2 = CollectionDraft.first
-    expect(@draft2.draft['DirectoryNames']).to eql(@draft1.draft['DirectoryNames'])
+    draft2 = CollectionDraft.first
+    expect(draft2.draft['DirectoryNames']).to eql(@draft1.draft['DirectoryNames'])
   end
 end
