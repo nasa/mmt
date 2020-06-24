4e01072f0ba7045da49c5e70f29816f0b7e230b8
Ryan Miller <ryan.miller-1@nasa.gov>
2020-06-24 15:39:18 -0400

    MMT-2267 updating tooltip text for UMM-S URL

--- lib/json_schema_form/umm_preview_elements/umm_preview_link.rb
+++ lib/json_schema_form/umm_preview_elements/umm_preview_link.rb
@@ -4,7 +4,7 @@
 # :nodoc:
 class UmmPreviewLink < UmmPreviewElement
   def render
-    title = 'USE SERVICE API' if schema_type == 'service'
+    title = 'Use Service API' if schema_type == 'service' && full_key == 'URL/URLValue'
     link_to element_value, element_value, title: title
   end
 end
