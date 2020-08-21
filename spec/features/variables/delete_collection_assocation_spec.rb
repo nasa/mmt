8629d304fb3642eab8691ab0ef30b02d9389043b
Ryan Miller <ryan.miller-1@nasa.gov>
2020-08-21 10:31:29 -0400

    MMT-2329 adding tests for new modal content. elongating link on delete modal

--- spec/features/variables/delete_collection_assocation_spec.rb
+++ spec/features/variables/delete_collection_assocation_spec.rb
@@ -33,8 +33,55 @@ describe 'Deleting Variable Collection Associations', js: true, reset_provider: 
         click_link 'Delete Selected Association'
       end
 
+      it 'displays an appropriate warning about association cascade deleting a variable' do
+        expect(page).to have_content('Deleting a variable association will also delete the associated variable. If you wish to associate this variable to a different collection, before deleting this association, you should clone the associated variable and associate the clone to the correct collection.')
+      end
+
       it 'displays an appropriate confirmation message' do
         expect(page).to have_content('Are you sure you want to delete the selected collection association')
+        expect(page).to have_link('clone the associated variable', href: clone_variable_path(id: @variable_ingest_response['concept-id']))
+      end
+
+      context 'when clicking the clone link in the modal' do
+        before do
+          click_on 'clone the associated variable'
+        end
+
+        it 'displays the draft preview page' do
+          # Clone link opens a new tab in this case, this switches to that tab
+          within_window(switch_to_window(windows.last)) do
+            within '.eui-breadcrumbs' do
+              expect(page).to have_content('Variable Drafts')
+            end
+
+            expect(page).to have_content('Publish Variable Draft')
+            expect(page).to have_content('Delete Variable Draft')
+            expect(page).to have_content('Metadata Fields')
+            expect(page).to have_content('Variable Information')
+          end
+        end
+
+        it 'removes the Name and Long Name from the metadata' do
+          within_window(switch_to_window(windows.last)) do
+            within '#variable_draft_draft_name_preview' do
+              expect(page).to have_css('p', text: 'No value for Name provided.')
+            end
+
+            within '#variable_draft_draft_long_name_preview' do
+              expect(page).to have_css('p', text: 'No value for Long Name provided.')
+            end
+          end
+        end
+
+        it 'displays a message that the draft needs a unique Name' do
+          within_window(switch_to_window(windows.last)) do
+            expect(page).to have_content('Records must have a unique Name and Long Name within a provider. Click here to enter a new Name and Long Name.')
+          end
+        end
+
+        it 'does not close the delete modal' do
+          expect(page).to have_content('Deleting a variable association will also delete the associated variable. If you wish to associate this variable to a different collection, before deleting this association, you should clone the associated variable and associate the clone to the correct collection.')
+        end
       end
 
       context 'When clicking Yes on the confirmation dialog' do
