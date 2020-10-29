09d5e0b63a316deb330080b01a1ed8dff46475b6
ryanmiller-1 <40173609+ryanmiller-1@users.noreply.github.com>
2020-10-29 12:49:26 -0400

    MMT-2389 data migration for UMM-S v 1.3.4 (#659)
    * MMT-2389 data migration for UMM-S v 1.3.4
    
    * MMT-2389 only adding supportedreformattings with no outputs if the input does not already exist
    
    * MMT-2389 simplifying logic, adjusting comment, avoiding unnecessary fetch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_150827) do
+ActiveRecord::Schema.define(version: 2020_10_13_182007) do
 
   create_table "draft_proposals", force: :cascade do |t|
     t.integer "user_id"
