2e4f46b5ae918ad745353135c6b173b165d72d33
Ryan Miller <ryan.miller-1@nasa.gov>
2020-11-03 13:05:23 -0500

    MMT-2366 removing old code

--- app/concerns/cmr_collections_helper.rb
+++ app/concerns/cmr_collections_helper.rb
@@ -27,28 +27,8 @@ module CMRCollectionsHelper
     revisions
   end
 
-  def get_associated_services(response)
-    if response.present?
-      service_ids = response.dig(0, 'meta', 'associations', 'services')
-      # This can happen when there are variable associations and probably tool
-      # in the future.
-      @services = [] and return unless service_ids
-
-      cmr_service_response = cmr_client.get_services(concept_id: service_ids)
-
-      @services = if cmr_service_response.success?
-                    cmr_service_response.body['items']
-                  else
-                    Rails.logger.error("Error searching for associated services in 'set_associated_services': #{cmr_service_response.clean_inspect}")
-                    []
-                  end
-    else
-      @services = []
-    end
-  end
-
-  def get_associated_concepts(response)
-    ['tools', 'services'].each do |concept_type|
+  def get_associated_concepts(response, concepts = ['tools', 'services'])
+    concepts.each do |concept_type|
       if response.present?
         concept_ids = response.dig(0, 'meta', 'associations', concept_type)
         # This can happen when there are variable associations and probably tool
