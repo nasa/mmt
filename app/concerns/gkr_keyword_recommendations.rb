ac7d0cd8c92e2a047e953be2ff26e913ba0c1576
jdfarley <john.farley@nasa.gov>
2020-12-03 07:52:01 -0500

    MMT-2410 added specs for gkr logging.

--- app/concerns/gkr_keyword_recommendations.rb
+++ app/concerns/gkr_keyword_recommendations.rb
@@ -4,6 +4,7 @@ module GKRKeywordRecommendations
 
   def fetch_keyword_recommendations(user,provider)
     abstract = get_resource.draft.fetch('Abstract', '')
+    
     response = cmr_client.fetch_keyword_recommendations(abstract)
     
     if response.error?
@@ -17,8 +18,7 @@ module GKRKeywordRecommendations
   
   def log_gkr_request_failed( user, provider, abstract, response)
       Rails.logger.info("GkrLog: type: FAILED - date: #{Time.new} - env: #{Rails.env} - user_id: #{user} - provider: #{provider}"\
-        " - request_id: #{request_id} - abstract: #{abstract}"\
-        " - failure: #{response.inspect}")
+        " - abstract: #{abstract} - error_code: #{response.status} - failure_description: #{response.body['description']}")
   end
   
   def log_gkr_on_save_keywords(user, provider, abstract, request_id, recommendations, science_keywords)
@@ -32,7 +32,7 @@ module GKRKeywordRecommendations
         selected_keywords = science_keywords.map { | keyword | keyword.values.join(" > ")}
         accepted = recommended_keywords.split(',').select{|keyword| selected_keywords.include? keyword }
         rejected = recommended_keywords.split(',').reject{|keyword| selected_keywords.include? keyword}
-        Rails.logger.info("GkrLog: gkr_type: PUBLISH - date: #{Time.new} - env: #{Rails.env} - user_id: #{user_id} - provider: #{provider_id}"\
+        Rails.logger.info("GkrLog: type: PUBLISH - date: #{Time.new} - env: #{Rails.env} - user_id: #{user_id} - provider: #{provider_id}"\
                           " - request_id: #{request_id}"\
                           " - accepted_recommendations: #{accepted}"\
                           " - rejected_recommendations: #{rejected}")
