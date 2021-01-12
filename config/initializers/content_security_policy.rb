5c3e12578f6bf893158f2531c8b37f09e140b7cb
jdfarley <john.farley@nasa.gov>
2021-01-12 17:46:48 -0500

    MMT-2261 et. al.  Eliminate CSP violation reports due to inline style statements,  turn on CSP in SIT & UAT.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,33 +1,32 @@
 # Be sure to restart your server when you modify this file.
-
 # Define an application-wide content security policy
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-
-if Rails.configuration.csplog_enabled 
+if Rails.configuration.csplog_enabled
   # Report CSP violations to a specified URI
   Rails.application.config.content_security_policy_report_only = true
 
   Rails.application.config.content_security_policy do |policy|
-  ##  Enable all directives supported by Rails 5.2
-    policy.base_uri :self, :https
-    policy.child_src :self, :https
-    policy.connect_src :self, :https
-    policy.default_src :self, :https, :http
-    policy.font_src :self, :https, :http, :data
-    policy.form_action :self, :https
-    policy.frame_ancestors :self, :https
-    policy.frame_src :self, :https
-    policy.img_src :self, :https, :http
-    policy.manifest_src :self, :https, :http
-    policy.media_src :self, :https
+    ##  Enable all directives supported by Rails 5.2
+    policy.base_uri 'self'
+    policy.child_src 'self'
+    policy.connect_src 'self', '*.earthdata.nasa.gov', '*.sit.earthdata.nasa.gov', '*.uat.earthdata.nasa.gov'
+    policy.default_src 'self', '*.earthdata.nasa.gov', '*.sit.earthdata.nasa.gov', '*.uat.earthdata.nasa.gov', 'https://fonts.googleapis.com'
+    policy.font_src 'self', '*.earthdata.nasa.gov', '*.sit.earthdata.nasa.gov', '*.uat.earthdata.nasa.gov', 'https://fonts.googleapis.com'
+    policy.form_action 'self'
+    policy.frame_ancestors 'self'
+    policy.frame_src 'self'
+    policy.img_src 'self', '*.earthdata.nasa.gov', '*.sit.earthdata.nasa.gov', '*.uat.earthdata.nasa.gov', 'https://fonts.googleapis.com'
+    policy.manifest_src 'self'
+    policy.media_src 'self'
     policy.object_src :none
-    policy.script_src :self, :https, :http
-    policy.style_src :self, :https, :http
-    policy.worker_src :self, :https
-    
+    policy.script_src 'self'
+    policy.script_src 'self', '*.earthdata.nasa.gov', '*.sit.earthdata.nasa.gov', '*.uat.earthdata.nasa.gov'
+    policy.style_src 'self', '*.earthdata.nasa.gov'
+    policy.worker_src 'self'
+
     # Specify URI for violation reports
-    policy.report_uri "/report_csp_violation"
+    policy.report_uri '/report_csp_violation'
   end
 end
