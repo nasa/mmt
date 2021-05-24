1725a21c5c7f2298921afa1650d331138a45a6f5
jdfarley <john.farley@nasa.gov>
2021-05-24 16:07:56 -0400

    Revert "GKR-30  if the GKR service is unavailable, display a friendly, MMT-consistent error message to the user"
    This reverts commit 1b6fc3de7abdd22a57cd24a3c2b2042e8dbeae1e.

--- app/concerns/gkr_keyword_recommendations.rb
+++ app/concerns/gkr_keyword_recommendations.rb
@@ -4,24 +4,17 @@ module GKRKeywordRecommendations
 
   def fetch_keyword_recommendations(user, provider)
     abstract = get_resource.draft.fetch('Abstract', '')
-    begin
-      response = cmr_client.fetch_keyword_recommendations(abstract)
-    rescue => e
-      log_gkr_comm_failed(user, provider, abstract, 'CommFailure', 'Error communicating with GKR server.')
-      return { error: 'CommFailure' }
-    end
+    response = cmr_client.fetch_keyword_recommendations(abstract)
 
-    { id: '', recommendations: [] } if response.error?
+    if response.error?
+      log_gkr_request_failed(user, provider, abstract, response)
+      { id: '', recommendations: [] }
+    end
 
     keyword_recommendations = response.body['recommendations'].map { |rec| rec['keyword'] }
     { id: response.body['uuid'], recommendations: keyword_recommendations }
   end
 
-  def log_gkr_comm_failed(user, provider, abstract, error, description)
-    Rails.logger.info("GkrLog: type: FAILED - date: #{Time.new} - env: #{Rails.env} - user_id: #{user} - provider: #{provider}"\
-      " - abstract: #{abstract} - error_code: #{error} - failure_description: #{description}")
-  end
-  
   def log_gkr_request_failed(user, provider, abstract, response)
     Rails.logger.info("GkrLog: type: FAILED - date: #{Time.new} - env: #{Rails.env} - user_id: #{user} - provider: #{provider}"\
       " - abstract: #{abstract} - error_code: #{response.status} - failure_description: #{response.body['description']}")
