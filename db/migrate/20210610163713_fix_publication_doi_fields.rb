7fe867266861c06e0283cb4824bbd0eb4fff7bd7
Chris Trummer <42478387+ChristianTrummer99@users.noreply.github.com>
2021-06-14 12:54:36 -0400

    MMT-2603: Collections->Data Identification->Publication References: Form validation error even though 'DOI Not Available' is selected and populated (#747)
    * MMT-2603
    
    * MMT-2603: wrote migration along with migration tests
    
    * MMT-2603: adjustments
    
    * MMT-2603: added explicit means of verifying doi type
    
    * MMT-2603: added missing tests
    
    Co-authored-by: Christian Trummer <ctrummer@mbp-ctrummer.edn.ecs.nasa.gov>

---
+++ db/migrate/20210610163713_fix_publication_doi_fields.rb
@@ -0,0 +1,17 @@
+class FixPublicationDoiFields < ActiveRecord::Migration[5.2]
+
+  def change
+    collection_drafts = CollectionDraft.where.not(draft: {})
+    proposals = CollectionDraftProposal.where.not(draft: {})
+    templates = CollectionTemplate.where.not(draft: {})
+    records = collection_drafts + proposals + templates
+
+    records.each do |record|
+      record.draft.fetch('PublicationReferences',[]).each do |pub_ref|
+        pub_ref.delete('DOI') if pub_ref.dig('DOI', 'MissingReason') || pub_ref.dig('DOI', 'Explanation')
+      end
+
+      record.save
+    end
+  end
+end
