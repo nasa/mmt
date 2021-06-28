7ee5e7d5fbafcf0a70d9ad61b5adf975fc85e892
Chris Trummer <42478387+ChristianTrummer99@users.noreply.github.com>
2021-06-28 11:34:04 -0400

    MMT-2486: UMM-C draft progress circle links do not take users to appropriate field (#753)
    * MMT-2486: add correct anchors for collection information preview circles; fixed incorrect anchors for other preview circles
    
    * MMT-2486: fixed bug
    
    * MMT-2486: fixed bug in tests
    
    * MMT-2486: fixed preexisting bug
    
    Co-authored-by: Christian Trummer <ctrummer@mbp-ctrummer.edn.ecs.nasa.gov>

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -199,8 +199,8 @@ module FormHelper
 
   def mmt_label(options)
     options[:name] = add_pipes(options[:name])
-    id = remove_pipes(options[:prefix] + options[:name]) if options[:set_id]
-    label_for = id.nil? ? remove_pipes(options[:prefix] + options[:name]) : nil
+    id = remove_pipes(options[:prefix] + options[:name] + '_label') if options[:label_id]
+    label_for = remove_pipes(options[:prefix] + options[:name])
 
     classes = []
     classes << 'required' if options[:required]
