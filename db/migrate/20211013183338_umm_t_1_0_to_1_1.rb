45e249fa79d70102f9240d2b1c6c1978bfd5e042
Hoan-Vu Tran-Ho <hoan.v.tranho@nasa.gov>
2021-10-13 17:03:57 -0400

    MMT-2712 Add down method

--- db/migrate/20211013183338_umm_t_1_0_to_1_1.rb
+++ db/migrate/20211013183338_umm_t_1_0_to_1_1.rb
@@ -5,4 +5,11 @@ class UmmT10To11 < ActiveRecord::Migration[5.2]
       tool_draft.draft.delete('SearchAction')
     end
   end
+
+  def down
+    tool_drafts = ToolDraft.all
+    tool_drafts.each do |tool_draft|
+      tool_draft.draft.delete('PotentialAction')
+    end
+  end
 end
