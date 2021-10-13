3f0486c4e4f89397d966058caffa5d5669524b47
Hoan-Vu Tran-Ho <hoan.v.tranho@nasa.gov>
2021-10-13 14:52:25 -0400

    MMT-2743 Add db migration

---
+++ db/migrate/20211013183338_umm_t_1_0_to_1_1.rb
@@ -0,0 +1,8 @@
+class UmmT10To11 < ActiveRecord::Migration[5.2]
+  def up
+    tool_drafts = ToolDraft.all
+    tool_drafts.each do |tool_draft|
+      tool_draft.draft.delete('SearchAction')
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_171616) do
+ActiveRecord::Schema.define(version: 2021_10_13_183338) do
 
   create_table "draft_proposals", force: :cascade do |t|
     t.integer "user_id"
