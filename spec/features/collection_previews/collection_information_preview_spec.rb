38566c02a9c10ffcd90453552f8f564dd1689157
Thanhtam <tle1989@yahoo.com>
2022-02-16 14:29:12 -0500

    MMT-2811:FIXED:rspec./spec/features/collection_previews/collection_information_preview_spec.rb:12 # BY COMMENTED OUT LINE 8, 13, AND CHANGE within 'ul.collectiontion-information-preview' do TO within *args '#metadata_preview' do

--- spec/features/collection_previews/collection_information_preview_spec.rb
+++ spec/features/collection_previews/collection_information_preview_spec.rb
@@ -1,18 +1,17 @@
-describe 'Collection information preview' do
+describe 'Collection information preview',js:true do
   context 'when viewing the preview page' do
     context 'when there is no metadata' do
       before do
         login
         draft = create(:collection_draft, user: User.where(urs_uid: 'testuser').first)
         visit collection_draft_path(draft)
-
-        find('.tab-label', text: 'Additional Information').click
+        #find('.tab-label', text: 'Additional Information').click
       end
 
       it 'does not display metadata' do
-        within 'ul.collection-information-preview' do
-          expect(page).to have_no_content('Purpose')
-          expect(page).to have_no_content('Version Description')
+          within 'ul.collection-information-preview' do
+            expect(page).to have_no_content('Purpose')
+            expect(page).to have_no_content('Version Description')
         end
       end
     end
@@ -22,21 +21,22 @@ describe 'Collection information preview' do
         login
         draft = create(:full_collection_draft, user: User.where(urs_uid: 'testuser').first)
         visit collection_draft_path(draft)
-
-        find('.tab-label', text: 'Additional Information').click
+        #find('.tab-label', text: 'Additional Information').click
       end
 
       it 'displays the metadata' do
-        within 'ul.collection-information-preview' do
-          within 'li.purpose' do
+        within '#metadata-preview' do
+          #within 'ul.collection-information-preview' do
+          #within 'li.purpose' do
             expect(page).to have_content('This is the purpose field')
           end
 
-          within 'li.version-description' do
+          within '#metadata-preview' do
+            #within 'li.version-description' do
             expect(page).to have_content('Version 1 Description')
           end
         end
       end
     end
   end
-end
+
