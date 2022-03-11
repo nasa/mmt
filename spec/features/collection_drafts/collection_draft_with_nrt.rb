5192c78e9498cbdc5609ce8e0fd522bb7f0b83cd
Charles Huang <charles.huang@nasa.gov>
2022-03-11 13:25:11 -0500

    MMT-2796: update UMM-C to v1.16.7 with support and badges for Low Latency and Expedited

--- spec/features/collection_drafts/collection_draft_with_nrt.rb
+++ spec/features/collection_drafts/collection_draft_with_nrt.rb
@@ -1,19 +1,48 @@
-describe 'Collection drafts with NEAR_REAL_TIME' do
+describe 'Collection drafts with Near Real Time Values' do
+  let(:collection_draft) { create(:full_collection_draft, user: User.where(urs_uid: 'testuser').first) }
+
   before do
     login
   end
 
-  context 'When viewing a collection draft with NEAR_REAL_TIME' do
+  context 'When viewing a collection draft with `NEAR_REAL_TIME`' do
     before do
-      @draft = create(:full_collection_draft, user: User.where(urs_uid: 'testuser').first)
-      @draft.draft['CollectionDataType'] = 'NEAR_REAL_TIME'
-      @draft.save
-      visit collection_draft_path(@draft)
+      collection_draft.draft['CollectionDataType'] = 'NEAR_REAL_TIME'
+      collection_draft.save
+      visit collection_draft_path(collection_draft)
     end
 
     it 'displays the NRT badge' do
       within '#collection-general-overview .collection-title .title-nrt-badge' do
         expect(page).to have_css('span.eui-badge.nrt', text: 'NRT')
+      end
+    end
+  end
+
+  context 'when viewing a collection draft with `LOW_LATENCY`' do
+    before do
+      collection_draft.draft['CollectionDataType'] = 'LOW_LATENCY'
+      collection_draft.save
+      visit collection_draft_path(collection_draft)
+    end
+
+    it 'displays the LOW LATENCY badge' do
+      within '#collection-general-overview .collection-title .title-nrt-badge' do
+        expect(page).to have_css('span.eui-badge.nrt', text: 'LOW LATENCY')
+      end
+    end
+  end
+
+  context 'when viewing a collection draft with `EXPEDITED`' do
+    before do
+      collection_draft.draft['CollectionDataType'] = 'EXPEDITED'
+      collection_draft.save
+      visit collection_draft_path(collection_draft)
+    end
+
+    it 'displays the EXPEDITED badge' do
+      within '#collection-general-overview .collection-title .title-nrt-badge' do
+        expect(page).to have_css('span.eui-badge.nrt', text: 'EXPEDITED')
       end
     end
   end
--- spec/features/collections/collection_with_nrt_spec.rb
+++ spec/features/collections/collection_with_nrt_spec.rb
@@ -1,17 +1,81 @@
-describe 'Collections with NEAR_REAL_TIME' do
+describe 'Collections with Near Real Time Values' do
   before(:all) do
-    @ingest_response, _concept_response = publish_collection_draft(collection_data_type: 'NEAR_REAL_TIME')
+    @nrt_ingest_response, _concept_response = publish_collection_draft(collection_data_type: 'NEAR_REAL_TIME')
+    @ll_ingest_response, _concept_response = publish_collection_draft(collection_data_type: 'LOW_LATENCY')
+    @expedited_ingest_response, _concept_response = publish_collection_draft(collection_data_type: 'EXPEDITED')
   end
 
-  context 'When viewing a collection with NEAR_REAL_TIME' do
+  context 'When viewing a collection with `NEAR_REAL_TIME`' do
     before do
       login
-      visit collection_path(@ingest_response['concept-id'])
+      visit collection_path(@nrt_ingest_response['concept-id'])
     end
 
     it 'displays the NRT badge' do
       within '#collection-general-overview .collection-title .title-nrt-badge' do
         expect(page).to have_css('span.eui-badge.nrt', text: 'NRT')
+      end
+    end
+
+    context 'when visiting the Revisions page' do
+      before do
+        click_on 'Revisions (1)'
+      end
+
+      it 'displays the NRT badge' do
+        within '.collection-basics' do
+          expect(page).to have_css('span.eui-badge.nrt', text: 'NRT')
+        end
+      end
+    end
+  end
+
+  context 'When viewing a collection with `LOW_LATENCY`' do
+    before do
+      login
+      visit collection_path(@ll_ingest_response['concept-id'])
+    end
+
+    it 'displays the LOW LATENCY badge' do
+      within '#collection-general-overview .collection-title .title-nrt-badge' do
+        expect(page).to have_css('span.eui-badge.nrt', text: 'LOW LATENCY')
+      end
+    end
+
+    context 'when visiting the Revisions page' do
+      before do
+        click_on 'Revisions (1)'
+      end
+
+      it 'displays the LOW LATENCY badge' do
+        within '.collection-basics' do
+          expect(page).to have_css('span.eui-badge.nrt', text: 'LOW LATENCY')
+        end
+      end
+    end
+  end
+
+  context 'When viewing a collection with `EXPEDITED`' do
+    before do
+      login
+      visit collection_path(@expedited_ingest_response['concept-id'])
+    end
+
+    it 'displays the EXPEDITED badge' do
+      within '#collection-general-overview .collection-title .title-nrt-badge' do
+        expect(page).to have_css('span.eui-badge.nrt', text: 'EXPEDITED')
+      end
+    end
+
+    context 'when visiting the Revisions page' do
+      before do
+        click_on 'Revisions (1)'
+      end
+
+      it 'displays the EXPEDITED badge' do
+        within '.collection-basics' do
+          expect(page).to have_css('span.eui-badge.nrt', text: 'EXPEDITED')
+        end
       end
     end
   end
