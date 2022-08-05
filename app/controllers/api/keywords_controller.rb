8a95660cc493990a9015fe075d53ce38564122ff
tranhohoanvu <hoan.v.tranho@nasa.gov>
2022-08-05 17:19:32 -0400

    MMT-2979: Implement save draft and get keywords

---
+++ app/controllers/api/keywords_controller.rb
@@ -0,0 +1,33 @@
+class Api::KeywordsController < BaseDraftsController
+  include ManageMetadataHelper
+
+  before_action :proposal_approver_permissions, except: [:show]
+  skip_before_action :ensure_user_is_logged_in, only: [:show]
+  skip_before_action :add_top_level_breadcrumbs, only: [:show]
+  skip_before_action :set_resource, only: [:show]
+
+  def fetch(keyword_scheme)
+    response = cmr_client.get_controlled_keywords(keyword_scheme)
+    if response.success?
+      response.body
+    else
+      []
+    end
+  end
+
+  def show
+    keyword_scheme = params[:id]
+    keywords = fetch(keyword_scheme)
+    if keyword_scheme == 'science_keywords'
+      if keywords.key? 'category'
+        keywords['category'].each do |category|
+          if category['value'] == 'EARTH SCIENCE'
+            keywords['category'].delete(category)
+            break
+          end
+        end
+      end
+    end
+    render json: JSON.pretty_generate(keywords)
+  end
+end
