fd3527e90bfb4d3a85be6dc0c50c36fc1886cee3
Christopher Gokey <christopher.d.gokey@nasa.gov>
2022-10-04 13:13:49 -0400

    MMT-2997: added debug message for francell.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -382,7 +382,9 @@ class ApplicationController < ActionController::Base
       end
     end
 
+    puts("******* francell token=#{token}")
     response = echo_client.get_provider_context_token(token, behalfOfProvider: current_user.provider_id)
+    puts("******* francell response=#{response.body}, error=#{response.error?}")
     if response.error?
       return
     end
