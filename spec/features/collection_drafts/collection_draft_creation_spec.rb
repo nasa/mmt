2ff4d4e1ec2a225ae88ea3b58597d6551f881fe1
William Valencia <william.m.valencia@nasa.gov>
2022-10-04 10:22:42 -0400

    MMT-3046: Adding the shortname to the download file name

--- spec/features/collection_drafts/collection_draft_creation_spec.rb
+++ spec/features/collection_drafts/collection_draft_creation_spec.rb
@@ -60,7 +60,7 @@ describe 'Collection Draft creation', js: true do
           context 'when downloading the JSON' do
             let(:id)  {current_url.gsub(/.*\//, '')}
             before do
-              @file = "#{Rails.root}/#{id}.json"
+              @file = "#{Rails.root}/123-#{id}.json"
               click_on 'Download JSON'
 
               # Seems to need a brief (>0.1) pause to actually find the file.
