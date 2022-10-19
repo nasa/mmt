04e1af91939ac67a0205fd16c466681f2cd3da5d
Thanhtam <tle1989@yahoo.com>
2022-10-19 15:58:42 -0400

    MMT-3054: Manually generated group_name for the test to use

--- spec/support/group_helper.rb
+++ spec/support/group_helper.rb
@@ -43,7 +43,7 @@ module Helpers
 
     # Need to change from random_group_name to just "group_name"
     def random_group_name
-      return '58732ce0dddssssjkjjaasa72ddd331eadycb9b6663ddddddddsdddddff3xx24dd6666dee'
+      return '58732ce0dddssssjkjjaasa72ddd331eadycb9b6663ddddddddsdddddff3xx24dd6666deeeaaaaaaa'
       # hex = SecureRandom.hex(10)
       # puts("hex=#{hex}")
       # hex
