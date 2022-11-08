1ef6b0df3e30645e15e536afef606a972fa1a793
dmistry2 <deep.mistry@nasa.gov>
2022-11-08 10:29:10 -0500

    MMT-3082: Fixed adding_provider_permissions_from_group_spec.rb test

--- spec/features/manage_cmr/provider_identity_permissions/adding_provider_permissions_from_group_spec.rb
+++ spec/features/manage_cmr/provider_identity_permissions/adding_provider_permissions_from_group_spec.rb
@@ -1,18 +1,20 @@
-# EDL Failed Test
-describe 'Saving Provider Object Permissions from the group show page', reset_provider: true, skip:true do
+describe 'Saving Provider Object Permissions from the group show page', reset_provider: true, js:true do
+  before do
+    @token = 'jwt_access_token'
+    allow_any_instance_of(Cmr::UrsClient).to receive(:get_client_token).and_return('client_access_token')
+    allow_any_instance_of(ApplicationController).to receive(:token).and_return(@token)
+  end
   before :all do
-    VCR.use_cassette('edl', record: :new_episodes) do
-      @provider_group_name = 'Test_Group_for_Creating_Provider_Object_Permissions_from_group_page'
-      @provider_group_description = 'Group for creating provider object permissions'
+    VCR.use_cassette("edl/#{File.basename(__FILE__, '.rb')}_vcr", record: :none) do
+      @provider_group_name = 'Test_Group_for_Creating_Provider_Object_Permissions_from_group_page_testing_10'
+      @provider_group_description = 'Group for creating provider object permission'
       @provider_group = create_group(
         name: @provider_group_name,
         description: @provider_group_description,
         provider_id: 'MMT_2'
       )
 
-      #wait_for_cmr
-
-      @system_group_name = 'Test_System_Group_for_Provider_Permissions_from_group_page'
+      @system_group_name = 'Test_System_Group_for_Provider_Permissions_from_group_page_testing_10'
       @system_group_description = 'System Group for creating provider permissions'
       @system_group = create_group(
         name: @system_group_name,
@@ -21,7 +23,6 @@ describe 'Saving Provider Object Permissions from the group show page', reset_pr
         admin: true
       )
 
-      #wait_for_cmr
     end
   end
 
@@ -32,26 +33,30 @@ describe 'Saving Provider Object Permissions from the group show page', reset_pr
       'page_size' => 50,
       'permitted_group' => @system_group['group_id']
     }
-
-    permissions_response_items = cmr_client.get_permissions(permissions_options, 'access_token').body.fetch('items', [])
-
-    permissions_response_items.each { |perm_item| remove_group_permissions(perm_item['concept_id']) }
-
     # delete the system group
-    VCR.use_cassette('edl', record: :new_episodes) do
+    VCR.use_cassette("edl/#{File.basename(__FILE__, '.rb')}_vcr", record: :none) do
+      permissions_response_items = cmr_client.get_permissions(permissions_options, @token).body.fetch('items', [])
+
+      permissions_response_items.each { |perm_item| remove_group_permissions(perm_item['concept_id']) }
+
       delete_group(concept_id: @system_group['group_id'], admin: true)
     end
   end
 
   context 'when logging in as a provider admin' do
     before do
+      @token = 'jwt_access_token'
       login
+      allow_any_instance_of(ApplicationController).to receive(:token).and_return(@token)
     end
 
     context 'when visiting the provider group page' do
       before do
-        VCR.use_cassette('edl', record: :new_episodes) do
+        VCR.use_cassette("edl/#{File.basename(__FILE__, '.rb')}_vcr", record: :none) do
+          # mocking user_has_permission_to as true
+          allow_any_instance_of(ApplicationController).to receive(:user_has_permission_to).and_return(true)
           visit group_path(@provider_group['group_id'])
+
         end
       end
 
@@ -69,7 +74,9 @@ describe 'Saving Provider Object Permissions from the group show page', reset_pr
 
       context 'when clicking on the provider object permissions link' do
         before do
-          click_on 'Provider Object Permissions for MMT_2'
+          VCR.use_cassette("edl/#{File.basename(__FILE__, '.rb')}_vcr", record: :none) do
+            click_on 'Provider Object Permissions for MMT_2'
+          end
         end
 
         it 'displays the Provider Object Permissions page' do
@@ -81,7 +88,9 @@ describe 'Saving Provider Object Permissions from the group show page', reset_pr
 
         context 'when clicking cancel' do
           before do
-            click_on 'Cancel'
+            VCR.use_cassette("edl/#{File.basename(__FILE__, '.rb')}_vcr", record: :none) do
+              click_on 'Cancel'
+            end
           end
 
           it 'returns to the group page' do
@@ -101,16 +110,17 @@ describe 'Saving Provider Object Permissions from the group show page', reset_pr
             check('provider_permissions_INGEST_MANAGEMENT_ACL_', option: 'update')
             check('provider_permissions_OPTION_DEFINITION_', option: 'create')
             check('provider_permissions_PROVIDER_POLICIES_', option: 'delete')
-
-            within '.provider-permissions-form' do
-              click_on 'Submit'
+            VCR.use_cassette("edl/#{File.basename(__FILE__, '.rb')}_vcr", record: :none) do
+              within '.provider-permissions-form' do
+                click_on 'Submit'
+              end
             end
 
             wait_for_cmr
           end
 
           it 'displays a success message and no error message on the group page' do
-            expect(page).to have_content('Option Definition, Provider Policies, Group, Ingest Management Acl permissions were saved')
+            expect(page).to have_content('Group, Ingest Management Acl, Option Definition, Provider Policies permissions were saved')
             expect(page).to have_no_content('permissions were unable to be saved')
 
             expect(page).to have_content(@provider_group_name)
@@ -128,12 +138,15 @@ describe 'Saving Provider Object Permissions from the group show page', reset_pr
 
   context 'when logging in as a system admin' do
     before do
+      @token = 'jwt_access_token'
       login_admin
+      allow_any_instance_of(ApplicationController).to receive(:token).and_return(@token)
     end
 
     context 'when visiting the provider group page' do
       before do
-        VCR.use_cassette('edl', record: :new_episodes) do
+        VCR.use_cassette("edl/#{File.basename(__FILE__, '.rb')}_vcr", record: :none) do
+          allow_any_instance_of(ApplicationController).to receive(:user_has_permission_to).and_return(true)
           visit group_path(@provider_group['group_id'])
         end
       end
@@ -158,7 +171,8 @@ describe 'Saving Provider Object Permissions from the group show page', reset_pr
 
     context 'when visiting the system group page' do
       before do
-        VCR.use_cassette('edl', record: :new_episodes) do
+        VCR.use_cassette("edl/#{File.basename(__FILE__, '.rb')}_vcr", record: :none) do
+          allow_any_instance_of(ApplicationController).to receive(:user_has_system_permission_to).and_return(true)
           visit group_path(@system_group['group_id'])
         end
       end
@@ -178,7 +192,9 @@ describe 'Saving Provider Object Permissions from the group show page', reset_pr
 
       context 'when clicking on the provider object permissions link' do
         before do
-          click_on 'Provider Object Permissions for MMT_2'
+          VCR.use_cassette("edl/#{File.basename(__FILE__, '.rb')}_vcr", record: :none) do
+            click_on 'Provider Object Permissions for MMT_2'
+          end
         end
 
         it 'displays the Provider Object Permissions page' do
@@ -190,7 +206,9 @@ describe 'Saving Provider Object Permissions from the group show page', reset_pr
 
         context 'when clicking cancel' do
           before do
-            click_on 'Cancel'
+            VCR.use_cassette("edl/#{File.basename(__FILE__, '.rb')}_vcr", record: :none) do
+              click_on 'Cancel'
+            end
           end
 
           it 'returns to the group page' do
@@ -211,16 +229,17 @@ describe 'Saving Provider Object Permissions from the group show page', reset_pr
             check('provider_permissions_INGEST_MANAGEMENT_ACL_', option: 'read')
             check('provider_permissions_PROVIDER_INFORMATION_', option: 'update')
             check('provider_permissions_EXTENDED_SERVICE_', option: 'delete')
+            VCR.use_cassette("edl/#{File.basename(__FILE__, '.rb')}_vcr", record: :none) do
 
-            within '.provider-permissions-form' do
-              click_on 'Submit'
+              within '.provider-permissions-form' do
+                click_on 'Submit'
+              end
             end
-
             wait_for_cmr
           end
 
           it 'displays a success message and no error message on the system group page' do
-            expect(page).to have_content('Extended Service, Provider Calendar Event, Provider Information, Ingest Management Acl permissions were saved')
+            expect(page).to have_content('Extended Service, Ingest Management Acl, Provider Calendar Event, Provider Information permissions were saved.')
             expect(page).to have_no_content('permissions were unable to be saved')
 
             expect(page).to have_content(@system_group_name)
