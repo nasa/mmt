e0917dffac8487dfb16258f7421ae6e9b6e63ec2
dmistry2 <deep.mistry@nasa.gov>
2023-01-25 15:54:14 -0500

    MMT-2995: Added migrations for EULA Migrations

--- app/models/collection_draft.rb
+++ app/models/collection_draft.rb
@@ -493,9 +493,9 @@ class CollectionDraft < Draft
 
   def set_metadata_specification
     metadata_specification = {
-      'URL' => 'https://cdn.earthdata.nasa.gov/umm/collection/v1.17.0',
+      'URL' => 'https://cdn.earthdata.nasa.gov/umm/collection/v1.17.1',
       'Name' => 'UMM-C',
-      'Version' => '1.17.0'
+      'Version' => '1.17.1'
     }
     unless self.draft['MetadataSpecification'] == metadata_specification
       self.draft['MetadataSpecification'] = metadata_specification
