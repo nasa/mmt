4785cc1693c28a5981d0afe2cc4dc191e5240df8
tranhohoanvu <hoan.v.tranho@nasa.gov>
2023-01-26 14:44:16 -0500

    MMT-3108: Initial check in

--- app/controllers/manage_cmr/order_policies_controller.rb
+++ app/controllers/manage_cmr/order_policies_controller.rb
@@ -13,7 +13,6 @@ class OrderPoliciesController < ManageCmrController
     set_policy
 
     add_breadcrumb 'New', new_order_policies_path
-
     redirect_to edit_order_policies_path, flash: { notice: "Order Policies already exist for #{current_user.provider_id}." } unless @policy.empty?
   end
 
@@ -25,8 +24,8 @@ class OrderPoliciesController < ManageCmrController
 
   def create
     # Attempt to upsert the policies
-    upsert_response = upsert_policy
-
+    upsert_response = create_provider_policy
+    puts("TTTTTTTEEEEEEESSSSSSTTTT upsert_response=#{upsert_response.body}")
     if upsert_response.error?
       @policy.deep_stringify_keys!
       Rails.logger.error("Create Order Policies Error: #{upsert_response.clean_inspect}")
@@ -39,7 +38,7 @@ class OrderPoliciesController < ManageCmrController
 
   def update
     # Attempt to upsert the policies
-    upsert_response = upsert_policy
+    upsert_response = update_provider_policy
 
     if upsert_response.error?
       @policy.deep_stringify_keys!
@@ -52,8 +51,7 @@ class OrderPoliciesController < ManageCmrController
   end
 
   def destroy
-    response = echo_client.remove_provider_policies(token, current_provider_guid)
-
+    response = cmr_client.remove_provider_policy(token, current_user.provider_id)
     if response.error?
       Rails.logger.error("Delete Order Policies Error: #{response.clean_inspect}")
       flash[:error] = response.error_message
@@ -65,10 +63,9 @@ class OrderPoliciesController < ManageCmrController
   end
 
   def test_endpoint_connection
-    response = echo_client.test_endpoint_connection(token, current_provider_guid)
-
+    response = cmr_client.test_endpoint_connection(token, current_user.provider_id)
     message = if response.error?
-                response.error_message
+                response.body.fetch('errors', []).first['message']
               else
                 'Test endpoint connection was successful.'
               end
@@ -79,74 +76,58 @@ class OrderPoliciesController < ManageCmrController
   private
 
   def set_policy
-    # Get the provider's policies (will only ever be one)
-    result = echo_client.get_providers_policies(token, current_provider_guid)
-
-    # Check for an error OR nil, rather than returning an empty list or error it returns nil
-    @policy = result.parsed_body.fetch('Item', {}) unless result.error? || result.parsed_body.fetch('Item', {}).fetch('xsi:nil', 'false') == 'true'
-
+    result = cmr_client.get_provider_policy(token, current_user.provider_id)
+    puts("############ result=#{result.inspect}")
+    puts("################# result.body=#{result.body.to_json}")
+    @policy = result.body.fetch('data', {}).fetch('providerPolicy', {}) unless result.error?
     if result.error?
       Rails.logger.error("#{request.uuid} - OrderPoliciesController#set_policy - Retrieve Providers Policies Error: #{result.clean_inspect}")
       flash[:error] = I18n.t("controllers.order_policies.set_policy.flash.timeout_error", request: request.uuid) if result.timeout_error?
     end
-
-    # Default value in case of error
-    @policy = {} if defined?(@policy).nil?
-
-    # This comes to us as a Hash containing 'Item' but is returned as a simple array. For the purposes
-    # of rendering this and DRYing up the code, we'll convert this here so its the same in all uses
-    if @policy.key?('SupportedTransactions')
-      @policy['SupportedTransactions'] = @policy.fetch('SupportedTransactions', {}).fetch('Item', [])
-    end
-
-    if @policy.key?('CollectionsSupportingDuplicateOrderItems')
-      @policy['CollectionsSupportingDuplicateOrderItems'] = @policy.fetch('CollectionsSupportingDuplicateOrderItems', {}).fetch('Item', [])
-    end
+    @policy = {} if @policy.nil?
   end
 
-  def generate_upsert_payload
-    # SUBMIT is given to all orders, no need to select it
-    provided_supported_transactions = %w(SUBMIT)
-
+  def generate_order_policy_payload
     @policy = {
-      RetryAttempts: params.fetch('retry_attempts'),
-      RetryWaitTime: params.fetch('retry_wait_time'),
-      EndPoint: params.fetch('end_point'),
-      Routing: 'ORDER_FULFILLMENT_V9', # Only one value exists for this key, so we're hardcoding it and hiding it from the form
-      overrideNotificationEnabled: params.fetch('override_notification_enabled', 'false'),
-      SslPolicy: {
-        SslEnabled: params.fetch('ssl_policy', {}).fetch('ssl_enabled', 'false'),
-        SslCertificate: params.fetch('ssl_policy', {}).fetch('ssl_certificate', '')
-      },
-      SupportedTransactions: params.fetch('supported_transactions', []) | provided_supported_transactions
+      retryAttempts: params.fetch('retry_attempts'),
+      retryWaitTime: params.fetch('retry_wait_time'),
+      endpoint: params.fetch('end_point'),
+      overrideNotifyEnabled: params.fetch('override_notification_enabled', '0'),
+      sslPolicy: {
+        sslEnabled: params.fetch('ssl_policy', {}).fetch('ssl_enabled', '0'),
+        sslCertificate: params.fetch('ssl_policy', {}).fetch('ssl_certificate', '')
+      }
     }
-
-    unless params.fetch('properties').empty?
-      @policy[:Properties] = params.fetch('properties')
-    end
-
-    order_items = params.fetch('collections_supporting_duplicate_order_items_toList', [])
-    unless order_items.empty?
-      @policy[:OrderSupportsDuplicateCatalogItems] = 'true'
-      @policy[:CollectionsSupportingDuplicateOrderItems] = order_items
-    end
-
     unless params.fetch('max_items_per_order').empty?
-      @policy[:MaxItemsPerOrder] = params.fetch('max_items_per_order')
+      @policy[:maxItemsPerOrder] = params.fetch('max_items_per_order')
     end
 
     unless params.fetch('ordering_suspended_until_date').empty?
-      @policy[:OrderingSuspendedUntilDate] = params.fetch('ordering_suspended_until_date')
+      @policy[:orderingSuspendedUntilDate] = params.fetch('ordering_suspended_until_date')
     end
 
+    unless params.fetch('properties').empty?
+      @policy[:referenceProps] = params.fetch('properties')
+    end
     @policy
   end
 
-  def upsert_policy
-    response = echo_client.set_provider_policies(token, current_provider_guid, generate_upsert_payload)
-
+  def update_provider_policy
+    response = cmr_client.update_provider_policy(token, current_user.provider_id, generate_order_policy_payload)
+    puts("@@@@@@@ update response=#{response.inspect}")
     if response.error?
-      Rails.logger.error("#{request.uuid} - OrderPoliciesController#upsert_policy - Set Providers Policies Error: #{response.clean_inspect}")
+      Rails.logger.error("#{request.uuid} - OrderPoliciesController#upsert_policy - Update Providers Policies Error: #{response.clean_inspect}")
+      flash[:error] = I18n.t("controllers.order_policies.upsert_policy.flash.timeout_error", request: request.uuid) if response.timeout_error?
+    end
+
+    response
+  end
+
+  def create_provider_policy
+    response = cmr_client.create_provider_policy(token, current_user.provider_id, generate_order_policy_payload)
+    puts("@@@@@@@ create response=#{response.inspect}")
+    if response.error?
+      Rails.logger.error("#{request.uuid} - OrderPoliciesController#upsert_policy - Create Providers Policies Error: #{response.clean_inspect}")
       flash[:error] = I18n.t("controllers.order_policies.upsert_policy.flash.timeout_error", request: request.uuid) if response.timeout_error?
     end
 
