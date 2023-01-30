d901e33eff6cd3976ba3801ff74aed5edc2cd47d
tranhohoanvu <hoan.v.tranho@nasa.gov>
2023-01-30 11:51:17 -0500

    MMT-3108: Re-add csplog

--- config/environments/sit.rb
+++ config/environments/sit.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
   config.subscriptions_enabled = true
 
   # Feature toggle for Content Security Policy (CSP) logging.
-  config.csplog_enabled = false
+  config.csplog_enabled = true
 
   # Feature toggle for UMM-T
   config.umm_t_enabled = true
