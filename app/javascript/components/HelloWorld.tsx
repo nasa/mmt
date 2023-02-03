9ecde08389f1c4bed7fc51936191df2d915db9f7
Christopher Gokey <christopher.d.gokey@nasa.gov>
2023-02-03 08:51:31 -0500

    MMT-2980: small update to HelloWorld.tsx

--- app/javascript/components/HelloWorld.tsx
+++ app/javascript/components/HelloWorld.tsx
@@ -5,19 +5,15 @@ interface HelloWorldProps {
   greeting: string
 }
 class HelloWorld extends React.Component<HelloWorldProps, never> {
-  // static propTypes: { greeting: PropTypes.Requireable<string>; };
   render () {
     return (
       <React.Fragment>
         <div>
-          Greetings from !@!@!@!@!: {this.props.greeting}
+          Greetings from: {this.props.greeting}
         </div>
       </React.Fragment>
     );
   }
 }
 
-// HelloWorld.propTypes = {
-//   greeting: PropTypes.string
-// };
 export default HelloWorld
