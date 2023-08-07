82a1d113b3f2634e4bb63b2b594a4f2cda377f71
htranho <34069728+htranho@users.noreply.github.com>
2023-08-07 14:12:53 -0400

    MMT-3175: Remove legacy code in Data Quality Summaries

--- app/controllers/manage_cmr/data_quality_summaries_controller.rb
+++ app/controllers/manage_cmr/data_quality_summaries_controller.rb
@@ -6,21 +6,12 @@ class DataQualitySummariesController < ManageCmrController
   RESULTS_PER_PAGE = 25
 
   def index
-    if use_legacy_order_service?
-      legacy_load_data_quality_summaries
-    else
-      load_data_quality_summaries
-    end
+    load_data_quality_summaries
   end
 
   def show
-    if use_legacy_order_service?
-      legacy_set_summary
-      add_breadcrumb @summary.fetch('Name'), data_quality_summary_path(@summary.fetch('Guid', nil))
-    else
-      set_data_quality_summary
-      add_breadcrumb @summary.fetch('umm', {}).fetch('Name', ''), data_quality_summary_path(@summary.fetch('umm', {}).fetch('Id', ''))
-    end
+    set_data_quality_summary
+    add_breadcrumb @summary.fetch('umm', {}).fetch('Name', ''), data_quality_summary_path(@summary.fetch('umm', {}).fetch('Id', ''))
   end
 
   def new
@@ -30,57 +21,24 @@ class DataQualitySummariesController < ManageCmrController
   end
 
   def edit
-    if use_legacy_order_service?
-      legacy_set_summary
-      add_breadcrumb @summary.fetch('Name'), data_quality_summary_path(@summary.fetch('Guid', nil))
-      add_breadcrumb 'Edit', edit_data_quality_summary_path(@summary.fetch('Guid', nil))
-    else
-      set_data_quality_summary
-      add_breadcrumb @summary.fetch('umm', {}).fetch('Name', ''), data_quality_summary_path(@summary.fetch('umm', {}).fetch('Id', ''))
-      add_breadcrumb 'Edit', edit_data_quality_summary_path(@summary.fetch('umm', {}).fetch('Id', ''))
-    end
+    set_data_quality_summary
+    add_breadcrumb @summary.fetch('umm', {}).fetch('Name', ''), data_quality_summary_path(@summary.fetch('umm', {}).fetch('Id', ''))
+    add_breadcrumb 'Edit', edit_data_quality_summary_path(@summary.fetch('umm', {}).fetch('Id', ''))
   end
 
   def create
-    if use_legacy_order_service?
-      legacy_create
-    else
-      create_data_quality_summary
-    end
+    create_data_quality_summary
   end
 
   def update
-    if use_legacy_order_service?
-      legacy_update_data_quality_summary
-    else
-      update_data_quality_summary
-    end
+    update_data_quality_summary
   end
 
   def destroy
-    if use_legacy_order_service?
-      legacy_destroy
-    else
-      destroy_data_quality_summary
-    end
+    destroy_data_quality_summary
   end
 
   private
-
-  def legacy_create
-    @summary = legacy_generate_payload
-
-    response = echo_client.create_data_quality_summary_definition(token, @summary)
-
-    if response.error?
-      Rails.logger.error("Create Data Quality Summary Error: #{response.clean_inspect}")
-      flash[:error] = response.error_message
-
-      render :new
-    else
-      redirect_to data_quality_summary_path(response.parsed_body), flash: { success: 'Data Quality Summary successfully created' }
-    end
-  end
 
   def create_data_quality_summary
     @summary = generate_payload
@@ -115,17 +73,6 @@ class DataQualitySummariesController < ManageCmrController
     summary
   end
 
-  def legacy_destroy
-    response = echo_client.remove_data_quality_summary_definition(token, params[:id])
-    if response.error?
-      Rails.logger.error("Delete Data Quality Summary Error: #{response.clean_inspect}")
-      flash[:error] = response.error_message
-    else
-      flash[:success] = 'Data Quality Summary successfully deleted'
-    end
-    redirect_to data_quality_summaries_path
-  end
-
   def destroy_data_quality_summary
     native_id = params[:id]
     response = cmr_client.remove_data_quality_summary(provider_id: current_user.provider_id, native_id: native_id, token: token)
@@ -136,21 +83,6 @@ class DataQualitySummariesController < ManageCmrController
       flash[:success] = 'Data Quality Summary successfully deleted'
     end
     redirect_to data_quality_summaries_path
-  end
-
-  def legacy_update_data_quality_summary
-    @summary = legacy_generate_payload
-
-    response = echo_client.update_data_quality_summary_definition(token, @summary)
-
-    if response.error?
-      Rails.logger.error("Update Data Quality Summary Error: #{response.clean_inspect}")
-      flash[:error] = response.error_message
-
-      render :edit
-    else
-      redirect_to data_quality_summary_path(response.parsed_body), flash: { success: 'Data Quality Summary successfully updated' }
-    end
   end
 
   def update_data_quality_summary
@@ -189,70 +121,6 @@ class DataQualitySummariesController < ManageCmrController
     summary_list = response.body.fetch('items', []) unless response.error?
     summary_list = summary_list.sort_by{ |summary| summary.fetch('umm', {}).fetch('Name', '').downcase }
     @summaries = Kaminari.paginate_array(summary_list, total_count: summary_list.count).page(page).per(RESULTS_PER_PAGE)
-  end
-
-  def legacy_load_data_quality_summaries
-    permitted = params.to_unsafe_h unless params.nil?# need to understand what this is doing more, think related to nested parameters not permitted.
-
-    response = echo_client.get_data_quality_summary_definition_name_guids(token, current_provider_guid)
-
-    summary_guids = []
-    # No ruby idioms exist that will allow us to ensure this is a list, because it
-    # is a list of dictionaries, not a list of strings
-    unless response.error? || response.parsed_body.nil?
-      parsed_response = response.parsed_body.fetch('Item', [])
-      if parsed_response.is_a?(Hash)
-        summary_guids << parsed_response.fetch('Guid', nil)
-      else
-        parsed_response.each do |item|
-          summary_guids << item.fetch('Guid', nil)
-        end
-      end
-      summary_guids = summary_guids.reject(&:blank?)
-    end
-
-    if response.error?
-      Rails.logger.error("#{request.uuid} - DataQualitySummariesController#legacy_load_data_quality_summaries - Retrieve Data Quality Summary Definition Name GUIDs Error: #{response.clean_inspect}")
-      flash[:error] = I18n.t("controllers.data_quality_summaries.index.flash.timeout_error", data: "data quality summary definition name guids", request: request.uuid) if response.timeout_error?
-    end
-
-    summary_list = []
-    summary_guids.each do |guid|
-      summary_response = echo_client.get_data_quality_summary_definition(token, guid)
-
-      if summary_response.success?
-        summary_list << summary_response
-      else
-        Rails.logger.error("#{request.uuid} - DataQualitySummariesController#legacy_load_data_quality_summaries - Retrieve Data Quality Summary Definition Error: #{summary_response.clean_inspect}")
-        flash[:error] = I18n.t("controllers.data_quality_summaries.index.flash.timeout_error", data: "data quality summary definitions", request: request.uuid) if summary_response.timeout_error?
-      end
-    end
-
-    summary_list.sort_by! { |summary| summary.parsed_body.fetch('Name', '').downcase }
-
-    # Default the page to 1
-    page = permitted.fetch('page', 1)
-
-    @summaries = Kaminari.paginate_array(summary_list, total_count: summary_list.count).page(page).per(RESULTS_PER_PAGE)
-  end
-
-  def legacy_generate_payload
-    {
-      'Guid'              => params.fetch('id'),
-      'Name'              => params.fetch('name'),
-      'Summary'           => sanitize_for_echo(params.fetch('summary')),
-      'OwnerProviderGuid' => current_provider_guid
-    }
-  end
-
-  def legacy_set_summary
-    result = echo_client.get_data_quality_summary_definition(token, params[:id])
-    if result.success?
-      @summary = result.parsed_body
-    else
-      Rails.logger.error("#{request.uuid} - DataQualitySummariesController#legacy_set_summary - Retrieve Data Quality Summary Definition Error: #{result.clean_inspect}")
-      flash[:error] = I18n.t("controllers.data_quality_summaries.set_summary.flash.timeout_error", request: request.uuid) if result.timeout_error?
-    end
   end
 
   def set_data_quality_summary
