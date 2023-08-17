c835815a7b6264471f07866786b44dcf56959e87
Deep Mistry <deep.mistry@nasa.gov>
2023-08-17 10:28:48 -0400

    MMT-3199: Added an accordion for jsonView

--- app/javascript/components/editor/components/JSONView.tsx
+++ app/javascript/components/editor/components/JSONView.tsx
@@ -1,8 +1,10 @@
-// import { cloneDeep } from 'lodash'
 import { cloneDeep } from 'lodash'
 import { observer } from 'mobx-react'
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import {
+  Accordion, Card
+} from 'react-bootstrap'
+import './JSONView.css'
 import JSONPretty from 'react-json-pretty'
 import MetadataEditor from '../MetadataEditor'
 import { removeEmpty } from '../utils/json_utils'
@@ -11,21 +13,45 @@ type JSONViewProps = {
   editor: MetadataEditor;
 }
 type JSONViewState = {
-  status: string
+  icon: string
 }
 
 class JSONView extends React.Component<JSONViewProps, JSONViewState> {
+  constructor(props: JSONViewProps) {
+    super(props)
+    this.state = {
+      icon: 'fa fa-chevron-down'
+    }
+  }
+  iconType() {
+    const { icon } = this.state
+    if (icon === 'fa fa-chevron-down') {
+      this.setState({ icon: 'fa fa-chevron-up' })
+    } else {
+      this.setState({ icon: 'fa fa-chevron-down' })
+    }
+  }
+
   render() {
     const { editor } = this.props
+    const { icon } = this.state
+
     const data = removeEmpty(cloneDeep(editor.fullData))
-    // const data = editor.fullData
+
     return (
-      <Card>
-        <Card.Header>JSON</Card.Header>
-        <Card.Body id="json-text-field">
-          <JSONPretty data={data} />
-        </Card.Body>
-      </Card>
+      <Accordion defaultActiveKey="0" className="json-accordion">
+        <Card>
+          <Accordion.Toggle className="json-accordion-header" data-testid="json-view__accordion-header" as={Card.Header} eventKey="0" onClick={() => { this.iconType() }}>
+            JSON
+            <i className={icon} style={{ marginLeft: '620px' }} />
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body id="json-text-field" data-testid="json-view__full-data">
+              <JSONPretty data={data} />
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
     )
   }
 }
