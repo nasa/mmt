3dc997a460e78523e997ab4cd10b61f9b02d8262
Deep Mistry <deep.mistry@nasa.gov>
2023-11-17 16:42:51 -0500

    MMT-3410: Fixed styles

--- static/src/js/components/BoundingRectangleField/BoundingRectangleField.jsx
+++ static/src/js/components/BoundingRectangleField/BoundingRectangleField.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import Button from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import './BoundingRectangleField.scss'
 
 const BoundingRectangleField = ({
