fc548430fa7d24dfa24dc956645fcea8b5335c1d
Matthew Crouch <matthew@element84.com>
2023-11-22 16:58:11 -0500

    MMT-3410: Added test

--- static/src/js/components/NavigationItemError/NavigationItemError.jsx
+++ static/src/js/components/NavigationItemError/NavigationItemError.jsx
@@ -13,10 +13,6 @@ const NavigationItemError = ({
   error,
   setFocusField
 }) => {
-  console.log('')
-  console.log('')
-  console.log('')
-  console.log('🚀 ~ file: NavigationItemError.jsx:13 ~ error:', error)
   const [hasFocus, setHasFocus] = useState(false)
 
   const {
@@ -32,152 +28,8 @@ const NavigationItemError = ({
     focusId = property.substring(1).replace(/\./g, '_')
   }
 
-  // Nested in Array
-  //   {
-  //     "name": "required",
-  //     "property": ".Organizations.1.ShortName",
-  //     "message": "must have required property 'ShortName'",
-  //     "params": {
-  //         "missingProperty": "ShortName"
-  //     },
-  //     "stack": "must have required property 'ShortName'",
-  //     "schemaPath": "#/properties/Organizations/items/required"
-  // }
-  // Related Url (1 of 1) // need index from property and total length from metadata
-  // Must have required property 'URL'
-  // Must have required property 'URLContentType'
-  // Must have required property 'Type'
-
-  // Top level
-  // {
-  //   "name": "required",
-  //   "property": "Name",
-  //   "message": "must have required property 'Name'",
-  //   "params": {
-  //       "missingProperty": "Name"
-  //   },
-  //   "stack": "must have required property 'Name'",
-  //   "schemaPath": "#/required"
-  // }
-
-  // Nested in an object
-  // {
-  //   "name": "required",
-  //   "property": ".URL.URLValue",
-  //   "message": "must have required property 'URL Value'",
-  //   "params": {
-  //       "missingProperty": "URLValue"
-  //   },
-  //   "stack": "must have required property 'URL Value'",
-  //   "schemaPath": "#/properties/URL/required"
-  // }
-
-  // Nested in an object
-  // {
-  //   message: "must have required property 'MissingReason'",
-  //   path: '.DOI.MissingReason',
-  //   errorProperty: '.DOI.MissingReason'
-  // }
-  // DOI
-  // Must have required property 'DOI'
-  // Must have required property 'Missing Reason'
-  // const isNestedItem = property.startsWith('.')
-  // const path = createPath(property)
-  // console.log('🚀 ~ file: NavigationItemError.jsx:92 ~ path:', path)
-  // const regexp = /^(.*[^\\[]+)\[(\d+)\]/
-  // const match = path.match(regexp)
-  // console.log('🚀 ~ file: ErrorList.tsx:206 ~ ErrorList ~ walkErrorMap ~ match:', match)
-
-  // let length
-  // if (match && match[1]) {
-  //   const matchedPath = match[1].substring(1)
-  //   console.log('🚀 ~ file: NavigationItemError.jsx:139 ~ ummMetadata:', ummMetadata)
-  //   console.log('🚀 ~ file: NavigationItemError.jsx:137 ~ matchedPath:', matchedPath)
-  //   const results = get(ummMetadata, matchedPath)
-  //   console.log('🚀 ~ file: NavigationItemError.jsx:139 ~ results:', results)
-  //   length = results?.length
-  // }
-
-  // console.log('🚀 ~ file: NavigationItemError.jsx:134 ~ length:', length)
-
-  // each error passed in is a single error, but if multiple errors are nested under a field they need to be grouped together
-
   let messageToDisplay = upperFirst(message)
   if (fieldName) messageToDisplay = fieldName
-
-  const nestedErrors = () => {
-    if (!errors) return null
-
-    return (
-      <For each={errors}>
-        {
-          (nestedError) => {
-            console.log('🚀 ~ file: NavigationItemError.jsx:125 ~ nestedErrors ~ nestedError:', nestedError)
-
-            return (
-              <NavigationItemError
-                className="ps-4"
-                error={nestedError}
-                setFocusField={setFocusField}
-              />
-            )
-            // const {
-            //   fieldName,
-            //   message,
-            //   visited
-            // } = nestedError
-
-            // let messageToDisplay = upperFirst(message)
-            // if (fieldName) messageToDisplay = fieldName
-
-            // return (
-            //   <ListGroup.Item
-            //     className={
-            //       classNames([
-            //         'navigation-item-error__item d-flex align-items-baseline border-0 ps-4 px-1 py-1',
-            //         {
-            //           'navigation-item-error__item--isFocused': hasFocus
-            //         }
-            //       ])
-            //     }
-            //     action
-            //     onMouseOver={
-            //       () => {
-            //         setHasFocus(true)
-            //       }
-            //     }
-            //     onMouseOut={
-            //       () => {
-            //         setHasFocus(false)
-            //       }
-            //     }
-            //     onClick={
-            //       () => {
-            //         setFocusField(createId())
-            //       }
-            //     }
-            //   >
-            //     <i className={
-            //       classNames([
-            //         'eui-icon eui-icon--sm navigation-item__icon',
-            //         {
-            //           'eui-fa-circle-o navigation-item__icon--not-started': !visited
-            //         },
-            //         {
-            //           'eui-fa-times-circle navigation-item__icon--error': visited
-            //         }
-            //       ])
-            //     }
-            //     />
-            //     {/* // TODO format message, handle array fields, etc from ErrorList */}
-            //     <span className="small">{messageToDisplay}</span>
-            //   </ListGroup.Item>
-            // )
-          }
-        }
-      </For>
-    )
-  }
 
   return (
     <div className={className}>
@@ -219,7 +71,6 @@ const NavigationItemError = ({
           ])
         }
         />
-        {/* // TODO format message, handle array fields, etc from ErrorList */}
         <span
           className={
             classNames([
@@ -233,12 +84,31 @@ const NavigationItemError = ({
           {messageToDisplay}
         </span>
       </ListGroup.Item>
-      {nestedErrors()}
+      {
+        errors && (
+          <For each={errors}>
+            {
+              (nestedError) => (
+                <NavigationItemError
+                  className="ps-4"
+                  error={nestedError}
+                  setFocusField={setFocusField}
+                />
+              )
+            }
+          </For>
+        )
+      }
     </div>
   )
 }
 
+NavigationItemError.defaultProps = {
+  className: null
+}
+
 NavigationItemError.propTypes = {
+  className: PropTypes.string,
   error: PropTypes.shape({
     fieldName: PropTypes.string,
     errors: PropTypes.arrayOf(
---
+++ static/src/js/components/NavigationItemError/__tests__/NavigationItemError.test.js
@@ -0,0 +1,116 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import userEvent from '@testing-library/user-event'
+
+import NavigationItemError from '../NavigationItemError'
+
+const setup = (overrideProps = {}) => {
+  const props = {
+    className: null,
+    error: {},
+    setFocusField: jest.fn(),
+    ...overrideProps
+  }
+
+  render(
+    <NavigationItemError {...props} />
+  )
+
+  return {
+    props,
+    user: userEvent.setup()
+  }
+}
+
+describe('NavigationItemError', () => {
+  describe('when the error does not have sub-errors', () => {
+    test('displays the error message', () => {
+      setup({
+        error: {
+          message: 'must have required property \'Field Name\'',
+          property: '.FieldName',
+          visited: false
+        }
+      })
+
+      expect(screen.getByText('Must have required property \'Field Name\'')).toBeInTheDocument()
+    })
+
+    describe('when hovering on the error', () => {
+      test('calls setFocusField', async () => {
+        const { user } = setup({
+          error: {
+            message: 'must have required property \'Field Name\'',
+            property: '.FieldName',
+            visited: false
+          }
+        })
+
+        const error = screen.getByRole('button')
+
+        await user.hover(error)
+
+        expect(error).toHaveClass('navigation-item-error__item--isFocused')
+      })
+    })
+
+    describe('when hovering off the error', () => {
+      test('calls setFocusField', async () => {
+        const { user } = setup({
+          error: {
+            message: 'must have required property \'Field Name\'',
+            property: '.FieldName',
+            visited: false
+          }
+        })
+
+        const error = screen.getByRole('button')
+
+        await user.hover(error)
+
+        expect(error).toHaveClass('navigation-item-error__item--isFocused')
+
+        await user.unhover(error)
+
+        expect(error).not.toHaveClass('navigation-item-error__item--isFocused')
+      })
+    })
+
+    describe('when clicking on the error', () => {
+      test('calls setFocusField', async () => {
+        const { props, user } = setup({
+          error: {
+            message: 'must have required property \'Field Name\'',
+            property: '.FieldName',
+            visited: false
+          }
+        })
+
+        const error = screen.getByRole('button')
+
+        await user.click(error)
+
+        expect(props.setFocusField).toHaveBeenCalledTimes(1)
+        expect(props.setFocusField).toHaveBeenCalledWith('FieldName')
+      })
+    })
+  })
+
+  describe('when the error has sub-errors', () => {
+    test('displays the sub error messages', () => {
+      setup({
+        error: {
+          errors: [{
+            message: 'must have required property \'Field Name\'',
+            property: '.FieldName',
+            visited: false
+          }],
+          fieldName: 'Mock Field'
+        }
+      })
+
+      expect(screen.getByRole('button', { name: 'Mock Field' })).toBeInTheDocument()
+      expect(screen.getByRole('button', { name: 'Must have required property \'Field Name\'' })).toBeInTheDocument()
+    })
+  })
+})
