06b85e8d1a5007125730ad84434430b573c7587f
htranho <34069728+htranho@users.noreply.github.com>
2023-11-29 14:28:53 -0500

    MMT-3410: Add test for getConceptTypeByDraftConceptId.js

---
+++ static/src/js/constants/draftConceptIdTypes.js
@@ -0,0 +1,11 @@
+/**
+ * Mapping of draft concept id prefixes to concept types
+ */
+const draftConceptIdTypes = {
+  CD: 'Collection',
+  SD: 'Service',
+  TD: 'Tool',
+  VD: 'Variable'
+}
+
+export default draftConceptIdTypes
--- static/src/js/utils/getConceptTypeByDraftConceptId.js
+++ static/src/js/utils/getConceptTypeByDraftConceptId.js
@@ -1,16 +1,12 @@
-// Mapping of draft concept id prefixes to concept types
-const draftConceptIdTypes = {
-  CD: 'Collection',
-  SD: 'Service',
-  TD: 'Tool',
-  VD: 'Variable'
-}
+import draftConceptIdTypes from '../constants/draftConceptIdTypes'
 
 /**
  * Find the concept type based on the provided draftConceptId
  * @param {String} draftConceptId Draft concept ID to determine the concept type
  */
 const getConceptTypeByDraftConceptId = (draftConceptId) => {
+  if (!draftConceptId) return undefined
+
   const prefix = draftConceptId.substring(0, 2)
 
   return draftConceptIdTypes[prefix]
