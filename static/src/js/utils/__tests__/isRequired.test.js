bbe26f70f3bbe960c38c7117c19991364bfb3aac
tle1989 <tle1989@yahoo.com>
2023-11-29 14:48:17 -0500

    MMT-3410: Replace it() to test()

--- static/src/js/utils/__tests__/isRequired.test.js
+++ static/src/js/utils/__tests__/isRequired.test.js
@@ -2,7 +2,7 @@ import isRequired from '../isRequired'
 
 describe('isRequired', () => {
   // Test case for when the field is required
-  it('returns true when the field is required', () => {
+  test('returns true when the field is required', () => {
     const schema = {
       required: ['fieldName']
     }
@@ -11,7 +11,7 @@ describe('isRequired', () => {
   })
 
   // Test case for when the field is not required
-  it('returns false when the field is not required', () => {
+  test('returns false when the field is not required', () => {
     const schema = {
       required: ['otherField']
     }
@@ -20,14 +20,14 @@ describe('isRequired', () => {
   })
 
   // Test case for when the schema does not have the 'required' property
-  it('returns false when the schema does not have the required property', () => {
+  test('returns false when the schema does not have the required property', () => {
     const schema = {}
     const result = isRequired('fieldName', schema)
     expect(result).toBe(false)
   })
 
   // Test case for when the 'required' property is not an array
-  it('returns false when the required property is not an array', () => {
+  test('returns false when the required property is not an array', () => {
     const schema = {
       required: 'fieldName'
     }
@@ -36,7 +36,7 @@ describe('isRequired', () => {
   })
 
   // Test case for when the field is in the required array, but not at the same index
-  it('returns true when the field is in the required array but not at the same index', () => {
+  test('returns true when the field is in the required array but not at the same index', () => {
     const schema = {
       required: ['otherField', 'fieldName']
     }
