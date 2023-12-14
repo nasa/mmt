251b1313d048baae2b9b2d2abcdfaa4222fec229
Deep Mistry <deep.mistry@nasa.gov>
2023-12-14 16:35:20 -0500

    MMT-3410: Added jsDoc for CustomFieldTemplate

--- static/src/js/components/CustomFieldTemplate/CustomFieldTemplate.jsx
+++ static/src/js/components/CustomFieldTemplate/CustomFieldTemplate.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+/**
+ * CustomFieldTemplate
+ * @typedef {Object} CustomFieldTemplate
+ * @property {String} className custom className for the div
+ * @property {Object} help An object that has the form information
+ * @property {Object} error An object that has the form errors
+ * @property {Object} children An object that has the form fields
+ */
+
+/**
+ * Renders Custom Field Template
+ * @param {CustomFieldTemplate} props
+ */
 const CustomFieldTemplate = ({
   classNames,
   help,
