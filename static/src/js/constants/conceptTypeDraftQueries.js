89f36b9c24c5972a33aff680ced4b9b1a7c7868f
Deep Mistry <deep.mistry@nasa.gov>
2024-01-23 09:27:36 -0500

    MMT-341: Fixed test coverage

--- static/src/js/constants/conceptTypeDraftQueries.js
+++ static/src/js/constants/conceptTypeDraftQueries.js
@@ -1,11 +1,12 @@
 import { SERVICE_DRAFT } from '../operations/queries/getServiceDraft'
 import { TOOL_DRAFT } from '../operations/queries/getToolDraft'
+import { VARIABLE_DRAFT } from '../operations/queries/getVariableDraft'
 
 const conceptTypeDraftQueries = {
   Collection: TOOL_DRAFT,
   Service: SERVICE_DRAFT,
   Tool: TOOL_DRAFT,
-  Variable: TOOL_DRAFT
+  Variable: VARIABLE_DRAFT
 }
 
 export default conceptTypeDraftQueries
