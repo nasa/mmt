18dff8687ea5c9ed1fc8ec3dadf1f063474f62f6
htranho <34069728+htranho@users.noreply.github.com>
2024-01-24 17:18:15 -0500

    MMT-3499: Add support for umm-c to metadata form

--- static/src/js/constants/conceptTypeDraftQueries.js
+++ static/src/js/constants/conceptTypeDraftQueries.js
@@ -1,8 +1,9 @@
 import { SERVICE_DRAFT } from '../operations/queries/getServiceDraft'
 import { TOOL_DRAFT } from '../operations/queries/getToolDraft'
+import { COLLECTION_DRAFT } from '../operations/queries/getCollectionDraft'
 
 const conceptTypeDraftQueries = {
-  Collection: TOOL_DRAFT,
+  Collection: COLLECTION_DRAFT,
   Service: SERVICE_DRAFT,
   Tool: TOOL_DRAFT,
   Variable: TOOL_DRAFT
---
+++ static/src/js/schemas/uiForms/collectionsConfiguration.js
@@ -0,0 +1,103 @@
+const collectionsConfiguration = [
+  {
+    displayName: 'Collection Information',
+    properties: [
+      'ShortName',
+      'Version',
+      'VersionDescription',
+      'EntryTitle',
+      'DOI',
+      'AssociatedDOIs',
+      'Abstract',
+      'Purpose',
+      'DataLanguage'
+    ]
+  },
+  {
+    displayName: 'Data Identification',
+    properties: [
+      'DataDates',
+      'CollectionDataType',
+      'StandardProduct',
+      'ProcessingLevel',
+      'CollectionProgress',
+      'Quality',
+      'UseConstraints',
+      'AccessConstraints',
+      'MetadataAssociations',
+      'PublicationReferences'
+    ]
+  },
+  {
+    displayName: 'Related URLs',
+    properties: [
+      'RelatedUrls'
+    ]
+  },
+  {
+    displayName: 'Descriptive Keywords',
+    properties: [
+      'ScienceKeywords',
+      'AncillaryKeywords',
+      'AdditionalAttributes'
+    ]
+  },
+  {
+    displayName: 'Acquisition Information',
+    properties: [
+      'Platforms',
+      'Projects'
+    ]
+  },
+  {
+    displayName: 'Temporal Information',
+    properties: [
+      'TemporalExtents',
+      'TemporalKeywords',
+      'PaleoTemporalCoverages'
+    ]
+  },
+  {
+    displayName: 'Spatial Information',
+    properties: [
+      'SpatialExtent',
+      'SpatialInformation',
+      'TilingIdentificationSystems',
+      'LocationKeywords'
+    ]
+  },
+  {
+    displayName: 'Data Centers',
+    properties: [
+      'DataCenters'
+    ]
+  },
+  {
+    displayName: 'Data Contacts',
+    properties: [
+      'DataContacts'
+    ]
+  },
+  {
+    displayName: 'Collection Citations',
+    properties: [
+      'CollectionCitations'
+    ]
+  },
+  {
+    displayName: 'Metadata Information',
+    properties: [
+      'MetadataLanguage',
+      'MetadataDates'
+    ]
+  },
+  {
+    displayName: 'Archive And Distribution Information',
+    properties: [
+      'ArchiveAndDistributionInformation',
+      'DirectDistributionInformation'
+    ]
+  }
+
+]
+export default collectionsConfiguration
