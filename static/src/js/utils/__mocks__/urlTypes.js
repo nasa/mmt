70618cedc655d52827c8cd7fc5e25d4d41ca53b0
tle1989 <tle1989@yahoo.com>
2024-02-29 17:04:59 -0500

    MMT-3498: add more UISchemas umm_c forms

---
+++ static/src/js/utils/__mocks__/urlTypes.js
@@ -0,0 +1,11 @@
+const urltypes = [
+  ['DistributionURL', 'DOWNLOAD SOFTWARE', 'MOBILE APP'],
+  ['DistributionURL', 'DOWNLOAD SOFTWARE', ''],
+  ['DistributionURL', 'GOTO WEB TOOL', 'LIVE ACCESS SERVER (LAS)'],
+  ['DistributionURL', 'GOTO WEB TOOL', 'MAP VIEWER'],
+  ['DistributionURL', 'GOTO WEB TOOL', 'SIMPLE SUBSET WIZARD (SSW)'],
+  ['DistributionURL', 'GOTO WEB TOOL', 'SUBSETTER'],
+  ['DistributionURL', 'GOTO WEB TOOL', '']
+]
+
+export default urltypes
