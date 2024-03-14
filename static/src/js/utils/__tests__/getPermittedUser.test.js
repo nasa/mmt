4becb3b4636369c57f99d455e0bf43cef40b0826
tle1989 <tle1989@yahoo.com>
2024-03-14 16:29:00 -0400

    MMT-3551: A fix based on PR's comments

--- static/src/js/utils/__tests__/getPermittedUser.test.js
+++ static/src/js/utils/__tests__/getPermittedUser.test.js
@@ -1,18 +1,33 @@
 import getPermittedUser from '../getPermittedUser'
+import { getApplicationConfig } from '../getConfig'
+
+jest.mock('../getConfig', () => ({
+  getApplicationConfig: jest.fn()
+}))
 
 describe('getPermittedUser function', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   it('should return "typical" for development environment', () => {
+    // Mock the application config to return 'development' version
+    getApplicationConfig.mockReturnValue({ version: 'development' })
+
     // Set environment to development
     process.env.NODE_ENV = 'development'
 
     expect(getPermittedUser()).toEqual('typical')
   })
 
-  it('should return SIT_PERMITTED_USER for SIT environment', () => {
-    // Set environment to SIT
-    process.env.NODE_ENV = 'test'
-    process.env.REACT_APP_SIT_PERMITTED_USER = 'some_sit_user'
+  it('should return the user\'s uid for non-development environment', () => {
+    // Mock the application config to return 'production' version
+    getApplicationConfig.mockReturnValue({ version: 'production' })
 
-    expect(getPermittedUser()).toEqual('some_sit_user')
+    // Set environment to production
+    process.env.NODE_ENV = 'production'
+
+    const user = { uid: 'some_user_uid' }
+    expect(getPermittedUser(user)).toEqual('some_user_uid')
   })
 })
--- static/src/js/utils/getPermittedUser.js
+++ static/src/js/utils/getPermittedUser.js
@@ -1,14 +1,18 @@
+import { getApplicationConfig } from './getConfig'
+
 /**
  *Logic to determine permittedUser based on environment
+ * @param {Object} user A user object
  */
-const getPermittedUser = () => {
+const getPermittedUser = (user) => {
+  const { version } = getApplicationConfig()
   // For development environment
-  if (process.env.NODE_ENV === 'development') {
+  if (version === 'development') {
     return 'typical'
   }
 
   // For SIT environment
-  return process.env.REACT_APP_SIT_PERMITTED_USER
+  return user?.uid
 }
 
 export default getPermittedUser
