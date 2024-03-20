48f6e8725f4c4622f779f59e15a28f573d45eca6
tle1989 <63017465+tle1989@users.noreply.github.com>
2024-03-20 11:35:44 -0400

    MMT-3498: Add Uischemas for umm-c (#1146)
    * MMT-3498: Update UiSchema for umm-c
    
    * MMT-3498: Add LanguageArray into collection_information.js
    
    * MMT-3498: Add LanguageArray
    
    * MMT-3498: add data_identification.js form
    
    * MMT-3498: add related_urls.js form
    
    * MMT-3498: add UISchemas umm_c forms
    
    * MMT-3498: add more UISchemas umm_c forms
    
    * MMT-3498: Add the rests of UISchemas umm_c forms
    
    * MMT-3498: work on descriptiveKeywords, and UI/UX
    
    * MMT-3498: Add headers for all the forms
    
    * MMT-3498: Remove 'ui:filter' from Location Keywords
    
    * MMT-3498: Clean up codes
    
    * MMT-3498: Add tests
    
    * MMT-3498: Remove snapshots and fix trailing comma
    
    * MMT-3498: Remove snapshots functionality from tests
    
    * MMT-3498: Address all Feedbacks from PR review
    
    * MMT-3498: fix languageArray import
    
    * MMT-3498: fix descriptiveKeywords
    
    * MMT-3498: Remove unused files
    
    * MMT-3498: add 'ui:header-classname': 'h3-title' back to variableInformation file
    
    * MMT-3498: clean codes
    
    * MMT-3498: Remove comments
    
    * MMT-3498: Fixed boundingRectangle is not rendering the custom boundingRectangle field
    
    * MMT-3498: Remove style in spatialInformation.jsx
    
    * MMT-3498: Fixed comments on Monday Mar 11, and Remove cmrKeywords.js
    
    * MMT-3498: Remove comment line
    
    * MMT-3498: A fix is based on PR feetbacks
    
    * MMT-3498: Fix lint complains on Github
    
    * MMT-3498: Fix based on PR comments
    
    * MMT-3498: Fix Orbital gap, disable the scroll wheel for numbers
    
    * MMT-3498: Fix stylelint(lts/hydrogen) complaint on Github
    
    * MMT-3498: Fix stylelint(lts/hydrogen) complaint on Github
    
    * MMT-3498: Add back comments
    
    * MMT-3498: Fix SpatialInformation form
    
    * MMT-3498: Fix gap between Quality and Use Constraints in Data Identification, orbital in Spatial Information
    
    * MMT-3498: A minor fix

---
+++ static/src/js/schemas/uiSchemas/collections/dataIdentification.js
@@ -0,0 +1,722 @@
+import CustomSelectWidget from '../../../components/CustomSelectWidget/CustomSelectWidget'
+import CustomTextareaWidget from '../../../components/CustomTextareaWidget/CustomTextareaWidget'
+import processingLevel from '../../../constants/processingLevel'
+
+const dataIdentificationUiSchema = {
+  'ui:submitButtonOptions': {
+    norender: true
+  },
+  'ui:heading-level': 'h3',
+  'ui:field': 'layout',
+  'ui:layout_grid': {
+    'ui:row': [
+      {
+        'ui:group': 'Data Identification',
+        'ui:col': {
+          md: 12,
+          children: [
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['DataDates']
+                  }
+                }
+              ]
+            },
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['CollectionDataType']
+                  }
+                }
+              ]
+            },
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['StandardProduct']
+                  }
+                }
+              ]
+            },
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['ProcessingLevel']
+                  }
+                }
+              ]
+            },
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['CollectionProgress']
+                  }
+                }
+              ]
+            },
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['Quality']
+                  }
+                }
+              ]
+            },
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['UseConstraints']
+                  }
+                }
+              ]
+            },
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['AccessConstraints']
+                  }
+                }
+              ]
+            },
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['MetadataAssociations']
+                  }
+                }
+              ]
+            },
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['PublicationReferences']
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      }
+    ]
+  },
+  DataDates: {
+    'ui:heading-level': 'h4',
+    items: {
+      'ui:field': 'layout',
+      'ui:layout_grid': {
+        'ui:row': [
+          {
+            'ui:col': {
+              md: 12,
+              children: [
+                {
+                  'ui:row': [
+                    {
+                      'ui:col': {
+                        md: 6,
+                        children: ['Type']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 6,
+                        children: ['Date']
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  },
+  ProcessingLevel: {
+    'ui:heading-level': 'h4',
+    'ui:field': 'layout',
+    'ui:layout_grid': {
+      'ui:row': [
+        {
+          'ui:group': 'Processing Level',
+          'ui:group-classname': 'h2-title',
+          'ui:group-description': true,
+          'ui:col': {
+            md: 12,
+            children: [
+              {
+                'ui:row': [
+                  {
+                    'ui:col': {
+                      md: 12,
+                      children: ['Id']
+                    }
+                  },
+                  {
+                    'ui:col': {
+                      md: 12,
+                      children: ['ProcessingLevelDescription']
+                    }
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      ]
+    },
+    Id: {
+      'ui:title': 'ID',
+      'ui:widget': CustomSelectWidget,
+      'ui:options': {
+        enumOptions: processingLevel
+      }
+    },
+    ProcessingLevelDescription: {
+      'ui:widget': CustomTextareaWidget
+    }
+  },
+  Quality: {
+    'ui:widget': CustomTextareaWidget
+  },
+  UseConstraints: {
+    'ui:widget': CustomSelectWidget,
+    'ui:heading-level': 'h4',
+    'ui:field': 'layout',
+    'ui:layout_grid': {
+      'ui:row': [
+        {
+          'ui:col': {
+            children: [
+              {
+                'ui:row': [
+                  {
+                    'ui:col': {
+                      md: 12,
+                      children: ['Description']
+                    }
+                  }
+                ]
+              },
+              {
+                'ui:row': [
+                  {
+                    'ui:col': {
+                      md: 12,
+                      children: ['FreeAndOpenData']
+                    }
+                  }
+                ]
+              },
+              {
+                'ui:row': [
+                  {
+                    'ui:col': {
+                      md: 12,
+                      children: ['EULAIdentifiers']
+                    }
+                  }
+                ]
+              },
+              {
+                'ui:row': [
+                  {
+                    'ui:col': {
+                      md: 12,
+                      children: ['LicenseURL']
+                    }
+                  }
+                ]
+              },
+              {
+                'ui:row': [
+                  {
+                    'ui:col': {
+                      md: 12,
+                      children: ['LicenseText']
+                    }
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      ]
+    },
+    Description: {
+      'ui:widget': CustomTextareaWidget
+    },
+    LicenseURL: {
+      'ui:heading-level': 'h4',
+      'ui:field': 'layout',
+      'ui:layout_grid': {
+        'ui:group': 'License URL',
+        'ui:required': 'false',
+        'ui:row': [
+          {
+            'ui:col': {
+              children: [
+                {
+                  'ui:row': [
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['Name']
+                      }
+                    }
+                  ]
+                },
+                {
+                  'ui:row': [
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['Linkage']
+                      }
+                    }
+                  ]
+                },
+                {
+                  'ui:row': [
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['Description']
+                      }
+                    }
+                  ]
+                },
+                {
+                  'ui:row': [
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['Protocol']
+                      }
+                    }
+                  ]
+                },
+                {
+                  'ui:row': [
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['ApplicationProfile']
+                      }
+                    }
+                  ]
+                },
+                {
+                  'ui:row': [
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['Function']
+                      }
+                    }
+                  ]
+                },
+                {
+                  'ui:row': [
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['MimeType']
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        ]
+      },
+      Description: {
+        'ui:widget': CustomTextareaWidget
+      },
+      MimeType: {
+        'ui:widget': CustomSelectWidget,
+        'ui:controlled': {
+          name: 'mime-type',
+          controlName: 'mime_type'
+        }
+      }
+    },
+    LicenseText: {
+      'ui:widget': CustomTextareaWidget
+    }
+  },
+  AccessConstraints: {
+    'ui:heading-level': 'h4',
+    'ui:field': 'layout',
+    'ui:layout_grid': {
+      'ui:row': [
+        {
+          'ui:group': 'Access Constraints',
+          'ui:group-description': true,
+          'ui:col': {
+            md: 12,
+            children: [
+              {
+                'ui:row': [
+                  {
+                    'ui:col': {
+                      md: 12,
+                      children: ['Value']
+                    }
+                  },
+                  {
+                    'ui:col': {
+                      md: 12,
+                      children: ['Description']
+                    }
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      ]
+    },
+    Description: {
+      'ui:widget': CustomTextareaWidget
+    }
+  },
+  MetadataAssociations: {
+    'ui:heading-level': 'h4',
+    items: {
+      'ui:field': 'layout',
+      'ui:layout_grid': {
+        'ui:row': [
+          {
+            'ui:col': {
+              md: 12,
+              children: [
+                {
+                  'ui:row': [
+                    {
+                      'ui:col': {
+                        md: 6,
+                        children: ['Type']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 6,
+                        children: ['EntryId']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['Description']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['Version']
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        ]
+      },
+      Description: {
+        'ui:widget': CustomTextareaWidget
+      }
+    }
+  },
+  PublicationReferences: {
+    'ui:heading-level': 'h4',
+    items: {
+      'ui:field': 'layout',
+      'ui:layout_grid': {
+        'ui:row': [
+          {
+            'ui:col': {
+              md: 12,
+              children: [
+                {
+                  'ui:row': [
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['Title']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['Publisher']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['DOI']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['Author']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['PublicationDate']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['Series']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['Edition']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 4,
+                        children: ['Volume']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 4,
+                        children: ['Issue']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 4,
+                        children: ['ReportNumber']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['PublicationPlace']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 6,
+                        children: ['Pages']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 6,
+                        children: ['ISBN']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['OtherReferenceDetails']
+                      }
+                    },
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['OnlineResource']
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        ]
+      },
+      Title: {
+        'ui:widget': CustomTextareaWidget
+      },
+      Publisher: {
+        'ui:widget': CustomTextareaWidget
+      },
+      DOI: {
+        'ui:heading-level': 'h5',
+        'ui:field': 'layout',
+        'ui:layout_grid': {
+          'ui:row': [
+            {
+              'ui:group': 'DOI',
+              'ui:group-classname': 'h2-title',
+              'ui:group-description': true,
+              'ui:col': {
+                md: 12,
+                children: [
+                  {
+                    'ui:row': [
+                      {
+                        'ui:col': {
+                          md: 12,
+                          children: ['DOI']
+                        }
+                      },
+                      {
+                        'ui:col': {
+                          md: 12,
+                          children: ['Authority']
+                        }
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+          ]
+        },
+        DOI: {
+          'ui:widget': CustomTextareaWidget
+        }
+      },
+      Author: {
+        'ui:widget': CustomTextareaWidget
+      },
+      Series: {
+        'ui:widget': CustomTextareaWidget
+      },
+      Edition: {
+        'ui:widget': CustomTextareaWidget
+      },
+      PublicationPlace: {
+        'ui:widget': CustomTextareaWidget
+      },
+      OtherReferenceDetails: {
+        'ui:widget': CustomTextareaWidget
+      },
+      OnlineResource: {
+        'ui:heading-level': 'h5',
+        'ui:field': 'layout',
+        'ui:layout_grid': {
+          'ui:group': 'Online Resource',
+          'ui:group-description': true,
+          'ui:required': 'false',
+          'ui:row': [
+            {
+              'ui:col': {
+                children: [
+                  {
+                    'ui:row': [
+                      {
+                        'ui:col': {
+                          md: 12,
+                          children: ['Name']
+                        }
+                      }
+                    ]
+                  },
+                  {
+                    'ui:row': [
+                      {
+                        'ui:col': {
+                          md: 12,
+                          children: ['Linkage']
+                        }
+                      }
+                    ]
+                  },
+                  {
+                    'ui:row': [
+                      {
+                        'ui:col': {
+                          md: 12,
+                          children: ['Description']
+                        }
+                      }
+                    ]
+                  },
+                  {
+                    'ui:row': [
+                      {
+                        'ui:col': {
+                          md: 12,
+                          children: ['Protocol']
+                        }
+                      }
+                    ]
+                  },
+                  {
+                    'ui:row': [
+                      {
+                        'ui:col': {
+                          md: 12,
+                          children: ['ApplicationProfile']
+                        }
+                      }
+                    ]
+                  },
+                  {
+                    'ui:row': [
+                      {
+                        'ui:col': {
+                          md: 12,
+                          children: ['Function']
+                        }
+                      }
+                    ]
+                  },
+                  {
+                    'ui:row': [
+                      {
+                        'ui:col': {
+                          md: 12,
+                          children: ['MimeType']
+                        }
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+          ]
+        },
+        Description: {
+          'ui:widget': CustomTextareaWidget
+        },
+        MimeType: {
+          'ui:widget': CustomSelectWidget,
+          'ui:controlled': {
+            name: 'mime-type',
+            controlName: 'mime_type'
+          }
+        }
+      }
+    }
+  }
+}
+
+export default dataIdentificationUiSchema
