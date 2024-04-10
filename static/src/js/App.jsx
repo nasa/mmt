581dff25be2812ebec71137178d406174ad708e2
Deep Mistry <deepmist2@gmail.com>
2024-04-10 17:07:19 -0400

    MMT-3546: Fixing route error in App

--- static/src/js/App.jsx
+++ static/src/js/App.jsx
@@ -153,7 +153,6 @@ export const App = () => {
                 )
               }
             />
-            />
             <Route
               path="templates/:templateType/new"
               element={
