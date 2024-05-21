728bc4d42139220b254d6657052da289b46a084f
Matthew Crouch <matthew@element84.com>
2024-05-21 15:51:33 -0400

    MMT-3529: Updates available providers query

--- static/src/js/operations/queries/getAvailableProviders.js
+++ static/src/js/operations/queries/getAvailableProviders.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client'
 
 // Query to available providers acls
 export const GET_AVAILABLE_PROVIDERS = gql`
-  query GetAvailableProviders($params: AclsInput) {
+  query AvailableProviders($params: AclsInput) {
     acls(params: $params) {
       items {
         providerIdentity
