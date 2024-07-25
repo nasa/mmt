8d236ac428170643dec0f4a22f98f3029f197014
htranho <34069728+htranho@users.noreply.github.com>
2024-07-25 18:05:50 -0400

    MMT-3771: Add GA4 script to index file

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,24 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
+import { ViteEjsPlugin } from 'vite-plugin-ejs'
+import { getApplicationConfig } from './sharedUtils/getConfig'
+
+const { analytics } = getApplicationConfig()
+const { gtmPropertyId, localIdentifier } = analytics
+const { enabled, propertyId } = localIdentifier
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    nodePolyfills()
+    nodePolyfills(),
+    ViteEjsPlugin({
+      gtmPropertyId,
+      environment: process.env.NODE_ENV || 'development',
+      gaPropertyId: propertyId,
+      includeDevGoogleAnalytics: enabled
+    })
   ],
   resolve: {
     alias: [
