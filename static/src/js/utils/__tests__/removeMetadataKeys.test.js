b43b4f8579a49ddda0085010bb5244714a8ea771
Deep Mistry <67551614+dmistry1@users.noreply.github.com>
2024-07-31 10:25:46 -0400

    MMT-3804: Adds EntryTitle to be removed from the metadata when cloning a record (#1268)

--- static/src/js/utils/__tests__/removeMetadataKeys.test.js
+++ static/src/js/utils/__tests__/removeMetadataKeys.test.js
@@ -13,4 +13,17 @@ describe('removeMetadataKeys', () => {
 
     expect(removeKeys).toMatchObject({ Description: 'test description' })
   })
+
+  test('should remove ShortName and EntryTitle from the metadata', () => {
+    const metadata = {
+      ShortName: 'test short name',
+      EntryTitle: 'test entry title',
+      Description: 'test description'
+    }
+    const keys = ['ShortName', 'EntryTitle']
+
+    const removeKeys = removeMetadataKeys(metadata, keys)
+
+    expect(removeKeys).toMatchObject({ Description: 'test description' })
+  })
 })
