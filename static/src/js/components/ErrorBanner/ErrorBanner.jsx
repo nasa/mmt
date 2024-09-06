a1526791548fa844bef82e6325259905d05a9a63
Mandy Parson <amanda.parson@nasa.gov>
2024-09-06 14:03:59 -0600

    MMT-3877: Cloning bug fix

--- static/src/js/components/ErrorBanner/ErrorBanner.jsx
+++ static/src/js/components/ErrorBanner/ErrorBanner.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import { Button } from 'react-bootstrap'
 import Alert from 'react-bootstrap/Alert'
 import Col from 'react-bootstrap/Col'
+import PropTypes from 'prop-types'
+import React, { useEffect, useState } from 'react'
 import Row from 'react-bootstrap/Row'
 
 /**
@@ -16,28 +17,77 @@ import Row from 'react-bootstrap/Row'
  */
 export const ErrorBanner = ({
   dataTestId,
-  message
-}) => (
-  <Row>
-    <Col>
-      <Alert variant="danger">
-        <Alert.Heading>Sorry!</Alert.Heading>
+  message,
+  previousURL
+}) => {
+  const [hasCMRLagError, setHasCMRLagError] = useState(false)
+  const [conceptIdDoesNotExist, setConceptIdDoesNotExist] = useState(false)
+  const currentURL = window.location.pathname
+  const conceptKeywords = ['/collections/', '/variables/', '/services/', '/tools/']
 
-        <span className="visually-hidden">{' '}</span>
+  // Catches if we are coming from a published record, indicating this is a CMR lag error
+  useEffect(() => {
+    if (previousURL && conceptKeywords.some((keyword) => previousURL.includes(keyword))) {
+      setHasCMRLagError(true)
+    }
 
-        <p data-testid={dataTestId}>{message}</p>
-      </Alert>
-    </Col>
-  </Row>
-)
+    if (currentURL && conceptKeywords.some((keyword) => currentURL.includes(keyword))) {
+      setConceptIdDoesNotExist(true)
+    }
+  }, [])
+
+  return (
+    hasCMRLagError ? (
+      <div>
+        <Alert className="fst-italic fs-6" variant="warning">
+          <i className="eui-icon eui-fa-info-circle" />
+          {' '}
+          Some operations may take time to populate in the Common Metadata Repository.
+          If you are not seeing what you expect below, please
+          {' '}
+          <Button
+            onClick={() => window.location.reload()}
+            className="text-decoration-underline"
+            style={
+              {
+                background: 'none',
+                border: 'none',
+                padding: 0,
+                color: 'blue',
+                textDecoration: 'underline',
+                cursor: 'pointer'
+              }
+            }
+          >
+            refresh the page
+          </Button>
+        </Alert>
+      </div>
+    ) : (
+      <Row>
+        <Col>
+          <Alert variant="danger">
+            <Alert.Heading>Sorry!</Alert.Heading>
+
+            <span className="visually-hidden">{' '}</span>
+
+            <p data-testid={dataTestId}>{conceptIdDoesNotExist ? 'This record does not exist' : message}</p>
+          </Alert>
+        </Col>
+      </Row>
+    )
+  )
+}
 
 ErrorBanner.propTypes = {
   dataTestId: PropTypes.string,
-  message: PropTypes.string.isRequired
+  message: PropTypes.string.isRequired,
+  previousURL: PropTypes.string
 }
 
 ErrorBanner.defaultProps = {
-  dataTestId: 'error-banner__message'
+  dataTestId: 'error-banner__message',
+  previousURL: null
 }
 
 export default ErrorBanner
--- static/src/js/components/ErrorBanner/__tests__/ErrorBanner.test.jsx
+++ static/src/js/components/ErrorBanner/__tests__/ErrorBanner.test.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { screen, render } from '@testing-library/react'
 
+import userEvent from '@testing-library/user-event'
+
 import { ErrorBanner } from '../ErrorBanner'
 
 describe('Error Banner Component', () => {
@@ -23,6 +25,34 @@ describe('Error Banner Component', () => {
 
       // Ensure the content is displayed correctly
       expect(screen.getByTestId('test-error')).toHaveTextContent('Cras mattis consectetur purus sit amet fermentum.')
+    })
+  })
+
+  describe('Provided a previousURL', () => {
+    test('it renders an error banner with prompt to refresh the page', async () => {
+      render(
+        <ErrorBanner message="Cras mattis consectetur purus sit amet fermentum." dataTestId="test-error" previousURL="/collections/C100000-TEST_PROV" />
+      )
+
+      // Ensure the content is displayed correctly
+      expect(screen.getByRole('alert', { name: '' })).toHaveTextContent('Some operations may take time to populate in the Common Metadata Repository. If you are not seeing what you expect below, please')
+
+      const refreshButton = screen.getByRole('button', { name: 'refresh the page' })
+      await userEvent.click(refreshButton)
+    })
+  })
+
+  describe('CurrentURL indicates the record does not exist', () => {
+    test('it renders an error banner with message', async () => {
+      const mockCurrentURL = new URL('http://localhost:5173/collections/C1000-TEST_PROV')
+      window.location = mockCurrentURL
+
+      render(
+        <ErrorBanner />
+      )
+
+      // Ensure the content is displayed correctly
+      expect(screen.getByRole('alert', { name: '' })).toHaveTextContent('Sorry! This record does not exist')
     })
   })
 })
