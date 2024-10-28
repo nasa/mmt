5291d7c30d37493dfa36a793322a5812e48e1da6
htranho <34069728+htranho@users.noreply.github.com>
2024-10-28 17:14:16 -0400

    MMT-3918: Remove simple util class

--- static/src/js/utils/removeInvalidGroupPermissions.js
+++
@@ -1,7 +0,0 @@
-/**
- * Given an array of group permissions, return items has id or userType
- * @param {Array} groupPermissions An array of group permissions
- */
-const removeInvalidGroupPermissions = (items) => items.filter((item) => item.id || item.userType)
-
-export default removeInvalidGroupPermissions
