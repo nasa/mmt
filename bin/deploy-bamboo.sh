52e432f676e51ef969af0e8696b94edd3c275c0e
htranho <34069728+htranho@users.noreply.github.com>
2025-04-21 15:45:18 -0400

    MMT-4013: Add Version Selector for KeywordManager (#1350)
    * MMT-4013: Add Version selector for Keyword Manager
    
    * MMT-4013: Remove files belong to MMT-4012
    
    * MMT-4013: Add Version Selector to KeywordManager page
    
    * MMT-4013: Add Version label
    
    * MMT-4013: Add comments
    
    * MMT-4013: Fix tests
    
    * MMT-4013: Fix onHandleChange
    
    * MMT-4013: Fix imports, add and fix tests
    
    * MMT-4013: Remove unused state variable
    
    * MMT-4013: Add example of usage
    
    * MMT-4013: Fix and update tests

--- bin/deploy-bamboo.sh
+++ bin/deploy-bamboo.sh
@@ -18,6 +18,9 @@ config="`jq '.application.apiHost = $newValue' --arg newValue $bamboo_API_HOST <
 config="`jq '.application.cmrHost = $newValue' --arg newValue $bamboo_CMR_HOST <<< $config`"
 config="`jq '.application.edscHost = $newValue' --arg newValue $bamboo_EDSC_HOST <<< $config`"
 config="`jq '.application.gkrHost = $newValue' --arg newValue $bamboo_GKR_HOST <<< $config`"
+config="`jq '.application.kmsHost = $newValue' --arg newValue $bamboo_KMS_HOST <<< $config`"
+# Remove after CMR-10452 has been completed
+config="`jq '.application.showKeywordManager = $newValue' --arg newValue $bamboo_SHOW_KEYWORD_MANAGER <<< $config`"
 config="`jq '.application.cookieDomain = $newValue' --arg newValue $bamboo_COOKIE_DOMAIN <<< $config`"
 config="`jq '.application.displayProdWarning = $newValue' --arg newValue $bamboo_DISPLAY_PROD_WARNING <<< $config`"
 config="`jq '.application.tokenValidTime = $newValue' --arg newValue $bamboo_JWT_VALID_TIME <<< $config`"
