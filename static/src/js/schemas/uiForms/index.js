1df9c1875c733d25a61230840e32e3790cb65d30
Mandy Parson <amanda.parson@nasa.gov>
2025-04-23 11:46:40 -0600

    MMT-4004: Removing keywordConfigs

--- static/src/js/schemas/uiForms/index.js
+++ static/src/js/schemas/uiForms/index.js
@@ -2,14 +2,12 @@ import servicesConfiguration from './serviceConfiguration'
 import toolsConfiguration from './toolsConfiguration'
 import collectionsConfiguration from './collectionsConfiguration'
 import variableConfiguration from './variableConfiguration'
-import keywordConfiguration from './keywordConfiguration'
 
 const formConfigurations = {
   Tool: toolsConfiguration,
   Service: servicesConfiguration,
   Collection: collectionsConfiguration,
-  Variable: variableConfiguration,
-  Keywords: keywordConfiguration
+  Variable: variableConfiguration
 }
 
 export default formConfigurations
