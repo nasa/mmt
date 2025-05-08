75e056d228d05aa1fec785daf4ddd3026c296c39
William Valencia <william.m.valencia@nasa.gov>
2025-05-07 22:01:03 -0400

    MMT-3987: Changing precompile uglifier call

--- config/environments/sit.rb
+++ config/environments/sit.rb
@@ -25,7 +25,11 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.configure do |env|
+    env.js_compressor  = :uglifier # or :closure, :yui
+    env.css_compressor = :sass   # or :yui
+  end
+  # config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
