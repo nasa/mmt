5140ae9d93e31beb6a72e878481a1a4a538b59d8
william-valencia <51178394+william-valencia@users.noreply.github.com>
2025-06-10 16:54:21 -0400

    MMT-4001: Getting more info for visualization associations (#1376)
    * MMT-4001: Getting more info for visualization associations
    
    * MMT-4001: Fixing sorting for collection associations table

--- static/src/js/operations/queries/getVisualization.js
+++ static/src/js/operations/queries/getVisualization.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client'
 
 export const GET_VISUALIZATION = gql`
-  query GetVisualization ($params: VisualizationInput) {
+  query GetVisualization ($params: VisualizationInput, $collectionsParams: CollectionsInput) {
     visualization (params: $params) {
       conceptId
       description
@@ -17,11 +17,14 @@ export const GET_VISUALIZATION = gql`
       revisions {
         count
       }
-      collections {
+      collections (params: $collectionsParams) {
         count
         items {
           conceptId
+          provider
+          shortName
           title
+          version
         }
       }
       scienceKeywords
