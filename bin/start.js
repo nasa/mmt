cdf0d4e237d1970213c9bec4ddbf643668240f95
htranho <34069728+htranho@users.noreply.github.com>
2025-07-22 12:22:59 -0400

    MMT-4061: Use node.id to call delete keyword (#1387)
    * MMT-4061: Use node.id to call delete keyword
    
    * MMT-4061: Add tests, rename conceptId to uuid
    
    * MMT-4061: Add start script for development
    
    * MMT-4061: Update to run cmr:start_and_setup for local dev token to work.
    
    * MMT-4061: Update form-data

---
+++ bin/start.js
@@ -0,0 +1,25 @@
+const concurrently = require('concurrently')
+
+concurrently([{
+  command: 'npm run watch',
+  name: 'watch'
+}, {
+  command: 'npm run cmr:start_and_setup',
+  name: 'cmr'
+}, {
+  command: 'npm run start:app',
+  name: 'vite'
+},
+{
+  command: 'npm run offline',
+  name: 'api'
+},
+{
+  command: 'npm run start:proxy',
+  name: 'proxy'
+}], {
+  prefix: 'name',
+  padPrefix: true,
+  prefixColors: 'auto',
+  handleInput: true
+})
