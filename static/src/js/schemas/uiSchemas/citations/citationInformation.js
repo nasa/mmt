a2ad399a6e88573d4555c34ec459f1e7f7158ed5
Mandy Parson <amanda.parson@nasa.gov>
2025-07-30 09:43:19 -0600

    MMT-4053: Adding needed files for other tickets

---
+++ static/src/js/schemas/uiSchemas/citations/citationInformation.js
@@ -0,0 +1,72 @@
+const citationInformationUiSchema = {
+  'ui:heading-level': 'h3',
+  'ui:field': 'layout',
+  'ui:layout_grid': {
+    'ui:row': [
+      {
+        'ui:group': 'Citation Information',
+        'ui:required': true,
+        'ui:col': {
+          md: 12,
+          children: [
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['Identifier']
+                  }
+                }
+              ]
+            },
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['IdentifierType']
+                  }
+                }
+              ]
+            },
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['Name']
+                  }
+                }
+              ]
+            },
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['Abstract']
+                  }
+                }
+              ]
+            },
+            {
+              'ui:row': [
+                {
+                  'ui:col': {
+                    md: 12,
+                    children: ['ResolutionAuthority']
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      }
+    ]
+  },
+  Abstract: {
+    'ui:widget': 'textarea'
+  }
+}
+
+export default citationInformationUiSchema
