be66510dd06d0249e19ad89894f41d043c2b0b46
htranho <hoan.v.tranho@nasa.gov>
2025-08-07 16:32:59 -0400

    MMT-4029: Use uuid for ID

--- static/src/js/components/JsonFileUploadModal/JsonFileUploadModal.jsx
+++ static/src/js/components/JsonFileUploadModal/JsonFileUploadModal.jsx
@@ -6,10 +6,10 @@ import React, {
 import PropTypes from 'prop-types'
 import { FaFile } from 'react-icons/fa'
 import { useDropzone } from 'react-dropzone'
+import { v4 as uuidv4 } from 'uuid'
 
 import CustomModal from '@/js/components/CustomModal/CustomModal'
 import { validateJson } from '@/js/utils/validateJson'
-import generateRandomId from '@/js/utils/generateRandomId'
 
 import './JsonFileUploadModal.scss'
 
@@ -64,7 +64,7 @@ export const JsonFileUploadModal = ({
   const handleUpload = () => {
     if (!file) {
       setErrors([{
-        id: generateRandomId,
+        id: uuidv4(),
         message: 'Please select a file to upload.'
       }])
 
@@ -81,7 +81,7 @@ export const JsonFileUploadModal = ({
         })
         if (validationResult.errors) {
           setErrors(validationResult.errors.map((error) => ({
-            id: generateRandomId,
+            id: uuidv4(),
             message: error
           })))
         } else {
@@ -90,7 +90,7 @@ export const JsonFileUploadModal = ({
         }
       } catch (error) {
         setErrors([{
-          id: generateRandomId,
+          id: uuidv4(),
           message: 'Invalid JSON file. Please upload a valid JSON file.'
         }])
       }
