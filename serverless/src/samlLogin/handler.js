9b917956f14d3d14b1d16e5f55dd0c2866535ae9
htranho <34069728+htranho@users.noreply.github.com>
2025-08-11 14:40:47 -0400

    MMT-4067: Update @node-saml/node-saml and @edsc/metadata-preview (#1397)
    * MMT-4047: Update @node-saml/node-saml
    
    * MMT-4067: Update to use @edsc/metadata-preview 1.4.1
    
    * MMT-4067: Add comment

--- serverless/src/samlLogin/handler.js
+++ serverless/src/samlLogin/handler.js
@@ -9,10 +9,11 @@ import { getSamlConfig } from '../../../sharedUtils/getConfig'
 const samlLogin = async (event) => {
   const options = getSamlConfig()
 
-  // SAML package will not produce an authorization url (see below) without the 'cert' property,
+  // SAML package will not produce an authorization url (see below) without the 'cert' and the 'idpCert' property,
   // although it is only necessary if we are validating tokens (which we are not doing at the
   // moment)
   options.cert = 'fake_cert'
+  options.idpCert = 'fake_idp_cert'
   const saml = new SAML(options)
   const { queryStringParameters } = event
   const { target: relayState } = queryStringParameters || {}
