074dcf3cdedc4a868f42370f47c0b7fd36d443fa
Mandy Parson <135638667+mandyparson@users.noreply.github.com>
2025-08-20 15:17:19 -0600

    MMT-4050-1: Fixing additional issue (#1400)

--- static/src/js/utils/__tests__/checkForCMRFetchDraftLag.test.js
+++ static/src/js/utils/__tests__/checkForCMRFetchDraftLag.test.js
@@ -7,9 +7,15 @@ describe('checkForCMRFetchDraftLag', () => {
     })
   })
 
-  describe('when the fetched revision Id does not match expected revision Id', () => {
+  describe('when the fetched revision Id is less than the expected revision Id, indicating a cmr lag', () => {
     test('throws error', () => {
       expect(() => checkForCMRFetchDraftLag('1', '2')).toThrow('Delay in CMR has been detected. Refresh the page in order to see latest revision')
+    })
+  })
+
+  describe('when the fetched revision Id is greater than the expected revision Id, indicating a draft has been published', () => {
+    test('throws error', () => {
+      expect(() => checkForCMRFetchDraftLag('2', '1')).not.toThrow('Delay in CMR has been detected. Refresh the page in order to see latest revision')
     })
   })
 })
