129f036e3af25d82989e6679f9f76061f77bf3bc
Mandy Parson <amanda.parson@nasa.gov>
2025-09-29 09:59:52 -0600

    MMT-4058: Enabling citation association to collection

--- static/src/js/operations/queries/getCitation.js
+++ static/src/js/operations/queries/getCitation.js
@@ -1,10 +1,20 @@
 import { gql } from '@apollo/client'
 
 export const GET_CITATION = gql`
-  query GetCitation ($params: CitationInput) {
+  query GetCitation ($params: CitationInput, $collectionsParams: CollectionsInput) {
     citation (params: $params) {
       abstract
       citationMetadata
+      collections (params: $collectionsParams) {
+        count
+        items {
+          conceptId
+          provider
+          shortName
+          title
+          version
+        }
+      }
       conceptId
       identifier
       identifierType
