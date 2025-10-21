02b06c2c3a650cd8b7e376ce5a5ffd0ac3c81cb5
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2025-10-21 14:44:25 -0400

    MMT-4038: Added missing Aggregation field into UI schema.

--- static/src/js/schemas/uiSchemas/services/serviceOptions.js
+++ static/src/js/schemas/uiSchemas/services/serviceOptions.js
@@ -23,6 +23,13 @@ const serviceOptionsUiSchema = {
                   },
                   {
                     'ui:col': {
+                      className: 'grid-layout__field-left-border',
+                      md: 12,
+                      children: ['Aggregation']
+                    }
+                  },
+                  {
+                    'ui:col': {
                       md: 12,
                       children: ['VariableAggregationSupportedMethods']
                     }
@@ -163,6 +170,35 @@ const serviceOptionsUiSchema = {
             }
           ]
         }
+      }
+    },
+    Aggregation: {
+      'ui:fieldReplacesAnyOrOneOf': true,
+      'ui:group-description': true,
+      'ui:heading-level': 'h4',
+      'ui:title': 'Aggregation',
+      'ui:field': 'layout',
+      'ui:layout_grid': {
+        'ui:row': [
+          {
+            'ui:group': 'Aggregation',
+            'ui:group-description': true,
+            'ui:col': {
+              md: 12,
+              children: [
+                {
+                  'ui:row': [
+                    {
+                      'ui:col': {
+                        md: 12,
+                        children: ['Concatenate']
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          }]
       }
     },
     SupportedOutputProjections: {
