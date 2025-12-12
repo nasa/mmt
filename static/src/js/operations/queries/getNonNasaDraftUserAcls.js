4626e4cc8751ad7416503161b86f2cd214b057dc
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2025-12-12 10:49:25 -0500

    MMT-4138: Fixed copy/paste error.

--- static/src/js/operations/queries/getNonNasaDraftUserAcls.js
+++ static/src/js/operations/queries/getNonNasaDraftUserAcls.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client'
 
-// Query to available providers acls
+// Query to get non nasa draft user acls
 export const GET_NON_NASA_DRAFT_USER_ACLS = gql`
   query GetNonNasaDraftUserAcls($params: AclsInput) {
     acls(params: $params) {
