4a1c316ecf17222f51e4d942e8a681f8ab4861f3
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2026-02-18 09:22:04 -0500

    MMT-4135: Change proxy host

--- setup/graphQlProxy.js
+++ setup/graphQlProxy.js
@@ -3,27 +3,25 @@ const httpProxy = require('http-proxy')
 
 const proxy = httpProxy.createProxyServer({})
 const apiProxyTarget = process.env.MMT_API_PROXY_TARGET || 'https://localhost:8443/cgokey-sit'
-const apiProxyHostHeader = process.env.MMT_API_HOST_HEADER
 
 // Create a proxy server to redirect path based CMR requests to the correct local CMR ports
 const server = http.createServer((req, res) => {
   // `req.url` here is the path of the requested URL
-  if (req.url.includes('/sit')) {
-    const [, rest] = req.url.split('/sit')
+  if (req.url.includes('/cgokey-sit')) {
+    const [, rest] = req.url.split('/cgokey-sit')
 
     // Replace the url value with everything after /sit
     req.url = rest || '/'
     const proxyOptions = {
       target: apiProxyTarget,
-      changeOrigin: true,
       secure: false
     }
 
-    if (apiProxyHostHeader) {
-      proxyOptions.headers = {
-        host: apiProxyHostHeader
-      }
+    proxyOptions.headers = {
+      ...req.headers
     }
+
+    proxyOptions.headers.host = '34lb0t2px1.execute-api.us-east-1.amazonaws.com'
 
     proxy.web(req, res, {
       ...proxyOptions
