3b613d5ea67d799d3cf3b34f6487a3a16892a2cc
Mandy Parson <amanda.parson@nasa.gov>
2026-08-24 12:44:28 -0400

    MMT-4182: Small fixes

--- static/src/storeAuthToken.js
+++ static/src/storeAuthToken.js
@@ -2,7 +2,7 @@ import consumeAuthToken from './js/utils/consumeAuthToken'
 
 /**
  * Imported for its side effect and imported ahead of the application on
- * purpose. ES modules are evaluted in the order they are imported and the
+ * purpose. ES modules are evaluated in the order they are imported and the
  * application's import graph evaluates 'react-cookie', which snapshots
  * document.cookie as it loads. A cookie written after that point is missing
  * from the first render and 'AuthContextProvider' treats a missing token as
