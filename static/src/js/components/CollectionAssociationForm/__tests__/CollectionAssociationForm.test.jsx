f9a38bab99111b371de97ad815b284c15c14dcce
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2026-09-17 15:35:33 -0400

    MMT-4209: oops remove .only from testing

--- static/src/js/components/CollectionAssociationForm/__tests__/CollectionAssociationForm.test.jsx
+++ static/src/js/components/CollectionAssociationForm/__tests__/CollectionAssociationForm.test.jsx
@@ -116,7 +116,7 @@ const setup = ({
 
 describe('CollectionAssociationForm component', () => {
   describe('when the component mounts', () => {
-    test.only('it should render the search form', async () => {
+    test('it should render the search form', async () => {
       const { user } = setup({})
 
       const searchField = await screen.findByText('Select Search Field')
